ad6cacb0dd5c77d5de5c6c19b87511b5ee637a9b
Yury Orlov <mpcteam.kvet@gmail.com>
2017-04-05 10:20:27 +0300

    Initial landing page setup

--- packages/dx-react-demos/webpack.config.js
+++ packages/dx-react-demos/webpack.config.js
@@ -4,50 +4,51 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    context: path.join(__dirname, 'src'),
-    entry: {
-        index: path.join(__dirname, 'src', 'index')
+  context: path.join(__dirname, 'src'),
+  entry: {
+    index: path.join(__dirname, 'src', 'index')
+  },
+  output: {
+    publicPath: '/dist',
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components|public\/)/,
+        use: ["babel-loader"]
+      },
+      { 
+        test: /\.css$/, 
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      // Strange hack for lerna
+      '@devexpress/dx-react-datagrid-bootstrap3': path.resolve('./node_modules/@devexpress/dx-react-datagrid-bootstrap3'),
+      '@devexpress/dx-react-datagrid': path.resolve('./node_modules/@devexpress/dx-react-datagrid'),
+      '@devexpress/dx-datagrid-core': path.resolve('./node_modules/@devexpress/dx-react-datagrid/node_modules/@devexpress/dx-datagrid-core'),
+      '@devexpress/dx-react-core': path.resolve('./node_modules/@devexpress/dx-react-datagrid/node_modules/@devexpress/dx-react-core'),
+      '@devexpress/dx-core': path.resolve('./node_modules/@devexpress/dx-react-datagrid/node_modules/@devexpress/dx-react-core/node_modules/@devexpress/dx-core'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
     },
-    output: {
-		publicPath: '/dist',
-		path: path.join(__dirname, 'dist'),
-		filename: '[name].js'
-	},
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components|public\/)/,
-                use: ["babel-loader"]
-            },
-            { 
-                test: /\.css$/, 
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    resolve: {
-        alias: {
-            // Strange hack for lerna
-            '@devexpress/dx-react-datagrid-bootstrap3': path.resolve('./node_modules/@devexpress/dx-react-datagrid-bootstrap3'),
-            '@devexpress/dx-react-datagrid': path.resolve('./node_modules/@devexpress/dx-react-datagrid'),
-            '@devexpress/dx-datagrid-core': path.resolve('./node_modules/@devexpress/dx-react-datagrid/node_modules/@devexpress/dx-datagrid-core'),
-            '@devexpress/dx-react-core': path.resolve('./node_modules/@devexpress/dx-react-datagrid/node_modules/@devexpress/dx-react-core'),
-            '@devexpress/dx-core': path.resolve('./node_modules/@devexpress/dx-react-datagrid/node_modules/@devexpress/dx-react-core/node_modules/@devexpress/dx-core'),
-            'react-dom': path.resolve('./node_modules/react-dom'),
-        },
-        extensions: [".webpack.js", ".web.js", ".js", ".jsx"]
-    },
-    plugins: [
-        new webpack.DefinePlugin({
-            "process.env": { 
-                //NODE_ENV: JSON.stringify("production") 
-            }
-        })
-    ],
-    devtool: 'eval-source-map',
-    devServer: {
-        host: '0.0.0.0',
-        port: 3002,
-    }
+    extensions: [".webpack.js", ".web.js", ".js", ".jsx"]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": { 
+        //NODE_ENV: JSON.stringify("production") 
+      }
+    })
+  ],
+  devtool: 'eval-source-map',
+  devServer: {
+    host: '0.0.0.0',
+    port: 3002,
+    historyApiFallback: true,
+  }
 }

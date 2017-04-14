25847bd34d9756ff1d250b015b32d02b52d54810
Yury Orlov <mpcteam.kvet@gmail.com>
2017-04-14 15:06:48 +0300

    Virtual scrolling guide

--- packages/dx-react-datagrid/docs/guides/virtual-scrolling.md
+++ packages/dx-react-datagrid/docs/guides/virtual-scrolling.md
@@ -1,3 +1,27 @@
-# DataGrid Virtual Srolling
+# DataGrid Virtual Scrolling
 
-To be described...
+## Overview
+
+The DataGrid component can handle a large amount of data passed to it. In some cases it may be required to show 10 000 rows or even 1 000 000 ones. As an alternative to paging with hundreds or thousands or pages there is virtual mode for data presentation.
+
+## Plugin List
+
+There are no plugins that implement virtual scrolling. It is designed as an alternative to the ordinary table template for the [TableView](table-view.md) plugin.
+
+## Basic Setup
+
+The following example demonstrates the basic DataGrid with virtual scrolling setup:
+
+[DEMO](http://devexpress.github.io/devextreme-reactive/react/datagrid/demos/#/virtual-scrolling/basic)
+
+[SOURCE](https://github.com/DevExpress/devextreme-reactive/tree/master/packages/dx-react-demos/src/bootstrap3/virtual-scrolling/basic.jsx)
+
+## Integration with Other Plugins
+
+Virtual table is also capable to work with other DataGrid features like filtering, sorting and so on. Here is an example:
+
+[DEMO](http://devexpress.github.io/devextreme-reactive/react/datagrid/demos/#/virtual-scrolling/integration-with-other-plugins)
+
+[SOURCE](https://github.com/DevExpress/devextreme-reactive/tree/master/packages/dx-react-demos/src/bootstrap3/virtual-scrolling/integration-with-other-plugins.jsx)
+
+
---
+++ packages/dx-react-demos/src/bootstrap3/virtual-scrolling/integration-with-other-plugins.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import {
+    DataGrid,
+    TableHeaderRow,
+    FilteringState,
+    SortingState,
+    LocalFiltering,
+    LocalSorting,
+} from '@devexpress/dx-react-datagrid';
+import {
+    VirtualTableView,
+    TableFilterRow,
+    TableHeaderRowSorting,
+    TableRowDetail,
+} from '@devexpress/dx-react-datagrid-bootstrap3';
+
+import {
+  generateColumns,
+  generateRows,
+} from '../../demoData';
+
+export class IntegrationWithOtherPluginsDemo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      columns: generateColumns(),
+      rows: generateRows(100000),
+    };
+
+    this.rowTemplate = ({ row }) => (
+      <div>
+        <div>Detail for {row.name} ({row.sex})</div>
+        <div>City: {row.city}</div>
+        <div>Car: {row.car}</div>
+      </div>
+    );
+  }
+  render() {
+    const { rows, columns } = this.state;
+
+    return (
+      <DataGrid
+        rows={rows}
+        columns={columns}
+      >
+        <FilteringState defaultFilters={[]} />
+        <SortingState defaultSorting={[]} />
+
+        <LocalFiltering />
+        <LocalSorting />
+
+        <VirtualTableView />
+
+        <TableHeaderRow />
+        <TableHeaderRowSorting />
+
+        <TableFilterRow />
+
+        <TableRowDetail template={this.rowTemplate} />
+      </DataGrid>
+    );
+  }
+}

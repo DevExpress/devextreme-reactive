09f46fe111d502316e4569f1214105ec48cb37c5
Alexander Bykov <dxbykov@users.noreply.github.com>
2017-04-26 16:13:02 +0300

    Site improvements (#25)

--- packages/dx-react-demos/src/index.jsx
+++ packages/dx-react-demos/src/index.jsx
@@ -48,7 +48,7 @@ const Container = withRouter(({ location }) => (
     : (
       <div className="container">
         <div className="row">
-          <div className="col-md-3">
+          <div className="col-md-3 main-menu">
             <h3>Simple Demos</h3>
             <ul className="list-unstyled">
               <li><Link to="/basic">Basic</Link></li>
@@ -64,7 +64,7 @@ const Container = withRouter(({ location }) => (
             </ul>
           </div>
 
-          <div className="col-md-9">
+          <div className="col-md-9 demo-content">
             <Demos />
           </div>
         </div>

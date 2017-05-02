74c93db22fd0ce486f8033ddcf7d2afd66610576
bykov <dxbykov@users.noreply.github.com>
2017-05-02 20:47:27 +0300

    Improve header styles

--- packages/dx-react-demos/src/bootstrap3/featured-uncontrolled.jsx
+++ packages/dx-react-demos/src/bootstrap3/featured-uncontrolled.jsx
@@ -4,7 +4,7 @@ import { UncontrolledModeDemo } from './featured/uncontrolled-mode';
 
 export const FeaturedUncontrolledDemos = () => (
   <div>
-    <h2>DataGrid Uncontrolled State Mode</h2>
+    <h1>DataGrid Uncontrolled State Mode</h1>
     <UncontrolledModeDemo />
     <a href="https://github.com/DevExpress/devextreme-reactive/blob/master/packages/dx-react-demos/src/bootstrap3/featured/uncontrolled-mode.jsx">
       SOURCE CODE
--- packages/dx-react-demos/src/bootstrap3/featured-virtual-scrolling.jsx
+++ packages/dx-react-demos/src/bootstrap3/featured-virtual-scrolling.jsx
@@ -4,7 +4,7 @@ import { VirtualScrollingDemo } from './featured/virtual-scrolling';
 
 export const FeaturedVirtualScrollingDemos = () => (
   <div>
-    <h2>DataGrid Virtual Scrolling (200K rows)</h2>
+    <h1>DataGrid Virtual Scrolling (200K rows)</h1>
     <VirtualScrollingDemo />
     <a href="https://github.com/DevExpress/devextreme-reactive/blob/master/packages/dx-react-demos/src/bootstrap3/featured/virtual-scrolling.jsx">
       SOURCE CODE

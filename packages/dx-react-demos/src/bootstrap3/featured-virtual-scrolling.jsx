9226bbebf492c66d2fa2487067c31bd2cb5b86c0
Victor Shchelkunov <viterobk@users.noreply.github.com>
2017-05-02 10:50:50 +0300

    Update featured-virtual-scrolling.jsx

--- packages/dx-react-demos/src/bootstrap3/featured-virtual-scrolling.jsx
+++ packages/dx-react-demos/src/bootstrap3/featured-virtual-scrolling.jsx
@@ -7,8 +7,8 @@ export const FeaturedVirtualScrollingDemos = () => (
     <h2>DataGrid Virtual Scrolling (200K rows)</h2>
     <p>
       Virtual scrolling mode is an alternative to data paging. It allows end users to navigate
-      the data rows by using its vertical scrollbar. In this demo, the DataGrid is bound to 200,000
-      records and virtual scrolling is enabled by using the VirtualTableView plugin.
+      the data rows using the vertical scrollbar. In this demo, the DataGrid is bound to 200,000
+      records and virtual scrolling is enabled using the VirtualTableView plugin.
     </p>
     <VirtualScrollingDemo />
     <a href="https://github.com/DevExpress/devextreme-reactive/blob/master/packages/dx-react-demos/src/bootstrap3/featured/virtual-scrolling.jsx">

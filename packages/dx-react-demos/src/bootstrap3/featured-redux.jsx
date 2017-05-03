8008f90b3a583f2ac00631c92155a03bb4f133ea
YanaOrlova <yana.orlova@devexpress.com>
2017-05-03 16:00:32 +0300

    Update featured-redux.jsx

--- packages/dx-react-demos/src/bootstrap3/featured-redux.jsx
+++ packages/dx-react-demos/src/bootstrap3/featured-redux.jsx
@@ -6,11 +6,11 @@ export const FeaturedReduxDemos = () => (
   <div>
     <h2>DataGrid Redux Integration</h2>
     <p>
-      In this demo the DataGrid state is managed by the Redux store. DataGrid works as
-      stateless component. All end-user actions that modifiy state produce corresponding
+      In this demo the DataGrid state is managed by the Redux store. DataGrid works as a
+      stateless component. All end-user actions that modifiy the component state produce corresponding
       Redux actions that are dispatched by the DataGrid reducer.
       The Redux DevTool Extenstion is activated in the code of this demo. You can open it
-      and perform time-traveling and observe the DataGrid state mutations.
+      and perform time-traveling and observe DataGrid state mutations.
     </p>
     <ReduxIntegrationDemo />
     <a href="https://github.com/DevExpress/devextreme-reactive/blob/master/packages/dx-react-demos/src/bootstrap3/featured/redux-integration.jsx">

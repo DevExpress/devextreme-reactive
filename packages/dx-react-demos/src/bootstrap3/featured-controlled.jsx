eb8d079f85ae3ca6c676cef8995593a119a30f4e
YanaOrlova <yana.orlova@devexpress.com>
2017-05-03 15:59:19 +0300

    Update featured-controlled.jsx

--- packages/dx-react-demos/src/bootstrap3/featured-controlled.jsx
+++ packages/dx-react-demos/src/bootstrap3/featured-controlled.jsx
@@ -9,8 +9,8 @@ export const FeaturedControlledDemos = () => (
       This demo shows the DataGrid capability to delegate its UI state management to the parent
       component. You can contol either the whole DataGrid state or just a part of it. In the
       first case, DataGrid becomes a stateless component. This mode is usefull if you need
-      to use DataGrid state in the other parts of your application. Using this mode you
-      can also persist state and restore it when you need.
+      to use the DataGrid state in other parts of your application. Using this mode, you
+      can also persist the component state and restore it when required.
     </p>
     <ControlledModeDemo />
     <a href="https://github.com/DevExpress/devextreme-reactive/blob/master/packages/dx-react-demos/src/bootstrap3/featured/controlled-mode.jsx">

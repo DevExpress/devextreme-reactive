e29456572f86b17519c2bff5bd648bbbded485f7
Yury Orlov <mpcteam.kvet@gmail.com>
2017-05-03 14:34:57 +0300

    Remove merge sort (#53)

--- packages/dx-datagrid-core/src/plugins/sorting-state/computeds.js
+++ packages/dx-datagrid-core/src/plugins/sorting-state/computeds.js
@@ -1,67 +1,12 @@
-function mergeSort(array, comparefn) {
-  /* eslint-disable */
-  function merge(arr, aux, lo, mid, hi, comparefn) {
-    let i = lo;
-    let j = mid + 1;
-    let k = lo;
-    while (true) {
-      const cmp = comparefn(arr[i], arr[j]);
-      if (cmp <= 0) {
-        aux[k++] = arr[i++];
-        if (i > mid) {
-          do { aux[k++] = arr[j++]; }
-          while (j <= hi);
-          break;
-        }
-      } else {
-        aux[k++] = arr[j++];
-        if (j > hi) {
-          do { aux[k++] = arr[i++]; }
-          while (i <= mid);
-          break;
-        }
-      }
-    }
-  }
-
-  function sortarrtoaux(arr, aux, lo, hi, comparefn) {
-    if (hi < lo) return;
-    if (hi == lo) {
-      aux[lo] = arr[lo];
-      return;
-    }
-    const mid = Math.floor(lo + (hi - lo) / 2);
-    sortarrtoarr(arr, aux, lo, mid, comparefn);
-    sortarrtoarr(arr, aux, mid + 1, hi, comparefn);
-    merge(arr, aux, lo, mid, hi, comparefn);
-  }
-
-  function sortarrtoarr(arr, aux, lo, hi, comparefn) {
-    if (hi <= lo) return;
-    const mid = Math.floor(lo + (hi - lo) / 2);
-    sortarrtoaux(arr, aux, lo, mid, comparefn);
-    sortarrtoaux(arr, aux, mid + 1, hi, comparefn);
-    merge(aux, arr, lo, mid, hi, comparefn);
-  }
-
-  function merge_sort(arr, comparefn) {
-    const aux = arr.slice(0);
-    sortarrtoarr(arr, aux, 0, arr.length - 1, comparefn);
-    return arr;
-  }
-
-  return merge_sort(array, comparefn);
-};
-
 const createSortingCompare = (sorting, compareEqual) => (a, b) => {
-  let sortColumn = sorting.column,
-    inverse = sorting.direction === 'desc';
+  const sortColumn = sorting.column;
+  const inverse = sorting.direction === 'desc';
 
   if (a[sortColumn] === b[sortColumn]) {
-    return compareEqual && compareEqual(a, b) || 0;
+    return (compareEqual && compareEqual(a, b)) || 0;
   }
 
-  return (a[sortColumn] < b[sortColumn]) ^ inverse ? -1 : 1;
+  return (a[sortColumn] < b[sortColumn]) ^ inverse ? -1 : 1; // eslint-disable-line no-bitwise
 };
 
 export const sortedRows = (rows, sortings) => {
@@ -71,5 +16,5 @@ export const sortedRows = (rows, sortings) => {
         .reverse()
         .reduce((prevCompare, sorting) => createSortingCompare(sorting, prevCompare), () => 0);
 
-  return mergeSort(rows.slice(), compare);
+  return rows.slice().sort(compare);
 };

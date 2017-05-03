6de7a163ea70b978d1c452efc2273c24159c5d25
YanaOrlova <yana.orlova@devexpress.com>
2017-05-03 16:01:43 +0300

    Update featured-uncontrolled.jsx

--- packages/dx-react-demos/src/bootstrap3/featured-uncontrolled.jsx
+++ packages/dx-react-demos/src/bootstrap3/featured-uncontrolled.jsx
@@ -7,11 +7,11 @@ export const FeaturedUncontrolledDemos = () => (
     <h2>DataGrid Uncontrolled State Mode</h2>
     <p>
       This demo shows the DataGrid capability to manage its UI state internally. This mode is
-      usefull if you do not need to use DataGrid state in the other parts of your application.
-      Also it can be useful if you are just prototyping your application and do not want to
+      usefull if you do not need to use the DataGrid state in other parts of your application.
+      Also, it can be useful if you are just prototyping your application and do not want to
       spend time on learning what this state consists of. If you just need to display your data
-      and analyse it using DataGrid features such as sorting, grouping, filtering, etc., you will
-      also like the uncontrolled state mode.
+      and analyse it using DataGrid features, such as sorting, grouping, filtering, etc., you will
+      also find the uncontrolled state mode helpful.
     </p>
     <UncontrolledModeDemo />
     <a href="https://github.com/DevExpress/devextreme-reactive/blob/master/packages/dx-react-demos/src/bootstrap3/featured/uncontrolled-mode.jsx">

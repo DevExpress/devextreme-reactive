188578ee6cc6ada0b69ca35696a9e8ed3fca343c
Yury Orlov <mpcteam.kvet@gmail.com>
2017-05-03 16:25:00 +0300

    Update featured-remote-data.jsx

--- packages/dx-react-demos/src/bootstrap3/featured-remote-data.jsx
+++ packages/dx-react-demos/src/bootstrap3/featured-remote-data.jsx
@@ -8,7 +8,8 @@ export const FeaturedRemoteDataDemos = () => (
     <p>
       This demo shows the DataGrid capability to request specific data from the server.
       Properties received from the server and used in the query construction
-      are managed by the parent component. You can display your own loading indicator during the server request processing.
+      are managed by the parent component. You can display your own loading
+      indicator during the server request processing.
     </p>
     <RemoteDataDemo />
     <a href="https://github.com/DevExpress/devextreme-reactive/blob/master/packages/dx-react-demos/src/bootstrap3/featured/remote-data.jsx">

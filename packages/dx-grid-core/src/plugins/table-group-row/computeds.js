05ff5ed83517d60750b0257a315d79963e3f2042
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-05-23 18:12:43 +0300

    table-group-row computed refactoring

--- packages/dx-grid-core/src/plugins/table-group-row/computeds.js
+++ packages/dx-grid-core/src/plugins/table-group-row/computeds.js
@@ -3,7 +3,5 @@ export const tableColumnsWithGroups = (columns, grouping) => [
   ...columns,
 ];
 
-export const tableColumnsWithoutGroups = (columns, grouping) => [
-  ...columns.filter(column =>
-  grouping.findIndex(g => g.columnName === column.name) === -1),
-];
+export const tableColumnsWithoutGroups = (columns, grouping) => columns.filter(column =>
+  grouping.findIndex(g => g.columnName === column.name) === -1);

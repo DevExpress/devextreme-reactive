62bbf43fa5f86afd6674f3e67c1c232c98bb078c
bykov <dxbykov@users.noreply.github.com>
2017-05-24 11:16:54 +0300

    Extract common table utils

--- packages/dx-react-grid-material-ui/src/templates/table.jsx
+++ packages/dx-react-grid-material-ui/src/templates/table.jsx
@@ -5,63 +5,16 @@ import {
     Table as TableMUI,
     TableBody as TableBodyMUI,
     TableHead as TableHeadMUI,
-    TableRow as TableRowMUI,
 } from 'material-ui';
 
-import { querySelectorAll } from '@devexpress/dx-react-grid';
+import {
+  getKeyGetter,
+  tableCellClickHandler,
+} from '@devexpress/dx-react-grid';
+
+import { TableRow } from './table-row';
 
 const MINIMAL_COLUMN_WIDTH = 120;
-
-const getKeyGetter = getIntrinsicKey => (object, index) => {
-  const intrinsicKey = getIntrinsicKey(object);
-  const type = object.type || 'data';
-  const key = intrinsicKey === undefined ? `$${index}` : intrinsicKey;
-  return `${type}_${key}`;
-};
-
-const getCellInfo = ({ row, columnIndex, columns }) => {
-  if (row.colspan !== undefined && columnIndex > row.colspan) { return { skip: true }; }
-  const colspan = row.colspan === columnIndex ? columns.length - row.colspan : 1;
-  return { colspan };
-};
-
-export const TableRow = (props) => {
-  const { row, columns, cellTemplate } = props;
-  const TableCell = cellTemplate;
-  const columnKeyGetter = getKeyGetter(column => column.name);
-
-  const style = (!row.height || row.height === 'auto') ? null : { height: `${row.height}px` };
-  return (
-    <TableRowMUI
-      selected={row.selected}
-      style={style}
-    >
-      {columns.map((column, columnIndex) => {
-        const info = getCellInfo({ row, column, columnIndex, columns });
-        if (info.skip) return null;
-        return (
-          <TableCell
-            key={columnKeyGetter(column, columnIndex)}
-            row={row}
-            column={column}
-            colspan={info.colspan}
-            style={column.width ? { width: column.width } : null}
-          />
-        );
-      })}
-    </TableRowMUI>
-  );
-};
-
-TableRow.propTypes = {
-  row: PropTypes.object.isRequired,
-  columns: PropTypes.array.isRequired,
-  cellTemplate: PropTypes.func.isRequired,
-};
-
-TableRow.defaultProps = {
-  isHeader: false,
-};
 
 export const Table = ({
   headerRows, bodyRows, columns, cellTemplate, onClick, getRowId,
@@ -82,20 +35,7 @@ export const Table = ({
         style={{
           minWidth: `${minWidth}px`,
         }}
-        onClick={(e) => {
-          const { target, currentTarget } = e;
-
-          const rowsEls = querySelectorAll(currentTarget, ':scope > thead > tr, :scope > tbody > tr');
-          const rowIndex = [...rowsEls].findIndex(rowEl => rowEl.contains(target));
-          if (rowIndex === -1) return;
-          const cellEls = querySelectorAll(rowsEls[rowIndex], ':scope > th, :scope > td');
-          const columnIndex = [...cellEls].findIndex(cellEl => cellEl.contains(target));
-          if (columnIndex === -1) return;
-
-          const row = [...headerRows, ...bodyRows][rowIndex];
-          const column = columns[columnIndex];
-          onClick({ row, column, e });
-        }}
+        onClick={tableCellClickHandler({ headerRows, bodyRows, columns, onClick })}
       >
         <TableHeadMUI>
           {headerRows.map((row, rowIndex) => (

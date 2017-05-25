4f516934928c9d6e83af18250c6c699a1a3627cb
bykov <dxbykov@users.noreply.github.com>
2017-05-25 17:07:26 +0300

    Improve the Grid Remote Data demo description

--- packages/dx-react-demos/src/bootstrap3/featured-remote-data.jsx
+++ packages/dx-react-demos/src/bootstrap3/featured-remote-data.jsx
@@ -6,10 +6,11 @@ export const FeaturedRemoteDataDemos = () => (
   <div>
     <h1>Grid Remote Data</h1>
     <p>
-      This demo shows the Grid capability to request specific data from the server.
-      Properties received from the server and used in the query construction
-      are managed by the parent component. You can display your own loading
-      indicator during the server request processing.
+      This demo shows the Grid capability to be bound to remote data.
+      To perform a parametrized HTTP request to a web-service, you can handle
+      the Grid state change events and use the state to construct a query that the
+      data service can understand and respond with a corresponding set of rows.
+      You can display your own loading indicator during the server request processing.
     </p>
     <RemoteDataDemo />
     <a href="https://github.com/DevExpress/devextreme-reactive/blob/master/packages/dx-react-demos/src/bootstrap3/featured/remote-data.jsx">

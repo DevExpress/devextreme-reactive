80dbe57530d2ad50b140691fad4cd2f3990dc70d
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-05-25 10:07:53 +0300

    Resolve conflicts

--- packages/dx-react-grid-material-ui/src/templates/pager.jsx
+++ packages/dx-react-grid-material-ui/src/templates/pager.jsx
@@ -1,52 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-  Button,
-} from 'material-ui';
+  Pagination,
+} from './pagination';
 
-const PageButton = ({ text, isActive, onClick }) => (
-  <Button
-    style={{ minWidth: '16px' }}
-    accent={isActive}
-    raised={isActive}
-    onTouchTap={onClick}
-  >
-    {text}
-  </Button>
-);
-
-PageButton.propTypes = {
-  text: PropTypes.string.isRequired,
-  isActive: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired,
-};
-
-const Pagination = ({ totalPages, currentPage, onCurrentPageChange }) => {
-  const pages = [];
-
-  for (let i = 0; i < totalPages; i += 1) {
-    pages.push(i);
-  }
-
-  return (
-    <div>
-      {pages.map(page => (
-        <PageButton
-          key={page}
-          text={String(page + 1)}
-          isActive={page === currentPage}
-          onClick={() => onCurrentPageChange(page)}
-        />
-      ))}
-    </div>
-  );
-};
-
-Pagination.propTypes = {
-  totalPages: PropTypes.number.isRequired,
-  currentPage: PropTypes.number.isRequired,
-  onCurrentPageChange: PropTypes.func.isRequired,
-};
 
 export const Pager = ({ currentPage, onCurrentPageChange, totalPages }) => (
   <Pagination
@@ -55,8 +12,8 @@ export const Pager = ({ currentPage, onCurrentPageChange, totalPages }) => (
       verticalAlign: 'bottom',
     }}
     totalPages={totalPages}
-    currentPage={currentPage}
-    onCurrentPageChange={page => onCurrentPageChange(page)}
+    currentPage={currentPage + 1}
+    onCurrentPageChange={page => onCurrentPageChange(page - 1)}
   />
 );
 
---
+++ packages/dx-react-grid-material-ui/src/templates/pagination.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Button,
+} from 'material-ui';
+
+const PageButton = ({ text, isActive, isDisabled, onClick }) => (
+  <Button
+    style={{ minWidth: '16px' }}
+    accent={isActive}
+    raised={isActive}
+    disabled={isDisabled}
+    onTouchTap={onClick}
+  >
+    {text}
+  </Button>
+);
+
+PageButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  isActive: PropTypes.bool,
+  isDisabled: PropTypes.bool,
+  onClick: PropTypes.func,
+};
+
+PageButton.defaultProps = {
+  onClick: () => {},
+  isDisabled: false,
+  isActive: false,
+};
+
+const ellipsisSymbol = '\u2026';
+
+const calculateStartPage = (currentPage, maxButtonCount, totalPageCount) => Math.max(
+  Math.min(
+    currentPage - Math.floor(maxButtonCount / 2, 10), (totalPageCount - maxButtonCount) + 1,
+  ),
+  1,
+);
+
+const renderPageButtons = (
+  currentPage,
+  totalPageCount,
+  onCurrentPageChange,
+  ) => {
+  const pageButtons = [];
+  const maxButtonCount = 5;
+  let startPage = 1;
+  let endPage = totalPageCount;
+
+  if (maxButtonCount < totalPageCount) {
+    startPage = calculateStartPage(currentPage, maxButtonCount, totalPageCount);
+    endPage = (startPage + maxButtonCount) - 1;
+  }
+
+  if (startPage > 1) {
+    pageButtons.push(
+      <PageButton
+        key={1}
+        text={String(1)}
+        onClick={() => onCurrentPageChange(1)}
+      />,
+    );
+
+    if (startPage > 2) {
+      pageButtons.push(
+        <PageButton
+          key={'ellipsisStart'}
+          text={ellipsisSymbol}
+          isDisabled
+        />,
+      );
+    }
+  }
+
+  for (let page = startPage; page <= endPage; page += 1) {
+    pageButtons.push(
+      <PageButton
+        key={page}
+        text={String(page)}
+        isActive={page === currentPage}
+        onClick={() => onCurrentPageChange(page)}
+      />,
+    );
+  }
+
+  if (endPage < totalPageCount) {
+    if (endPage < totalPageCount - 1) {
+      pageButtons.push(
+        <PageButton
+          key={'ellipsisEnd'}
+          text={ellipsisSymbol}
+          isDisabled
+        />,
+      );
+    }
+
+    pageButtons.push(
+      <PageButton
+        key={totalPageCount}
+        text={String(totalPageCount)}
+        onClick={() => onCurrentPageChange(totalPageCount)}
+      />,
+    );
+  }
+
+  return pageButtons;
+};
+
+export const Pagination = ({ totalPages, currentPage, onCurrentPageChange }) => (
+  <div>
+    {renderPageButtons(currentPage, totalPages, onCurrentPageChange)}
+  </div>
+);
+
+Pagination.propTypes = {
+  totalPages: PropTypes.number.isRequired,
+  currentPage: PropTypes.number.isRequired,
+  onCurrentPageChange: PropTypes.func.isRequired,
+};

cf3c5a424e149e36a789de21d76dbb1ff67365b5
SergeyAlexeev <SergeyAlexeev@users.noreply.github.com>
2017-06-02 15:43:18 +0300

    Clean up table detail row (#119)

--- packages/dx-react-grid/src/plugins/table-row-detail.jsx
+++ packages/dx-react-grid/src/plugins/table-row-detail.jsx
@@ -1,28 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Getter, Template, PluginContainer } from '@devexpress/dx-react-core';
-import { setDetailRowExpanded, expandedDetailRows, isDetailRowExpanded, tableColumnsWithDetail } from '@devexpress/dx-grid-core';
+import { expandedDetailRows, isDetailRowExpanded, tableColumnsWithDetail } from '@devexpress/dx-grid-core';
 
 export class TableRowDetail extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      expandedRows: props.defaultExpandedRows || [],
-    };
-
-    this._setDetailRowExpanded = ({ rowId }) => {
-      const prevExpandedDetails = this.props.expandedRows || this.state.expandedRows;
-      const expandedRows = setDetailRowExpanded(prevExpandedDetails, { rowId });
-      const { onExpandedRowsChange } = this.props;
-      this.setState({ expandedRows });
-      if (onExpandedRowsChange) {
-        onExpandedRowsChange(expandedRows);
-      }
-    };
-  }
   render() {
-    const expandedRows = this.props.expandedRows || this.state.expandedRows;
     const { rowHeight, template, detailToggleTemplate, detailCellTemplate } = this.props;
 
     return (
@@ -39,13 +21,23 @@ export class TableRowDetail extends React.PureComponent {
           predicate={({ column, row }) => column.type === 'detail' && !row.type}
           connectGetters={getter => ({
             getRowId: getter('getRowId'),
+            expandedRows: getter('expandedRows'),
+          })}
+          connectActions={action => ({
+            setDetailRowExpanded: action('setDetailRowExpanded'),
           })}
         >
-          {({ row, getRowId, ...restParams }) => detailToggleTemplate({
-            ...restParams,
-            expanded: isDetailRowExpanded(expandedRows, getRowId(row)),
-            toggleExpanded: () => this._setDetailRowExpanded({ rowId: getRowId(row) }),
-          })}
+          {({
+              row,
+              getRowId,
+              expandedRows,
+              setDetailRowExpanded,
+              ...restParams
+            }) => detailToggleTemplate({
+              ...restParams,
+              expanded: isDetailRowExpanded(expandedRows, getRowId(row)),
+              toggleExpanded: () => setDetailRowExpanded({ rowId: getRowId(row) }),
+            })}
         </Template>
 
         <Getter
@@ -53,7 +45,7 @@ export class TableRowDetail extends React.PureComponent {
           pureComputed={expandedDetailRows}
           connectArgs={getter => [
             getter('tableBodyRows'),
-            expandedRows,
+            getter('expandedRows'),
             getter('getRowId'),
             rowHeight,
           ]}
@@ -71,9 +63,6 @@ export class TableRowDetail extends React.PureComponent {
 }
 
 TableRowDetail.propTypes = {
-  expandedRows: PropTypes.array,
-  defaultExpandedRows: PropTypes.array,
-  onExpandedRowsChange: PropTypes.func,
   template: PropTypes.func,
   detailToggleTemplate: PropTypes.func.isRequired,
   detailCellTemplate: PropTypes.func.isRequired,
@@ -84,9 +73,6 @@ TableRowDetail.propTypes = {
 };
 
 TableRowDetail.defaultProps = {
-  expandedRows: undefined,
-  defaultExpandedRows: undefined,
-  onExpandedRowsChange: undefined,
   rowHeight: 'auto',
   template: undefined,
 };

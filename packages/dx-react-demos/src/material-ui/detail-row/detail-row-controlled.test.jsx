5dd210996f66c5e1303c08d15e5099cd56c7b217
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-06-02 09:55:19 +0300

    Tests

---
+++ packages/dx-react-demos/src/material-ui/detail-row/detail-row-controlled.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import { DetailRowControlledDemo } from './detail-row-controlled';
+
+describe('MUI: detail row controlled demo', () => {
+  test('should work', () => {
+    mount(
+      <MuiThemeProvider theme={createMuiTheme()}>
+        <DetailRowControlledDemo />
+      </MuiThemeProvider>,
+    );
+
+    expect(true).toBeTruthy();
+  });
+});

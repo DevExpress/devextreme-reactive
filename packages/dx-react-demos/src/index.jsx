9d3a11969be1a482a6d59884af5eef677d294804
Alexander Bykov <dxbykov@users.noreply.github.com>
2017-06-09 09:50:08 +0300

    chore(demos): Use bootstrap 3 demos as default (#127)

--- packages/dx-react-demos/src/index.jsx
+++ packages/dx-react-demos/src/index.jsx
@@ -18,9 +18,17 @@ import './index.css';
 const App = () => (
   <Router>
     <div>
-      <Route exact path="/" render={() => <Redirect push from="/" to="/bootstrap3" />} />
       <Route path="/bootstrap3" component={Bootstrap3Demos} />
       <Route path="/material-ui" component={MaterialUIDemos} />
+      <Route
+        path="/"
+        render={({ location }) => (
+          (location.pathname.startsWith('/bootstrap3')
+            || location.pathname.startsWith('/material-ui'))
+          ? null
+          : <Redirect push from={location.pathname} to={`/bootstrap3${location.pathname}`} />
+        )}
+      />
     </div>
   </Router>
 );

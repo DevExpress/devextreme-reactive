414fe851b28d8235d18cfb911aa86a060216c9a2
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-06-14 16:14:39 +0300

    Do not include setupConsole in production

--- packages/dx-core/rollup.config.js
+++ packages/dx-core/rollup.config.js
@@ -1,5 +1,6 @@
 import babel from 'rollup-plugin-babel';
 import license from 'rollup-plugin-license';
+import replace from 'rollup-plugin-replace';
 import { banner, external, babelrc, moduleName, globals } from '../../tools/rollup-utils';
 
 export default {
@@ -27,6 +28,10 @@ export default {
     license({
       sourceMap: true,
       banner,
+    }),
+    replace({
+      exclude: 'node_modules/**',
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
   ],
 };
--- packages/dx-core/src/utils/testing.js
+++ packages/dx-core/src/utils/testing.js
@@ -1,27 +1,33 @@
 export const setupConsole = (config = {}) => {
-  /* eslint-disable no-console */
-  const savedConsoleWarn = console.warn;
-  const savedConsoleError = console.error;
+  /* eslint-disable no-undef */
+  if (ENV !== 'production') {
+    /* eslint-disable no-console */
+    const savedConsoleWarn = console.warn;
+    const savedConsoleError = console.error;
 
-  const logToError = (...args) => {
-    const formatOutput = config.formatOutput;
-    const errorMessage = args[0];
+    const logToError = (...args) => {
+      const formatOutput = config.formatOutput;
+      const errorMessage = args[0];
 
-    if (!config.ignore || !config.ignore.filter(message => errorMessage.includes(message)).length) {
-      throw new Error(
-        formatOutput
-        ? formatOutput(...args).replace(/^Error: (?:Warning: )?/, '')
-        : errorMessage,
-      );
-    }
-  };
+      if (!config.ignore
+      ||
+      !config.ignore.filter(message => errorMessage.includes(message)).length) {
+        throw new Error(
+          formatOutput
+          ? formatOutput(...args).replace(/^Error: (?:Warning: )?/, '')
+          : errorMessage,
+        );
+      }
+    };
 
-  console.warn = logToError;
-  console.error = logToError;
+    console.warn = logToError;
+    console.error = logToError;
 
-  return () => {
-    console.warn = savedConsoleWarn;
-    console.error = savedConsoleError;
-  };
-  /* eslint-enable no-console */
+    return () => {
+      console.warn = savedConsoleWarn;
+      console.error = savedConsoleError;
+    };
+    /* eslint-enable no-console */
+  }
+  return () => {};
 };

668e128a568afa43ed9e234466fdc950e29e1f59
bykov <dxbykov@users.noreply.github.com>
2017-06-26 15:11:16 +0300

    Switch from Jekyll plugin to gulp task for demo embedding

--- gulpfile.js
+++ gulpfile.js
@@ -6,14 +6,6 @@ var gulp = require('gulp'),
 
 var distPath = 'site/';
 
-gulp.task('site:clean', function() {
-  return gulp.src(
-      ['react'].map(function(dir) { return distPath + dir; }),
-      { read: false }
-    )
-    .pipe(clean());
-});
-
 var splitNameToPath = function(path) {
   // dx-react-grid-bs3\... ==> react\grid\bs3\...
   return path
@@ -28,6 +20,13 @@ var extractMDTitle = function(content) {
 
 var formatFrontMatter = function(title) {
   return `---${title ? '\ntitle: ' + title: ''}\n---\n\n`;
+};
+
+var addFrontMatter = function(content) {
+    var title = extractMDTitle(content),
+        frontMatter = formatFrontMatter(title);
+
+    return frontMatter + content;
 };
 
 var patchMDLinks = function(content) {
@@ -58,6 +57,61 @@ var patchMDTables = function(content) {
   return lines.join('\n');
 };
 
+var applyInterceptors = function(content, ...interceptors) {
+  return interceptors.reduce((result, interceptor) => {
+    return interceptor(result);
+  }, content);
+};
+
+var injectLiveDemos = function(content) {
+  var demoDef = '';
+  var demoDefRegex = /(.+?):\n\[Demo\]\(([^\s]*)\)\s\|\s+\[Source\]\(([^\s]*)\)\n\n/g;
+  var demoBlockRegex = new RegExp(`(${demoDefRegex.source})+`, 'g');
+
+  return content.replace(demoBlockRegex, (demoBlock) => {
+    var items = [];
+    var match;
+    while(match = demoDefRegex.exec(demoBlock)) {
+      items.push({
+        title: match[1],
+        url: match[2],
+        demoSrc: match[3],
+      });
+    }
+    if (items.length === 1) {
+      var path = items[0].url.replace(/\/demos\//, '/embedded-demo/');
+      return `<p>
+          <iframe class=\"embedded-demo\" scrolling=\"no\" src=\"${path}\"></iframe>
+        </p>`;
+    }
+    else {
+      var path = items[0].url.replace(/\/demos\//, '/embedded-demo/');
+      return `<p>
+          <div class=\"dropdown template-chooser\">
+            <a class=\"dropdown-toggle\" type=\"button\" data-toggle=\"dropdown\">
+              <span class=\"caption\">Bootstrap 3</span>
+              <span class=\"caret\"></span>
+            </a>
+            <ul class=\"dropdown-menu\">
+              <li><a tabindex=\"-1\" data-value=\"bootstrap3\">Bootstrap 3</a></li>
+              <li><a tabindex=\"-1\" data-value=\"material-ui\">Material UI</a></li>
+            </ul>
+          </div>
+          <iframe class=\"embedded-demo\" scrolling=\"no\" src=\"${path}\"></iframe>
+        </p>`;
+    }
+
+  });
+};
+
+gulp.task('site:clean', function() {
+  return gulp.src(
+      ['react'].map(function(dir) { return distPath + dir; }),
+      { read: false }
+    )
+    .pipe(clean());
+});
+
 gulp.task('site:docs', function() {
   return gulp.src([
       'packages/**/*.md',
@@ -71,13 +125,14 @@ gulp.task('site:docs', function() {
         .replace(/readme/i, 'index');
     }))
     .pipe(intercept(function(file){
-      var content = file.contents.toString(),
-          title = extractMDTitle(content),
-          frontMatter = formatFrontMatter(title);
-
-      content = frontMatter + patchMDTables(patchMDLinks(content));
+      var content = applyInterceptors(
+        file.contents.toString(),
+        patchMDLinks,
+        patchMDTables,
+        injectLiveDemos,
+        addFrontMatter
+      );
       file.contents = new Buffer(content);
-
       return file;
     }))
     .pipe(gulp.dest(distPath));

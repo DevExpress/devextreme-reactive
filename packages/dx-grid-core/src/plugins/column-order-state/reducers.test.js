be5d5bda2df57bbf2bbceb6f9fddfaa71a5bd6bc
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-07-05 12:35:31 +0300

    Tests refactoring

--- packages/dx-grid-core/src/plugins/column-order-state/reducers.test.js
+++ packages/dx-grid-core/src/plugins/column-order-state/reducers.test.js
@@ -8,16 +8,15 @@ describe('ColumnOrderState reducers', () => {
   describe('#setColumnOrder', () => {
     const order = ['a', 'b', 'c'];
     const payload = { sourceColumnName: 'a', targetColumnName: 'b' };
-    const setUpOrder = useImmutable => (useImmutable ? Immutable(order) : order);
 
     it('should work', () => {
-      const nextOrder = setColumnOrder(setUpOrder(), payload);
+      const nextOrder = setColumnOrder(order, payload);
 
       expect(nextOrder).toEqual(['b', 'a', 'c']);
     });
 
     it('should work with immutable order', () => {
-      const nextOrder = setColumnOrder(setUpOrder(true), payload);
+      const nextOrder = setColumnOrder(Immutable(order), payload);
 
       expect(nextOrder).toEqual(['b', 'a', 'c']);
     });

d64ec5d2897e5680e3b01f3381c2fcce9fb68b3b
Yury Orlov <mpcteam.kvet@gmail.com>
2017-07-14 09:39:04 +0300

    prepare demos to be emmeded multiple times on one page

--- packages/dx-react-demos/src/index.jsx
+++ packages/dx-react-demos/src/index.jsx
@@ -4,8 +4,10 @@ import 'whatwg-fetch';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 import {
-  HashRouter as Router,
+  HashRouter,
+  MemoryRouter,
   Route,
   Redirect,
 } from 'react-router-dom';
@@ -15,25 +17,47 @@ import { MaterialUIDemos } from './material-ui';
 
 import './index.css';
 
-const App = () => (
-  <Router>
-    <div>
-      <Route path="/bootstrap3" component={Bootstrap3Demos} />
-      <Route path="/material-ui" component={MaterialUIDemos} />
-      <Route
-        path="/"
-        render={({ location }) => (
-          (location.pathname.startsWith('/bootstrap3')
-            || location.pathname.startsWith('/material-ui'))
-          ? null
-          : <Redirect push from={location.pathname} to={`/bootstrap3${location.pathname}`} />
-        )}
-      />
-    </div>
-  </Router>
-);
+const App = ({ router }) => {
+  const Router = router === 'hash' ? HashRouter : MemoryRouter;
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app'),
-);
+  return (
+    <Router>
+      <div>
+        <Route path="/bootstrap3" component={Bootstrap3Demos} />
+        <Route path="/material-ui" component={MaterialUIDemos} />
+        <Route
+          path="/"
+          render={({ location }) => (
+            (location.pathname.startsWith('/bootstrap3')
+              || location.pathname.startsWith('/material-ui'))
+            ? null
+            : <Redirect push from={location.pathname} to={`/bootstrap3${location.pathname}`} />
+          )}
+        />
+      </div>
+    </Router>
+  );
+};
+
+App.propTypes = {
+  router: PropTypes.string,
+};
+
+App.defaultProps = {
+  router: 'memory',
+};
+
+
+const embeddedDemoPlaceholders = document.getElementsByClassName('embedded-demo');
+const embeddedDemoConfigs = [...embeddedDemoPlaceholders]
+  .map(placeholder => ({
+    options: JSON.parse(placeholder.getAttribute('data-options') || '{}'),
+    placeholder,
+  }));
+embeddedDemoConfigs
+  .forEach((config) => {
+    ReactDOM.render(
+      <App {...config.options} />,
+      config.placeholder,
+    );
+  });

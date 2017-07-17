af26c64d41bb077c40cd557e4c673edd4abbf86b
Yury Orlov <mpcteam.kvet@gmail.com>
2017-07-17 11:09:40 +0300

    fix(react-grid): turn edit cell input into the controlled mode (#201)

--- packages/dx-react-demos/src/bootstrap3/editing/edit-row-controlled.jsx
+++ packages/dx-react-demos/src/bootstrap3/editing/edit-row-controlled.jsx
@@ -21,7 +21,7 @@ export class EditRowControlledDemo extends React.PureComponent {
 
     this.state = {
       columns: [
-        { name: 'id', title: 'ID', width: 50 },
+        { name: 'id', title: 'ID', width: 60 },
         { name: 'name', title: 'Name' },
         { name: 'sex', title: 'Sex' },
         { name: 'city', title: 'City' },

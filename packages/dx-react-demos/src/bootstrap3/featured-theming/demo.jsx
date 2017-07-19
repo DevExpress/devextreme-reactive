f387066d815e21a8ba49c36f8543dba928b071e1
Yury Orlov <mpcteam.kvet@gmail.com>
2017-07-19 13:02:06 +0300

    update demo

--- packages/dx-react-demos/src/bootstrap3/featured-theming/demo.jsx
+++ packages/dx-react-demos/src/bootstrap3/featured-theming/demo.jsx
@@ -1,40 +1,152 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {
-  SortingState, SelectionState, FilteringState, PagingState, GroupingState,
-  LocalFiltering, LocalGrouping, LocalPaging, LocalSorting,
-  ColumnOrderState,
+  SortingState, SelectionState, PagingState, GroupingState,
+  LocalGrouping, LocalPaging, LocalSorting,
+  ColumnOrderState, RowDetailState,
 } from '@devexpress/dx-react-grid';
 import {
   Grid,
-  TableView, TableHeaderRow, TableFilterRow, TableSelection, TableGroupRow,
-  PagingPanel, GroupingPanel, DragDropContext,
+  TableView, TableHeaderRow, TableSelection, TableGroupRow,
+  PagingPanel, GroupingPanel, DragDropContext, TableRowDetail,
 } from '@devexpress/dx-react-grid-bootstrap3';
-import {
-  ProgressBarCell,
-} from '../templates/progress-bar-cell';
-import {
-  HighlightedCell,
-} from '../templates/highlighted-cell';
+import { Nav, NavItem, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 import {
   generateRows,
-  globalSalesValues,
+  employeeValues,
+  employeeTaskValues,
 } from '../../demo-data/generator';
 
+
+const TaskIcon = ({ status }) => {
+  switch (status) {
+    case 'Deferred': return <i className="glyphicon glyphicon-time" />;
+    case 'In Progress': return <i className="glyphicon glyphicon-cog" />;
+    case 'Need Assistance': return <i className="glyphicon glyphicon-earphone" />;
+    default: return <i className="glyphicon glyphicon-ok" />;
+  }
+};
+
+TaskIcon.propTypes = {
+  status: PropTypes.string.isRequired,
+};
+
+const TabContainer = ({ rows }) => (
+  <ListGroup>
+    {rows.map((item, index) => {
+      const key = index;
+      const status = item.status;
+      return (
+        <ListGroupItem key={key}>
+          <span style={{ float: 'right' }}>
+            <TaskIcon status={status} />
+          </span>
+          {item.subject}
+        </ListGroupItem>
+      );
+    })}
+  </ListGroup>
+);
+
+TabContainer.propTypes = {
+  rows: PropTypes.array.isRequired,
+};
+
+class GridDetailContainer extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lowPriorityTasks: this.findTasks('Low'),
+      normalPriorityTasks: this.findTasks('Normal'),
+      highPriorityTasks: this.findTasks('High'),
+    };
+    this.state.index = this.firstSelectedIndex();
+    this.handleChange = this.handleChange.bind(this);
+  }
+  findTasks(priority) {
+    return this.props.data.tasks.filter(task => task.priority === priority);
+  }
+  firstSelectedIndex() {
+    let result = 0;
+    const { lowPriorityTasks, normalPriorityTasks } = this.state;
+
+    if (!lowPriorityTasks.length) {
+      result = normalPriorityTasks.length ? 1 : 2;
+    }
+
+    return result;
+  }
+  handleChange(event, index) {
+    this.setState({ index });
+  }
+  render() {
+    const { lowPriorityTasks, normalPriorityTasks, highPriorityTasks, index } = this.state;
+    const { data } = this.props;
+
+    return (
+      <div
+        style={{ margin: '20px' }}
+      >
+        <h4
+          style={{ marginBottom: '20px' }}
+        >
+          {data.firstName} {data.lastName}&apos;s Tasks by Priority:
+        </h4>
+
+        <Nav
+          bsStyle="pills"
+          activeKey={index}
+          onSelect={newIndex => this.setState({ index: newIndex })}
+          style={{ marginBottom: '20px' }}
+        >
+          <NavItem eventKey={0} title="Low" disabled={!lowPriorityTasks.length}>
+            {`Low (${lowPriorityTasks.length})`}
+          </NavItem>
+          <NavItem eventKey={1} title="Normal" disabled={!normalPriorityTasks.length}>
+            {`Normal (${normalPriorityTasks.length})`}
+          </NavItem>
+          <NavItem eventKey={2} title="High" disabled={!highPriorityTasks.length}>
+            {`High (${highPriorityTasks.length})`}
+          </NavItem>
+        </Nav>
+        {index === 0 && <TabContainer rows={lowPriorityTasks} />}
+        {index === 1 && <TabContainer rows={normalPriorityTasks} />}
+        {index === 2 && <TabContainer rows={highPriorityTasks} />}
+      </div>
+    );
+  }
+}
+
+GridDetailContainer.propTypes = {
+  data: PropTypes.shape().isRequired,
+};
+
+// eslint-disable-next-line
 export default class Demo extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
       columns: [
-        { name: 'product', title: 'Product' },
-        { name: 'region', title: 'Region' },
-        { name: 'amount', title: 'Sale Amount', align: 'right' },
-        { name: 'discount', title: 'Discount' },
-        { name: 'saleDate', title: 'Sale Date' },
-        { name: 'customer', title: 'Customer' },
+        { name: 'prefix', title: 'Title', width: 100 },
+        { name: 'firstName', title: 'First Name' },
+        { name: 'lastName', title: 'Last Name' },
+        { name: 'position', title: 'Position', width: 170 },
+        { name: 'state', title: 'State', width: 125 },
       ],
-      rows: generateRows({ columnValues: globalSalesValues, length: 1000 }),
+      rows: generateRows({
+        columnValues: {
+          ...employeeValues,
+          tasks: ({ random }) => generateRows({
+            columnValues: employeeTaskValues,
+            length: Math.floor(random() * 4) + 5,
+            random,
+          }),
+        },
+        length: 40,
+      }),
       allowedPageSizes: [5, 10, 15],
     };
   }
@@ -48,25 +160,20 @@ export default class Demo extends React.PureComponent {
       >
         <ColumnOrderState defaultOrder={columns.map(column => column.name)} />
 
-        <FilteringState
-          defaultFilters={[{ columnName: 'saleDate', value: '2016-02' }]}
-        />
         <SortingState
           defaultSorting={[
             { columnName: 'product', direction: 'asc' },
-            { columnName: 'saleDate', direction: 'asc' },
           ]}
         />
-        <GroupingState
-          defaultGrouping={[{ columnName: 'product' }]}
-          defaultExpandedGroups={['EnviroCare Max']}
-        />
+        <GroupingState />
         <PagingState
           defaultCurrentPage={0}
           defaultPageSize={10}
         />
+        <RowDetailState
+          defaultExpandedRows={[2]}
+        />
 
-        <LocalFiltering />
         <LocalSorting />
         <LocalGrouping />
         <LocalPaging />
@@ -77,31 +184,22 @@ export default class Demo extends React.PureComponent {
 
         <DragDropContext />
 
-        <TableView
-          tableCellTemplate={({ row, column, style }) => {
-            if (column.name === 'discount') {
-              return (
-                <ProgressBarCell value={row.discount * 100} style={style} />
-              );
-            } else if (column.name === 'amount') {
-              return (
-                <HighlightedCell align={column.align} value={row.amount} style={style} />
-              );
-            }
-            return undefined;
-          }}
-          allowColumnReordering
-        />
+        <TableView allowColumnReordering />
 
         <TableHeaderRow allowSorting allowGrouping allowDragging />
-        <TableFilterRow />
         <PagingPanel
           allowedPageSizes={allowedPageSizes}
         />
         <TableSelection />
+        <TableRowDetail
+          template={({ row }) => (
+            <GridDetailContainer
+              data={row}
+            />
+          )}
+        />
         <TableGroupRow />
         <GroupingPanel allowSorting />
-
       </Grid>
     );
   }

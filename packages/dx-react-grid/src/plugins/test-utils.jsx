7ef1b050c32cd8aa9233d47e5141c2d5f5a62234
Yury Orlov <mpcteam.kvet@gmail.com>
2017-07-31 09:22:45 +0300

    update test util keys

--- packages/dx-react-grid/src/plugins/test-utils.jsx
+++ packages/dx-react-grid/src/plugins/test-utils.jsx
@@ -20,10 +20,10 @@ export const pluginDepsToComponents = (
     {entries({ ...deps.getter, ...depsOverrides.getter })
       .map(([name, value]) => <Getter key={`getter_${name}`} name={name} value={value} />)}
     {entries({ ...deps.action, ...depsOverrides.action })
-      .map(([name, action]) => <Action key={`getter_${name}`} name={name} action={action} />)}
+      .map(([name, action]) => <Action key={`action_${name}`} name={name} action={action} />)}
     {entries({ ...deps.template, ...depsOverrides.template })
       .map(([name, templateParams]) => (
-        <Template key={`getter_${name}`} name="root">
+        <Template key={`template_${name}`} name="root">
           <div>
             <TemplatePlaceholder name={name} params={templateParams} />
             <TemplatePlaceholder />

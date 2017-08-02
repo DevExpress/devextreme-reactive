c45ef850c45337b8bb841023f183e6d26caf922a
Yury Orlov <mpcteam.kvet@gmail.com>
2017-08-02 19:12:28 +0300

    fix(react-grid): pass style field to editCellTemplate of TableEditRow (#235)
    Fixes #234

--- packages/dx-react-grid-bootstrap3/src/templates/table-edit-cell.jsx
+++ packages/dx-react-grid-bootstrap3/src/templates/table-edit-cell.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const EditCell = ({ column, value, onValueChange }) => (
+export const EditCell = ({ column, value, onValueChange, style }) => (
   <td
     style={{
       verticalAlign: 'middle',
       padding: '1px',
+      ...style,
     }}
   >
     <input
       type="text"
       className="form-control"
-      value={value || ''}
+      value={value}
       onChange={e => onValueChange(e.target.value)}
       style={{ width: '100%', textAlign: column.align }}
     />
   </td>
 );
 EditCell.propTypes = {
-  column: PropTypes.object.isRequired,
+  column: PropTypes.object,
   value: PropTypes.any,
   onValueChange: PropTypes.func.isRequired,
+  style: PropTypes.object,
 };
 EditCell.defaultProps = {
-  value: undefined,
+  column: {},
+  value: '',
+  style: {},
 };

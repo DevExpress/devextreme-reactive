c8bb7e6cd941bb0238c4f4464aac0fdfb4bb0cf0
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-08-18 11:35:16 +0300

    Refactor filtering computed

--- packages/dx-grid-core/src/plugins/filtering-state/computeds.js
+++ packages/dx-grid-core/src/plugins/filtering-state/computeds.js
@@ -1,16 +1,15 @@
 const toString = value => String(value).toLowerCase();
 
-const applyRowFilter = (row, filter, getCellData) => {
-  const value = getCellData(row, filter.columnName);
+const applyFilter = (filter, value) => (toString(value).indexOf(toString(filter.value)) > -1);
 
-  return toString(value).indexOf(toString(filter.value)) > -1;
-};
-
-export const filteredRows = (rows, filters, getCellData, filterFn = applyRowFilter) => {
+export const filteredRows = (rows, filters, getCellData, filterFn) => {
   if (!filters.length) return rows;
 
   return rows.filter(
     row => filters.reduce(
       (accumulator, filter) =>
-        accumulator && filterFn(row, filter, getCellData), true));
+        accumulator && (
+          filterFn ?
+            filterFn(row, filter) :
+            applyFilter(filter, getCellData(row, filter.columnName))), true));
 };

cca7265a47991766aafde39be79a9c4470eb3cbb
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-08-18 11:52:42 +0300

    Performance optimization

--- packages/dx-grid-core/src/plugins/filtering-state/computeds.js
+++ packages/dx-grid-core/src/plugins/filtering-state/computeds.js
@@ -2,14 +2,14 @@ const toString = value => String(value).toLowerCase();
 
 const applyFilter = (filter, value) => (toString(value).indexOf(toString(filter.value)) > -1);
 
-export const filteredRows = (rows, filters, getCellData, filterFn) => {
+export const filteredRows = (rows, filters, getCellData, userFilterFn) => {
   if (!filters.length) return rows;
+
+  const filterFn = userFilterFn ||
+    ((row, filter) => applyFilter(filter, getCellData(row, filter.columnName)));
 
   return rows.filter(
     row => filters.reduce(
       (accumulator, filter) =>
-        accumulator && (
-          filterFn ?
-            filterFn(row, filter) :
-            applyFilter(filter, getCellData(row, filter.columnName))), true));
+        accumulator && filterFn(row, filter), true));
 };

c90bd405097dac9fac81c7db6ff74108eb7978ee
George Sobolev <gsobolev@users.noreply.github.com>
2017-08-21 16:02:52 +0300

    Fix grouping panel empty message

--- packages/dx-react-grid-bootstrap3/src/templates/group-panel.jsx
+++ packages/dx-react-grid-bootstrap3/src/templates/group-panel.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { GroupPanelLayout } from '@devexpress/dx-react-grid';
 
-const defaultText = (
-  <span
-    style={{
-      padding: '7px 0',
-      marginBottom: '5px',
-      display: 'inline-block',
-    }}
-  >
-    Drag a column header here to group by that column
-  </span>
+const defaultTextStyle = {
+  padding: '7px 0',
+  marginBottom: '5px',
+  display: 'inline-block',
+};
+
+const getDefaultText = allowDragging => (
+  allowDragging
+  ? (
+    <span style={defaultTextStyle}>
+      Drag a column header here to group by that column
+    </span>
+  )
+  : (
+    <span style={defaultTextStyle}>
+      Click
+      &nbsp;
+      <i
+        className="glyphicon glyphicon-th-list"
+        style={{
+          top: '0',
+          fontSize: '9px',
+        }}
+      />
+      &nbsp;
+      icon in the column header to group by that column
+    </span>
+  )
 );
 
 const PanelTemplate = ({ items }) => <div>{items}</div>;
@@ -25,7 +42,7 @@ const panelTemplate = props => <PanelTemplate {...props} />;
 
 export const GroupPanel = ({ groupByColumnText, ...restProps }) => (
   <GroupPanelLayout
-    groupByColumnText={groupByColumnText || defaultText}
+    groupByColumnText={groupByColumnText || getDefaultText(restProps.allowDragging)}
     panelTemplate={panelTemplate}
     {...restProps}
   />
--- packages/dx-react-grid-material-ui/src/templates/group-panel.jsx
+++ packages/dx-react-grid-material-ui/src/templates/group-panel.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import { withStyles, createStyleSheet } from 'material-ui/styles';
-
+import List from 'material-ui-icons/List';
 import { GroupPanelLayout } from '@devexpress/dx-react-grid';
 
 const styleSheet = createStyleSheet('GroupPanel', theme => ({
@@ -17,16 +16,35 @@ const styleSheet = createStyleSheet('GroupPanel', theme => ({
     display: 'inline-block',
     color: theme.typography.title.color,
   },
+  groupIcon: {
+    display: 'inline-block',
+    verticalAlign: 'middle',
+  },
 }));
 
-const DefaultTextBase = ({ classes }) => (
-  <span className={classes.groupInfo}>
-    Drag a column header here to group by that column
-  </span>
+const DefaultTextBase = ({ classes, allowDragging }) => (
+  allowDragging
+    ? (
+      <span className={classes.groupInfo}>
+        Drag a column header here to group by that column
+      </span>
+    )
+    : (
+      <span className={classes.groupInfo}>
+        Click
+        &nbsp;
+        <span className={classes.groupIcon}>
+          <List />
+        </span>
+        &nbsp;
+        icon in the column header to group by that column
+      </span>
+    )
 );
 
 DefaultTextBase.propTypes = {
   classes: PropTypes.shape().isRequired,
+  allowDragging: PropTypes.bool.isRequired,
 };
 
 const DefaultText = withStyles(styleSheet)(DefaultTextBase);
@@ -49,7 +67,8 @@ const panelTemplate = props => <PanelTemplate {...props} />;
 const GroupPanelBase = ({ groupByColumnText, classes, ...restProps }) => (
   <div className={classes.panel}>
     <GroupPanelLayout
-      groupByColumnText={groupByColumnText || <DefaultText />}
+      groupByColumnText={groupByColumnText
+        || <DefaultText allowDragging={restProps.allowDragging} />}
       panelTemplate={panelTemplate}
       {...restProps}
     />

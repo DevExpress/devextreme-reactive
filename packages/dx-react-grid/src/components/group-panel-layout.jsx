e89e2cd2e5c5a99b9726c24f1273b81c24c9f76e
George Sobolev <gsobolev@users.noreply.github.com>
2017-09-15 17:10:01 +0300

    Update dx-react-grid dependencies

--- packages/dx-react-grid/src/components/group-panel-layout.jsx
+++ packages/dx-react-grid/src/components/group-panel-layout.jsx
@@ -125,7 +125,7 @@ export class GroupPanelLayout extends React.PureComponent {
           >
             {item}
           </div>
-      );
+        );
     });
   }
 
--- packages/dx-react-grid/src/plugins/table-view.jsx
+++ packages/dx-react-grid/src/plugins/table-view.jsx
@@ -107,7 +107,7 @@ export class TableView extends React.PureComponent {
                     params={params}
                   />
                 )
-              )}
+                )}
             </TemplateConnector>
           )}
         </Template>

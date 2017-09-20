39bf7ef5e4a1b0d60d9bfb25b0176c6650580b62
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-09-20 19:03:30 +0300

    Fix lint

--- packages/dx-react-grid-bootstrap3/src/templates/virtual-table/virtual-box.jsx
+++ packages/dx-react-grid-bootstrap3/src/templates/virtual-table/virtual-box.jsx
@@ -185,12 +185,10 @@ export class VirtualBox extends React.Component {
 
     return (
       <RootTag
-        {
-          ...{
-            ...(iref ? { ref: iref } : {}),
-            ...rootTagProps,
-          }
-        }
+        {...{
+          ...(iref ? { ref: iref } : {}),
+          ...rootTagProps,
+        }}
       >
         {visibleItems}
       </RootTag>

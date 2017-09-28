a364646a60ac746cf64b48b4aa1806838e9e4469
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-09-28 10:56:04 +0300

    Refactoring

--- packages/dx-grid-core/src/plugins/sorting-state/computeds.js
+++ packages/dx-grid-core/src/plugins/sorting-state/computeds.js
@@ -1,6 +1,6 @@
 import mergeSort from '../../utils/merge-sort';
 
-const createSortingCompare = (sorting, compareEqual, getCellValue) => (a, b) => {
+const createSortingComparer = (sorting, compareEqual, getCellValue) => (a, b) => {
   const inverse = sorting.direction === 'desc';
   const { columnName } = sorting;
   const aValue = getCellValue(a, columnName);
@@ -13,22 +13,18 @@ const createSortingCompare = (sorting, compareEqual, getCellValue) => (a, b) => 
   return (aValue < bValue) ^ inverse ? -1 : 1; // eslint-disable-line no-bitwise
 };
 
-export const sortedRows = (rows, sorting, getCellValue, comparer) => {
+export const sortedRows = (rows, sorting, getCellValue, sortingComparer) => {
   if (!sorting.length) return rows;
 
   const compare = Array.from(sorting)
     .reverse()
     .reduce(
       (prevCompare, columnSorting) => {
-        const defaultComparer = createSortingCompare(columnSorting, prevCompare, getCellValue);
-        if (comparer) {
-          const sortingFn = comparer(columnSorting);
-          if (sortingFn) {
-            return sortingFn;
-          }
-          return defaultComparer;
+        if (sortingComparer) {
+          const comparer = sortingComparer(columnSorting);
+          return comparer || createSortingComparer(columnSorting, prevCompare, getCellValue);
         }
-        return defaultComparer;
+        return createSortingComparer(columnSorting, prevCompare, getCellValue);
       },
       () => 0,
     );

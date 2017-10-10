80eb3c3d39ba081d3650fad6695460edab12d671
Yury Orlov <mpcteam.kvet@gmail.com>
2017-10-10 19:17:25 +0300

    chore(react-grid-material-ui): remove redundant tap event plugin

--- packages/dx-react-demos/src/material-ui/basic/table-cell-template.test.jsx
+++ packages/dx-react-demos/src/material-ui/basic/table-cell-template.test.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 import Demo from './table-cell-template';
-
-injectTapEventPlugin();
 
 describe('MUI table cell template', () => {
   it('should work', () => {

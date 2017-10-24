a7803efe6b2a7b904ee30f9949000dac7a1959ba
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2017-10-24 15:19:26 +0300

    Refactor

--- packages/dx-react-grid-material-ui/src/plugins/table-column-resizing.jsx
+++ packages/dx-react-grid-material-ui/src/plugins/table-column-resizing.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { TableColumnResizing as TableColumnResizingBase } from '@devexpress/dx-react-grid';
 
-export const TableColumnResizing = ({ columnWidths, onColumnWidthsChange, ...restProps }) => (
-  <TableColumnResizingBase
-    columnWidths={columnWidths}
-    onColumnWidthsChange={onColumnWidthsChange}
-    {...restProps}
-  />
-);
-
-TableColumnResizing.propTypes = {
-  columnWidths: PropTypes.objectOf(PropTypes.number),
-  onColumnWidthsChange: PropTypes.func,
-};
-
-TableColumnResizing.defaultProps = {
-  columnWidths: undefined,
-  onColumnWidthsChange: undefined,
-};
+export class TableColumnResizing extends React.PureComponent {
+  render() {
+    return (
+      <TableColumnResizingBase
+        {...this.props}
+      />
+    );
+  }
+}

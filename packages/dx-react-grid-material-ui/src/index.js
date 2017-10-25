4be4ebf204dfe7b368fedff1855ef8f0b81bcbf2
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2017-10-25 13:33:16 +0300

    refactor(react-grid): export of TableColumnResizing from theme packages (#438)
    BREAKING CHANGES:
    
    The TableColumnResizing plugin is now available in the "@devexpress/dx-react-grid-bootstrap3" and "@devexpress/dx-react-grid-material-ui" packages.
    
    Use the following code to import the plugin.
    
    ```
    import {
      TableColumnResizing,
    } from from '@devexpress/dx-react-grid-bootstrap3'/* or '@devexpress/dx-react-grid-material-ui' */;
    ```

--- packages/dx-react-grid-material-ui/src/index.js
+++ packages/dx-react-grid-material-ui/src/index.js
@@ -12,3 +12,4 @@ export { TableHeaderRow } from './plugins/table-header-row';
 export { TableEditColumn } from './plugins/table-edit-column';
 export { TableEditRow } from './plugins/table-edit-row';
 export { TableColumnVisibility } from './plugins/table-column-visibility';
+export { TableColumnResizing } from './plugins/table-column-resizing';

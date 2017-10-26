098694d88f69fef9b9c49b1f93c06176da7130cd
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-10-26 16:40:56 +0300

    Sort group rows

--- packages/dx-grid-core/src/plugins/local-sorting/computeds.js
+++ packages/dx-grid-core/src/plugins/local-sorting/computeds.js
@@ -29,39 +29,67 @@ const createCompare = (sorting, getColumnCompare, getCellValue) =>
       () => 0,
     );
 
-const sortRows = (rows, compare, isGroupRow) => {
-  const lastIndex = rows.length - 1;
+const prepareRows = (rows, isGroupRow) => rows.reduce((acc, item) => {
+  const { key: groupKey } = item;
+  const { processed, groupKeys } = acc;
 
-  return rows.reduce((acc, row, index) => {
-    const { boundaries, sorted } = acc;
-    const currentRowIsGroup = isGroupRow ? isGroupRow(row) : false;
+  if (isGroupRow(item)) {
+    const groupItem = { item, items: [] };
 
-    if (index === 0) {
-      boundaries.push(currentRowIsGroup ? 1 : 0);
-      if (currentRowIsGroup) {
-        sorted.push(row);
+    if (!groupKeys.size) {
+      processed.push(groupItem);
+    } else {
+      const parentKey = groupKey.replace(`|${item.value}`, '');
+      const parent = groupKeys.get(parentKey);
+
+      if (parent) {
+        parent.items.push(groupItem);
+      } else {
+        processed.push(groupItem);
       }
-    } else if (index === lastIndex) {
-      boundaries.push(currentRowIsGroup ? index : undefined);
-    } else if (currentRowIsGroup) {
-      boundaries.push(index);
     }
 
-    if (boundaries.length === 2) {
-      const from = boundaries[0];
-      const to = boundaries[1];
-      const sotredSection = mergeSort(rows.slice(from, to), compare);
-
-      sorted.push(...sotredSection);
-      if (to && isGroupRow(rows[to])) {
-        sorted.push(rows[to]);
-      }
-
-      boundaries.splice(0, 2, to + 1);
-    }
-
+    groupKeys.set(groupKey, groupItem);
     return acc;
-  }, { boundaries: [], sorted: [] }).sorted;
+  }
+  if (!groupKeys.size) {
+    processed.push(item);
+  } else {
+    const parentKey = Array.from(groupKeys.keys())[groupKeys.size - 1];
+    groupKeys.get(parentKey).items.push(item);
+  }
+  return acc;
+}, { processed: [], groupKeys: new Map() }).processed;
+
+const sortPreparedRows = (rows, compare, groupCompare) => {
+  const sorted = mergeSort(rows, rows[0].item && rows[0].item.groupedBy ? groupCompare : compare);
+
+  sorted.forEach((row) => {
+    if (row.items && row.items.length) {
+      const sortedItems = sortPreparedRows(row.items, compare, groupCompare);
+      row.items.splice(0, row.items.length, ...sortedItems);
+    }
+  });
+
+  return sorted;
+};
+
+const makePlain = (rows, processed = []) => {
+  const result = rows.reduce((acc, row) => {
+    const { item } = row;
+
+    if (item) {
+      acc.push(item);
+      if (row.items && row.items.length) {
+        makePlain(row.items, acc);
+      }
+    } else {
+      acc.push(row);
+    }
+    return acc;
+  }, processed);
+
+  return result;
 };
 
 export const sortedRows = (
@@ -73,5 +101,18 @@ export const sortedRows = (
 ) => {
   if (!sorting.length) return rows;
   const compare = createCompare(sorting, getColumnCompare, getCellValue);
-  return sortRows(Array.from(rows), compare, isGroupRow);
+
+  if (isGroupRow) {
+    const preparedRows = prepareRows(rows, isGroupRow);
+    const groupCompare = createCompare(sorting, getColumnCompare, (row, field) => {
+      const { item } = row;
+      if (item.groupedBy === field) return item.value;
+      return undefined;
+    });
+    const sortedPreparedRows = sortPreparedRows(preparedRows, compare, groupCompare);
+
+    return makePlain(sortedPreparedRows);
+  }
+
+  return mergeSort(Array.from(rows), compare);
 };

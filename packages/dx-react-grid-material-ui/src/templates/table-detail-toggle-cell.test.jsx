2b28caf30b7b5418d43336da187d34ba17263f5d
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-10-30 14:53:01 +0300

    Fix warnings in MUI

--- packages/dx-react-grid-material-ui/src/templates/table-detail-toggle-cell.test.jsx
+++ packages/dx-react-grid-material-ui/src/templates/table-detail-toggle-cell.test.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { Table } from 'material-ui';
+import { setupConsole } from '@devexpress/dx-testing';
 import { TableDetailToggleCell } from './table-detail-toggle-cell';
 
 describe('TableDetailToggleCell', () => {
+  let resetConsole;
+  beforeAll(() => {
+    resetConsole = setupConsole({ ignore: ['validateDOMNesting', 'SheetsRegistry'] });
+  });
+  afterAll(() => {
+    resetConsole();
+  });
+
   it('should render IconButton', () => {
     const tree = mount((
-      <TableDetailToggleCell />
+      <Table><TableDetailToggleCell /></Table>
     ));
 
     expect(tree.find('IconButton').exists())
@@ -15,9 +25,11 @@ describe('TableDetailToggleCell', () => {
   it('should handle click', () => {
     const toggleExpanded = jest.fn();
     const tree = mount((
-      <TableDetailToggleCell
-        toggleExpanded={toggleExpanded}
-      />
+      <Table>
+        <TableDetailToggleCell
+          toggleExpanded={toggleExpanded}
+        />
+      </Table>
     ));
 
     tree.find('IconButton').simulate('click');

e9c00727be310f6bb995cd20b02d1c17b7970ef7
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-11-07 10:11:00 +0300

    Refactor MUI tests

--- packages/dx-react-grid-material-ui/src/templates/table-detail-toggle-cell.test.jsx
+++ packages/dx-react-grid-material-ui/src/templates/table-detail-toggle-cell.test.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import { Table } from 'material-ui';
+import { createMount } from 'material-ui/test-utils';
 import { setupConsole } from '@devexpress/dx-testing';
 import { TableDetailToggleCell } from './table-detail-toggle-cell';
 
 describe('TableDetailToggleCell', () => {
   let resetConsole;
+  let mount;
   beforeAll(() => {
     resetConsole = setupConsole({ ignore: ['validateDOMNesting', 'SheetsRegistry'] });
+    mount = createMount({ context: { table: {} }, childContextTypes: { table: () => null } });
   });
   afterAll(() => {
     resetConsole();
+    mount.cleanUp();
   });
 
   it('should render IconButton', () => {
     const tree = mount((
-      <Table><TableDetailToggleCell /></Table>
+      <TableDetailToggleCell />
     ));
 
     expect(tree.find('IconButton').exists())
@@ -25,11 +27,9 @@ describe('TableDetailToggleCell', () => {
   it('should handle click', () => {
     const toggleExpanded = jest.fn();
     const tree = mount((
-      <Table>
-        <TableDetailToggleCell
-          toggleExpanded={toggleExpanded}
-        />
-      </Table>
+      <TableDetailToggleCell
+        toggleExpanded={toggleExpanded}
+      />
     ));
 
     tree.find('IconButton').simulate('click');

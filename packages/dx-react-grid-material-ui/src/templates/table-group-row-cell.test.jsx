c285ced9ac5b654a615f6e737d9cdef2e3da2682
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2017-11-09 18:46:53 +0300

    Add test

--- packages/dx-react-grid-material-ui/src/templates/table-group-row-cell.test.jsx
+++ packages/dx-react-grid-material-ui/src/templates/table-group-row-cell.test.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { createMount } from 'material-ui/test-utils';
-import { Table } from 'material-ui';
+import { createMount, createShallow } from 'material-ui/test-utils';
+import { Table, TableCell } from 'material-ui';
 import { setupConsole } from '@devexpress/dx-testing';
 import { TableGroupCell } from './table-group-row-cell';
 
 describe('TableCell', () => {
   let resetConsole;
   let mount;
+  let shallow;
   beforeAll(() => {
     resetConsole = setupConsole({ ignore: ['validateDOMNesting'] });
     const mountMUI = createMount();
+    shallow = createShallow({ dive: true });
     mount = component => mountMUI(<Table>{component}</Table>);
   });
 
@@ -48,5 +50,18 @@ describe('TableCell', () => {
 
     expect(tree.find('IconButton').exists())
       .toBeTruthy();
+  });
+
+  it('should expand grouped row on click', () => {
+    const toggleGroupExpanded = jest.fn();
+    const tree = shallow((
+      <TableGroupCell
+        toggleGroupExpanded={toggleGroupExpanded}
+      />
+    ));
+    tree.find(TableCell).simulate('click');
+
+    expect(toggleGroupExpanded)
+      .toHaveBeenCalled();
   });
 });

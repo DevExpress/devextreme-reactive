e9e7098ae91f6d96000123ff5ac6f8774fe45421
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2017-11-09 15:34:18 +0300

    fix(react-grid-bootstrap3): the TableSelectCell has bad markup

--- packages/dx-react-grid-bootstrap3/src/templates/table-select-cell.jsx
+++ packages/dx-react-grid-bootstrap3/src/templates/table-select-cell.jsx
@@ -5,6 +5,7 @@ export const TableSelectCell = ({ style, selected, changeSelected }) => (
   <td
     style={{
       cursor: 'pointer',
+      verticalAlign: 'middle',
       ...style,
     }}
     onClick={(e) => {
@@ -15,7 +16,8 @@ export const TableSelectCell = ({ style, selected, changeSelected }) => (
     <input
       style={{
         cursor: 'pointer',
-        margin: 0,
+        margin: '0 auto',
+        display: 'block',
       }}
       type="checkbox"
       checked={selected}

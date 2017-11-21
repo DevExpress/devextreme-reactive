d01a3b4776b93e2f5aa333df93ce1c9be6e925f9
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-11-21 14:55:49 +0300

    Drop demos

--- packages/dx-react-demos/src/bootstrap3/basic/table-header-cell-template.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import Demo from './table-header-cell-template';
-
-describe('BS3: table header cell template demo', () => {
-  it('should work', () => {
-    mount(<Demo />);
-  });
-});

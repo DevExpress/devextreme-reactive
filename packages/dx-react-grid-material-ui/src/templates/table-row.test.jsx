37b4393f6d1411d82eb2999e92de52768bfd5287
SergeyAlexeev <SergeyAlexeev@users.noreply.github.com>
2017-11-28 15:18:09 +0300

    feat(react-grid): add components for the Table plugin (#516)

---
+++ packages/dx-react-grid-material-ui/src/templates/table-row.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createShallow } from 'material-ui/test-utils';
+import { TableRow } from './table-row';
+
+describe('TableRow', () => {
+  let shallow;
+  beforeAll(() => {
+    shallow = createShallow({ dive: true });
+  });
+
+  it('should pass rest props to the root element', () => {
+    const tree = shallow((
+      <TableRow className="custom-class" />
+    ));
+
+    expect(tree.is('.custom-class'))
+      .toBeTruthy();
+  });
+});

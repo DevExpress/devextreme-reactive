c465d78ceb58c3700e63f2dee53662a3563ced88
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2017-12-04 14:44:39 +0300

    Refactor after review

--- packages/dx-react-grid/src/plugins/local-paging.jsx
+++ packages/dx-react-grid/src/plugins/local-paging.jsx
@@ -8,45 +8,52 @@ const pluginDependencies = [
 
 const rowsWithHeadersComputed = ({ rows, pageSize, getRowLevelKey }) =>
   rowsWithPageHeaders(rows, pageSize, getRowLevelKey);
-const totalCountComputed = ({ rows }) => rowCount(rows);
 const paginatedRowsComputed = ({ rows, pageSize, currentPage }) =>
   paginatedRows(rows, pageSize, currentPage);
-const gettersChangesComputed = (returnedValue, getters, actions) => {
-  const totalPages = pageCount(getters.totalCount, getters.pageSize);
-  if (totalPages - 1 < getters.currentPage) {
-    actions.setCurrentPage(Math.max(totalPages - 1, 0));
-  }
-  return returnedValue;
-};
 
 // eslint-disable-next-line react/prefer-stateless-function
 export class LocalPaging extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      totalCount: undefined,
+      pageSize: undefined,
+    };
+
+    this.totalCountComputed = this.totalCountComputed.bind(this);
+    this.pageSizeComputed = this.pageSizeComputed.bind(this);
+  }
+
+  totalCountComputed({ rows }) {
+    const totalCount = rowCount(rows);
+    this.setState({ totalCount });
+    return totalCount;
+  }
+  pageSizeComputed({ pageSize }) {
+    this.setState({ pageSize });
+    return pageSize;
+  }
   render() {
+    const { totalCount, pageSize } = this.state;
+
+    const currentPageComputed = ({ currentPage }, { setCurrentPage }) => {
+      const totalPages = pageCount(totalCount, pageSize);
+      if (totalPages - 1 < currentPage) {
+        setCurrentPage(Math.max(totalPages - 1, 0));
+      }
+      return currentPage;
+    };
+
     return (
       <PluginContainer
         pluginName="LocalPaging"
         dependencies={pluginDependencies}
       >
         <Getter name="rows" computed={rowsWithHeadersComputed} />
-        <Getter name="totalCount" computed={totalCountComputed} />
-        <Getter
-          name="currentPage"
-          computed={(getters, actions) =>
-            gettersChangesComputed(getters.currentPage, getters, actions)
-          }
-        />
-        <Getter
-          name="totalCount"
-          computed={(getters, actions) =>
-            gettersChangesComputed(getters.totalCount, getters, actions)
-          }
-        />
-        <Getter
-          name="pageSize"
-          computed={(getters, actions) =>
-            gettersChangesComputed(getters.pageSize, getters, actions)
-          }
-        />
+        <Getter name="totalCount" computed={this.totalCountComputed} />
+        <Getter name="pageSize" computed={this.pageSizeComputed} />
+        <Getter name="currentPage" computed={currentPageComputed} />
         <Getter name="rows" computed={paginatedRowsComputed} />
       </PluginContainer>
     );

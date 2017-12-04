38cbc91b47f1375d73dcff88d945697720d08354
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2017-12-04 16:24:10 +0300

    Refactor

--- packages/dx-react-grid/src/plugins/local-paging.jsx
+++ packages/dx-react-grid/src/plugins/local-paging.jsx
@@ -6,53 +6,32 @@ const pluginDependencies = [
   { pluginName: 'PagingState' },
 ];
 
+const clamp = (value, max) => Math.max(Math.min(value, max), 0);
+
 const rowsWithHeadersComputed = ({ rows, pageSize, getRowLevelKey }) =>
   rowsWithPageHeaders(rows, pageSize, getRowLevelKey);
+const totalCountComputed = ({ rows }) => rowCount(rows);
 const paginatedRowsComputed = ({ rows, pageSize, currentPage }) =>
   paginatedRows(rows, pageSize, currentPage);
+const currentPageComputed = ({ currentPage, totalCount, pageSize }, { setCurrentPage }) => {
+  const totalPages = pageCount(totalCount, pageSize);
+  const adjustedCurrentPage = clamp(currentPage, totalPages - 1);
+  if (totalPages - 1 < currentPage) {
+    setCurrentPage(adjustedCurrentPage);
+  }
+  return adjustedCurrentPage;
+};
 
 // eslint-disable-next-line react/prefer-stateless-function
 export class LocalPaging extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      totalCount: undefined,
-      pageSize: undefined,
-    };
-
-    this.totalCountComputed = this.totalCountComputed.bind(this);
-    this.pageSizeComputed = this.pageSizeComputed.bind(this);
-  }
-
-  totalCountComputed({ rows }) {
-    const totalCount = rowCount(rows);
-    this.setState({ totalCount });
-    return totalCount;
-  }
-  pageSizeComputed({ pageSize }) {
-    this.setState({ pageSize });
-    return pageSize;
-  }
   render() {
-    const { totalCount, pageSize } = this.state;
-
-    const currentPageComputed = ({ currentPage }, { setCurrentPage }) => {
-      const totalPages = pageCount(totalCount, pageSize);
-      if (totalPages - 1 < currentPage) {
-        setCurrentPage(Math.max(totalPages - 1, 0));
-      }
-      return currentPage;
-    };
-
     return (
       <PluginContainer
         pluginName="LocalPaging"
         dependencies={pluginDependencies}
       >
         <Getter name="rows" computed={rowsWithHeadersComputed} />
-        <Getter name="totalCount" computed={this.totalCountComputed} />
-        <Getter name="pageSize" computed={this.pageSizeComputed} />
+        <Getter name="totalCount" computed={totalCountComputed} />
         <Getter name="currentPage" computed={currentPageComputed} />
         <Getter name="rows" computed={paginatedRowsComputed} />
       </PluginContainer>

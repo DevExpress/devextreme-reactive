6788ba6cb8c0e4e7a2590864f6c6f50c11b8ef09
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2017-12-07 13:52:53 +0300

    fix(grid-core): allow to place TableColumnVisibility after another table plugins (#557)

--- packages/dx-grid-core/src/plugins/table-column-visibility/computeds.js
+++ packages/dx-grid-core/src/plugins/table-column-visibility/computeds.js
@@ -1,2 +1,5 @@
+import { TABLE_DATA_TYPE } from './../table/constants';
+
 export const visibleTableColumns = (tableColumns, hiddenColumns) =>
-  tableColumns.filter(tableColumn => hiddenColumns.indexOf(tableColumn.column.name) === -1);
+  tableColumns.filter(tableColumn =>
+    tableColumn.type !== TABLE_DATA_TYPE || hiddenColumns.indexOf(tableColumn.column.name) === -1);
--- packages/dx-grid-core/src/plugins/table-column-visibility/computeds.test.js
+++ packages/dx-grid-core/src/plugins/table-column-visibility/computeds.test.js
@@ -1,18 +1,31 @@
 import { visibleTableColumns } from './computeds';
+import { TABLE_DATA_TYPE } from './../table/constants';
 
 describe('TableColumnVisibility computeds', () => {
   describe('#visibleTableColumns', () => {
     it('should return a correct array of visible table columns', () => {
       const tableColumns = [
-        { column: { name: 'a' } },
-        { column: { name: 'b' } },
-        { column: { name: 'c' } },
-        { column: { name: 'd' } },
+        { type: TABLE_DATA_TYPE, column: { name: 'a' } },
+        { type: TABLE_DATA_TYPE, column: { name: 'b' } },
+        { type: TABLE_DATA_TYPE, column: { name: 'c' } },
+        { type: TABLE_DATA_TYPE, column: { name: 'd' } },
       ];
       const hiddenColumns = ['a', 'c', 'd'];
 
       expect(visibleTableColumns(tableColumns, hiddenColumns))
-        .toEqual([{ column: { name: 'b' } }]);
+        .toEqual([{ type: TABLE_DATA_TYPE, column: { name: 'b' } }]);
+    });
+
+    it('should ignore non-data columns', () => {
+      const tableColumns = [
+        { key: 'editCommand' },
+        { type: TABLE_DATA_TYPE, column: { name: 'a' } },
+        { type: TABLE_DATA_TYPE, column: { name: 'b' } },
+      ];
+      const hiddenColumns = ['a'];
+
+      expect(visibleTableColumns(tableColumns, hiddenColumns))
+        .toEqual([{ key: 'editCommand' }, { type: TABLE_DATA_TYPE, column: { name: 'b' } }]);
     });
   });
 });

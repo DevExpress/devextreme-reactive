64074e6c55dde0972329f946b9fd6af1bec69592
Yury Orlov <mpcteam.kvet@gmail.com>
2017-12-12 11:31:33 +0300

    row detail state

--- packages/dx-react-grid/src/plugins/row-detail-state.jsx
+++ packages/dx-react-grid/src/plugins/row-detail-state.jsx
@@ -11,29 +11,49 @@ export class RowDetailState extends React.PureComponent {
       expandedRows: props.defaultExpandedRows || [],
     };
 
-    this.setDetailRowExpanded = ({ rowId }) => {
-      const prevExpandedDetails = this.props.expandedRows || this.state.expandedRows;
-      const expandedRows = setDetailRowExpanded(prevExpandedDetails, { rowId });
-      const { onExpandedRowsChange } = this.props;
-      this.setState({ expandedRows });
-      if (onExpandedRowsChange) {
-        onExpandedRowsChange(expandedRows);
-      }
+    this.setDetailRowExpanded = (payload) => {
+      this.applyReducer(state => ({
+        expandedRows: setDetailRowExpanded(state.expandedRows, payload),
+      }));
     };
   }
+  getState(temporaryState) {
+    return {
+      ...this.state,
+      expandedRows: this.props.expandedRows || this.state.expandedRows,
+      ...(this.state !== temporaryState ? temporaryState : null),
+    };
+  }
+  applyReducer(reduce, payload) {
+    const stateUpdater = (prevState) => {
+      const state = this.getState(prevState);
+      const nextState = { ...state, ...reduce(state, payload) };
 
+      if (stateUpdater === this.lastStateUpdater) {
+        this.notifyStateChange(nextState, state);
+      }
+
+      return nextState;
+    };
+    this.lastStateUpdater = stateUpdater;
+
+    this.setState(stateUpdater);
+  }
+  notifyStateChange(nextState, state) {
+    const { expandedRows } = nextState;
+    const { onExpandedRowsChange } = this.props;
+    if (onExpandedRowsChange && expandedRows !== state.expandedRows) {
+      onExpandedRowsChange(expandedRows);
+    }
+  }
   render() {
     const expandedRows = this.props.expandedRows || this.state.expandedRows;
     return (
       <PluginContainer
         pluginName="RowDetailState"
       >
-        <Action
-          name="setDetailRowExpanded"
-          action={({ rowId }) => this.setDetailRowExpanded({ rowId })}
-        />
-
         <Getter name="expandedRows" value={expandedRows} />
+        <Action name="setDetailRowExpanded" action={this.setDetailRowExpanded} />
       </PluginContainer>
     );
   }

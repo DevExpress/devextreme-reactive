3a5b5c5b3723fb1b2721c5d8d148e720edb02b31
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-12-18 14:01:12 +0300

    Add styles for flexible space

--- packages/dx-react-grid-material-ui/src/plugins/toolbar.jsx
+++ packages/dx-react-grid-material-ui/src/plugins/toolbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Toolbar as ToolbarBase } from '@devexpress/dx-react-grid';
 import { Toolbar as Root } from '../templates/toolbar';
 
-const FlexibleSpace = () => (<div />);
+const FlexibleSpace = () => (<div style={{ flex: '1 0' }} />);
 
 export class Toolbar extends React.PureComponent {
   render() {

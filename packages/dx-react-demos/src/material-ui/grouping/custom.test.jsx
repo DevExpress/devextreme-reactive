d244aa720ada8300bf2ce38f1db8c8c9baa28825
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2017-12-26 15:27:20 +0300

    Fix tests

--- packages/dx-react-demos/src/material-ui/grouping/custom.test.jsx
+++ packages/dx-react-demos/src/material-ui/grouping/custom.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import Demo from './local-grouping-custom';
+import Demo from './custom';
 
 describe('MUI: integrated-grouping-custom', () => {
   it('should work', () => {

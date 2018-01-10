2276956ecd0e50602b0bc7be80e37febd51732e4
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-01-10 11:07:49 +0300

    Fix tests

--- packages/dx-grid-core/src/plugins/table-column-visibility/computeds.js
+++ packages/dx-grid-core/src/plugins/table-column-visibility/computeds.js
@@ -1,7 +1,6 @@
 import { TABLE_DATA_TYPE } from './../table/constants';
 
 export const visibleTableColumns = (tableColumns, hiddenColumnNames) =>
-  tableColumns.filter((tableColumn) => {
-    const isTableColumnVisible = hiddenColumnNames.indexOf(tableColumn.column.name) === -1;
-    return tableColumn.type !== TABLE_DATA_TYPE || isTableColumnVisible;
-  });
+  tableColumns.filter(tableColumn =>
+    tableColumn.type !== TABLE_DATA_TYPE ||
+    hiddenColumnNames.indexOf(tableColumn.column.name) === -1);

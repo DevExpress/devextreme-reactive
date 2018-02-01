e6a59e0040fd3b7cea10f5271d9d16c4d9a801f3
George Sobolev <gsobolev@users.noreply.github.com>
2018-02-01 14:44:47 +0300

    Update TypeScript demos

--- packages/dx-react-demos/src/demo-sources/grid-typescript/material-ui/basic.tsx
+++ packages/dx-react-demos/src/demo-sources/grid-typescript/material-ui/basic.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Column } from '@devexpress/dx-react-grid';
+import { Column, TableColumnExtension } from '@devexpress/dx-react-grid';
 import { Grid, Table, TableHeaderRow } from '@devexpress/dx-react-grid-material-ui';
 import { generateRows } from '../../../demo-data/generator';
 
@@ -10,6 +10,9 @@ const columns: Array<Column> = [
   { name: 'city', title: 'City' },
   { name: 'car', title: 'Car' },
 ];
+const tableColumnExtensions: Array<TableColumnExtension> = [
+  { columnName: 'sex', width: 100 },
+];
 
 export default class Demo extends React.Component {
   render() {
@@ -18,7 +21,7 @@ export default class Demo extends React.Component {
         rows={rows}
         columns={columns}
       >
-        <Table />
+        <Table columnExtensions={tableColumnExtensions} />
         <TableHeaderRow />
       </Grid>
     );

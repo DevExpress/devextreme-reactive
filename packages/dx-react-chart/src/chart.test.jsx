7f6ae3e70b3a67471310854dbaab0be0e2ec8839
Alexey Semikozov <forworknoname@gmail.com>
2018-03-12 10:49:29 +0300

    Cover test chart

--- packages/dx-react-chart/src/chart.test.jsx
+++ packages/dx-react-chart/src/chart.test.jsx
@@ -1,4 +1,55 @@
+
+import * as React from 'react';
+import { mount } from 'enzyme';
+import { setupConsole } from '@devexpress/dx-testing';
+import { Chart } from './chart';
+import { ChartCore } from './plugins/chart-core';
+import { LayoutManager } from './plugins/layout-manage';
+
+jest.mock('./plugins/chart-core', () => ({
+  ChartCore: () => null,
+}));
+const TestChildren = () => null;
+
 describe('Chart', () => {
-  it('empty test', () => {
+  let resetConsole;
+  beforeAll(() => {
+    resetConsole = setupConsole();
+  });
+  afterAll(() => {
+    resetConsole();
+  });
+
+  const defaultProps = {
+    data: [],
+  };
+
+  it('should render root children', () => {
+    const tree = mount((
+      <Chart
+        {...defaultProps}
+      >
+        <TestChildren />
+        <TestChildren />
+        <TestChildren />
+      </Chart>
+    ));
+
+    expect(tree.find(TestChildren))
+      .toHaveLength(3);
+  });
+
+  it('should render chart core', () => {
+    const tree = mount((<Chart {...defaultProps} />));
+
+    expect(tree.find(ChartCore).exists())
+      .toBeTruthy();
+  });
+
+  it('should render layout manager', () => {
+    const tree = mount((<Chart {...defaultProps} />));
+
+    expect(tree.find(LayoutManager).exists())
+      .toBeTruthy();
   });
 });

87d74ca700873e5320a8f05a1e544a1d36adb228
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-03-13 15:33:43 +0300

    Small refactoring

--- packages/dx-react-chart-svg/src/templates/axis/tick.test.jsx
+++ packages/dx-react-chart-svg/src/templates/axis/tick.test.jsx
@@ -4,18 +4,20 @@ import { Tick } from './tick';
 
 describe('Tick', () => {
   it('should render line with correct coordinates', () => {
-    const line = shallow((
+    const {
+      x1, x2, y1, y2,
+    } = shallow((
       <Tick
         x1={1}
         x2={2}
         y1={3}
         y2={4}
       />
-    )).find('line');
+    )).find('line').props();
 
-    expect(line.props().x1).toBe(1);
-    expect(line.props().x2).toBe(2);
-    expect(line.props().y1).toBe(3);
-    expect(line.props().y2).toBe(4);
+    expect(x1).toBe(1);
+    expect(x2).toBe(2);
+    expect(y1).toBe(3);
+    expect(y2).toBe(4);
   });
 });

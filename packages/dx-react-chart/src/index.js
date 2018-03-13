9760b66d72b204dd00f5f2d4e04958cdf679e041
Alexey Semikozov <forworknoname@gmail.com>
2018-03-13 19:27:58 +0300

    Remove scale state plugin

--- packages/dx-react-chart/src/index.js
+++ packages/dx-react-chart/src/index.js
@@ -1,6 +1,5 @@
 export { Chart } from './chart';
 export { Axis } from './plugins/axis';
-export { ScaleState } from './plugins/scale-state';
 export { LineSeries } from './plugins/line-series';
 export { SplineSeries } from './plugins/spline-series';
 export { IntegratedScaleProcessing } from './plugins/integrated-scale-processing';

6aeacee6dd10813e43eb816af85ce8631abd8b64
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-03-13 15:00:44 +0300

    Add markup for axis components

--- packages/dx-react-chart-svg/src/templates/axis/label.jsx
+++ packages/dx-react-chart-svg/src/templates/axis/label.jsx
@@ -1,5 +1,20 @@
 import * as React from 'react';
+import * as PropTypes from 'prop-types';
 
-export const Label = props => (
-  <text {...props} />
+export const Label = ({ text, x, y }) => (
+  <text
+    alignmentBaseline="middle"
+    textAnchor="middle"
+    key={text}
+    x={x}
+    y={y}
+  >
+    {text}
+  </text>
 );
+
+Label.propTypes = {
+  text: PropTypes.string.isRequired,
+  x: PropTypes.number.isRequired,
+  y: PropTypes.number.isRequired,
+};
--- packages/dx-react-chart-svg/src/templates/axis/line.jsx
+++ packages/dx-react-chart-svg/src/templates/axis/line.jsx
@@ -1,5 +1,21 @@
 import * as React from 'react';
+import * as PropTypes from 'prop-types';
 
-export const Line = props => (
-  <line {...props} />
+export const Line = ({
+  x1, x2, y1, y2,
+}) => (
+  <line
+    style={{ stroke: 'black', strokeWidth: '1px' }}
+    x1={x1}
+    x2={x2}
+    y1={y1}
+    y2={y2}
+  />
 );
+
+Line.propTypes = {
+  x1: PropTypes.number.isRequired,
+  x2: PropTypes.number.isRequired,
+  y1: PropTypes.number.isRequired,
+  y2: PropTypes.number.isRequired,
+};

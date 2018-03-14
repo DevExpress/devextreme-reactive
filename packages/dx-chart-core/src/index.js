0d57937dfe7f82f0d52f425779a1c763fecf93a6
Alexey Semikozov <forworknoname@gmail.com>
2018-03-14 15:15:52 +0300

    Move calculate axis coords to chart core package

--- packages/dx-chart-core/src/index.js
+++ packages/dx-chart-core/src/index.js
@@ -1,2 +1,3 @@
 export * from './plugins/integrated-scale-processing/computeds';
 export * from './plugins/chart-core/computeds';
+export * from './plugins/axis/computeds';
--- packages/dx-react-chart/src/plugins/axis.jsx
+++ packages/dx-react-chart/src/plugins/axis.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import { scaleLinear } from 'd3-scale';
 
 import {
   Plugin,
@@ -9,41 +8,7 @@ import {
   TemplatePlaceholder,
 } from '@devexpress/dx-react-core';
 
-const tickSize = 10;
-
-const getAxisCoords = (scale, width, height, orientation) => {
-  let getTickCoords;
-  if (orientation === 'horizontal') {
-    getTickCoords = (tick) => {
-      const xCoords = scale(tick);
-      return {
-        x1: xCoords,
-        x2: xCoords,
-        y1: 0,
-        y2: tickSize,
-        text: tick,
-        xText: xCoords,
-        yText: 20,
-      };
-    };
-  } else {
-    getTickCoords = (tick) => {
-      const yCoords = scale(tick);
-      return {
-        y1: yCoords,
-        y2: yCoords,
-        x1: 30,
-        x2: 30 + tickSize,
-        text: tick,
-        xText: 15,
-        yText: yCoords,
-      };
-    };
-  }
-  return {
-    ticks: scale.ticks().map(getTickCoords),
-  };
-};
+import { calculateAxisCoords } from '@devexpress/dx-chart-core';
 
 const createRefsHandler = (placeholder, setBBox) => (el) => {
   if (!el) {
@@ -77,17 +42,8 @@ export class Axis extends React.PureComponent {
                 const refsHandler = createRefsHandler(placeholder, setBBox);
                 const domain = domains[name];
                 const { orientation } = axes.find(axis => axis.name === name);
-                const scale = scaleLinear()
-                  .domain(domain)
-                  .range(orientation === 'horizontal'
-                      ? [0, width]
-                      : [height, 0]);
-                const axesCoords = getAxisCoords(
-                  scale,
-                  width,
-                  height,
-                  orientation,
-                );
+
+                const axesCoords = calculateAxisCoords(domain, orientation, width, height);
 
                 return ((
                   <Root refsHandler={refsHandler} x={x} y={y}>

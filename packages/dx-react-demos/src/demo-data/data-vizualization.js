75442a6f80fe881a63f6d7daa03063758202cd81
Alexey Semikozov <forworknoname@gmail.com>
2018-03-16 00:26:00 +0300

    Refactor chart demo

---
+++ packages/dx-react-demos/src/demo-data/data-vizualization.js
@@ -0,0 +1,26 @@
+export const born = [
+  {
+    year: 1995, Russia: 101, China: 170, USA: 31,
+  },
+  {
+    year: 1998, Russia: 200, China: 80, USA: 90,
+  },
+  {
+    year: 2000, Russia: 100, China: 70, USA: 45,
+  },
+  {
+    year: 2001, Russia: 110, China: 70, USA: 75,
+  },
+  {
+    year: 2002, Russia: 95, China: 120, USA: 25,
+  },
+  {
+    year: 2006, Russia: 115, China: 50, USA: 45,
+  },
+  {
+    year: 2007, Russia: 230, China: 110, USA: 120,
+  },
+  {
+    year: 2015, Russia: 89, China: 90, USA: 19,
+  },
+];
--- packages/dx-react-demos/src/demo-sources/chart-basic/bootstrap3/basic.skip.jsx
+++ packages/dx-react-demos/src/demo-sources/chart-basic/bootstrap3/basic.skip.jsx
@@ -7,33 +7,7 @@ import {
   SplineSeries,
   Grid,
 } from '@devexpress/dx-react-chart-svg';
-
-const data = [
-  {
-    year: 1995, Russia: 101, China: 170, USA: 31,
-  },
-  {
-    year: 1998, Russia: 200, China: 80, USA: 90,
-  },
-  {
-    year: 2000, Russia: 100, China: 70, USA: 45,
-  },
-  {
-    year: 2001, Russia: 110, China: 70, USA: 75,
-  },
-  {
-    year: 2002, Russia: 95, China: 120, USA: 25,
-  },
-  {
-    year: 2006, Russia: 115, China: 50, USA: 45,
-  },
-  {
-    year: 2007, Russia: 230, China: 110, USA: 120,
-  },
-  {
-    year: 2015, Russia: 89, China: 90, USA: 19,
-  },
-];
+import { born as data } from '../../../demo-data/data-vizualization';
 
 export default class Demo extends React.PureComponent {
   constructor(props) {

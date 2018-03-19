c02903ab8444be8adc0342138eb3b57582d10bfd
Smirnova Yuliya <Krijovnick@users.noreply.github.com>
2018-03-19 11:59:03 +0300

    feat(react-grid): add minColumnWidth option (#837)

--- packages/dx-grid-core/src/plugins/table-column-resizing/reducers.js
+++ packages/dx-grid-core/src/plugins/table-column-resizing/reducers.js
@@ -1,11 +1,9 @@
-const MIN_SIZE = 40;
-
-export const changeTableColumnWidth = (state, { columnName, shift }) => {
+export const changeTableColumnWidth = (state, { columnName, shift, minColumnWidth }) => {
   const { columnWidths } = state;
   const nextColumnWidth = columnWidths.slice();
   const index = nextColumnWidth.findIndex(elem => elem.columnName === columnName);
   const updatedColumn = nextColumnWidth[index];
-  const size = Math.max(MIN_SIZE, updatedColumn.width + shift);
+  const size = Math.max(minColumnWidth, updatedColumn.width + shift);
   nextColumnWidth.splice(index, 1, { columnName, width: size });
 
   return {
@@ -13,10 +11,10 @@ export const changeTableColumnWidth = (state, { columnName, shift }) => {
   };
 };
 
-export const draftTableColumnWidth = (state, { columnName, shift }) => {
+export const draftTableColumnWidth = (state, { columnName, shift, minColumnWidth }) => {
   const { columnWidths } = state;
   const updatedColumn = columnWidths.find(elem => elem.columnName === columnName);
-  const size = Math.max(MIN_SIZE, updatedColumn.width + shift);
+  const size = Math.max(minColumnWidth, updatedColumn.width + shift);
 
   return {
     draftColumnWidths: [{ columnName: updatedColumn.columnName, width: size }],

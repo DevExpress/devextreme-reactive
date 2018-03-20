0d18298969c03b83590fd6e04ff4adece19e26c2
Alexey Semikozov <forworknoname@gmail.com>
2018-03-20 15:12:27 +0300

    Refactor test

--- packages/dx-chart-core/src/plugins/chart-core/computeds.test.js
+++ packages/dx-chart-core/src/plugins/chart-core/computeds.test.js
@@ -1,11 +1,13 @@
 import { argumentAxisName } from './computeds';
 
 describe('Chart core', () => {
-  it('getArgumentAxisName should return horizontal axis', () => {
-    const axisName = argumentAxisName([
-      { name: 'axis' },
-      { orientation: 'horizontal', name: 'argumentAxis' },
-    ]);
-    expect(axisName).toBe('argumentAxis');
+  describe('getArgumentAxisName', () => {
+    it('should return horizontal axis', () => {
+      const axisName = argumentAxisName([
+        { name: 'axis' },
+        { orientation: 'horizontal', name: 'argumentAxis' },
+      ]);
+      expect(axisName).toBe('argumentAxis');
+    });
   });
 });

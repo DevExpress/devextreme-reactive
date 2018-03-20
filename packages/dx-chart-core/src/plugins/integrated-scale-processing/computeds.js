806bcd89e58bc689f51c4b9fdf0a52308c226d8b
Alexey Semikozov <forworknoname@gmail.com>
2018-03-20 14:18:09 +0300

    Add type to domain processing

--- packages/dx-chart-core/src/plugins/integrated-scale-processing/computeds.js
+++ packages/dx-chart-core/src/plugins/integrated-scale-processing/computeds.js
@@ -1,35 +1,57 @@
 import { extent } from 'd3-array';
 
-const getAxesDomain = axes =>
-  axes.reduce((acc, {
-    name, min, max, orientation,
-  }) => {
-    acc[name] = { domain: [min, max], orientation };
-    return acc;
-  }, {});
+const isNotUndefined = item => item !== undefined;
 
-const calculateDomain = (series, data, domains, argumentAxisName) =>
-  (
-    {
-      ...series.reduce((acc, seriesOption) => {
-        const { valueField, argumentField, axisName } = seriesOption;
-        const valueDomain = extent(data, item => item[valueField]);
-        const argumentDomain = extent(data, item => item[argumentField]);
-        acc[axisName] = {
-          domain: acc[axisName]
-            ? extent(acc[axisName].domain.concat(valueDomain))
-            : valueDomain,
-          orientation: acc[axisName] && acc[axisName].orientation,
-        };
-        acc[argumentAxisName] = {
-          domain: acc[argumentAxisName]
-            ? extent(acc[argumentAxisName].domain.concat(argumentDomain))
-            : argumentDomain,
-          orientation: acc[argumentAxisName] && acc[argumentAxisName].orientation,
-        };
-        return acc;
-      }, domains),
-    }
+const getAxesDomain = axes =>
+  axes.reduce(
+    (domains, {
+      name, min, max, orientation, type,
+    }) => ({
+      ...domains,
+      [name]: {
+        domain: [min, max].filter(isNotUndefined),
+        orientation,
+        type,
+      },
+    }),
+    {},
+  );
+
+const calculateDomainFiled = (field, data, domain = [], type) => {
+  const getFieldItem = object => object[field];
+  if (type === 'band') {
+    return [...domain, ...data.map(getFieldItem)];
+  }
+  return extent([...domain, ...extent(data, getFieldItem)]);
+};
+
+const calculateDomain = (series, data, axesDomains, argumentAxisName) =>
+  series.reduce(
+    (domains, { valueField, argumentField, axisName }) => ({
+      ...domains,
+      [axisName]: {
+        domain: calculateDomainFiled(
+          valueField,
+          data,
+          domains[axisName].domain,
+          domains[axisName].type,
+        ),
+        orientation: domains[axisName] && domains[axisName].orientation,
+        type: domains[axisName] && domains[axisName].type,
+      },
+      [argumentAxisName]: {
+        domain: calculateDomainFiled(
+          argumentField,
+          data,
+          domains[argumentAxisName].domain,
+          domains[argumentAxisName].type,
+        ),
+        orientation:
+          domains[argumentAxisName] && domains[argumentAxisName].orientation,
+        type: domains[argumentAxisName].type,
+      },
+    }),
+    axesDomains,
   );
 
 export const domains = (axes, series, data, argumentAxisName) => {
--- packages/dx-chart-core/src/plugins/integrated-scale-processing/computeds.test.js
+++ packages/dx-chart-core/src/plugins/integrated-scale-processing/computeds.test.js
@@ -1,21 +1,53 @@
 import { domains } from './computeds';
 
 describe('calculateDomain', () => {
+  const argumentAxis = { name: 'argumentAxis', orientation: 'horizontal' };
+  const valueAxis = { name: 'valueAxis', orientation: 'vertical' };
+
   it('should be equal axes min max option', () => {
-    const axes = [{
-      name: 'axis', min: 0, max: 10, orientation: 'horizontal',
-    }];
-    const series = [];
-    const data = [];
-    const calculatedDomains = domains(axes, series, data, 'axis');
-    expect(calculatedDomains).toEqual({ axis: { domain: [0, 10], orientation: 'horizontal' } });
+    const calculatedDomains = domains(
+      [{ ...argumentAxis, min: 0, max: 10 }],
+      [],
+      [],
+      'argumentAxis',
+    );
+    expect(calculatedDomains).toEqual({
+      argumentAxis: { domain: [0, 10], orientation: 'horizontal' },
+    });
   });
 
+
   it('should be computed from data and series option', () => {
-    const axes = [{ name: 'axis', orientation: 'horizontal' }];
-    const series = [{ axisName: 'axis', argumentField: 'arg', valueField: 'val' }];
-    const data = [{ arg: 1, val: 9 }];
-    const calculatedDomains = domains(axes, series, data, 'axis');
-    expect(calculatedDomains).toEqual({ axis: { domain: [1, 9], orientation: 'horizontal' } });
+    const calculatedDomains = domains(
+      [argumentAxis, valueAxis],
+      [{ axisName: 'valueAxis', argumentField: 'arg', valueField: 'val' }],
+      [{ arg: 1, val: 9 }],
+      'argumentAxis',
+    );
+
+    expect(calculatedDomains).toEqual({
+      argumentAxis: { domain: [1, 1], orientation: 'horizontal' },
+      valueAxis: { domain: [9, 9], orientation: 'vertical' },
+    });
+  });
+
+  it('should be computed from data and series option with band type option', () => {
+    const calculatedDomains = domains(
+      [{ ...argumentAxis, type: 'band' }, valueAxis],
+      [{ axisName: 'valueAxis', argumentField: 'arg', valueField: 'val' }],
+      [{ arg: 'a', val: 1 }, { arg: 'b', val: 2 }, { arg: 'c' }],
+      'argumentAxis',
+    );
+    expect(calculatedDomains).toEqual({
+      argumentAxis: {
+        domain: ['a', 'b', 'c'],
+        orientation: 'horizontal',
+        type: 'band',
+      },
+      valueAxis: {
+        domain: [1, 2],
+        orientation: 'vertical',
+      },
+    });
   });
 });

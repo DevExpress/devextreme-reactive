d5f2b2a19e6c2227594bd5c3dcd443861d6bb53a
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2018-04-11 14:51:11 +0300

    chore: add the IntegratedPaging plugin for Vue Grid

---
+++ packages/dx-vue-grid/src/plugins/integrated-paging.js
@@ -0,0 +1,39 @@
+import { Getter, Plugin } from '@devexpress/dx-vue-core';
+import { paginatedRows, rowsWithPageHeaders, pageCount, rowCount } from '@devexpress/dx-grid-core';
+
+const pluginDependencies = [
+  { name: 'PagingState' },
+];
+
+const clamp = (value, max) => Math.max(Math.min(value, max), 0);
+
+const rowsWithHeadersComputed = ({ rows, pageSize, getRowLevelKey }) =>
+  rowsWithPageHeaders(rows, pageSize, getRowLevelKey);
+const totalCountComputed = ({ rows }) => rowCount(rows);
+const paginatedRowsComputed = ({ rows, pageSize, currentPage }) =>
+  paginatedRows(rows, pageSize, currentPage);
+const currentPageComputed = ({ currentPage, totalCount, pageSize }, { setCurrentPage }) => {
+  const totalPages = pageCount(totalCount, pageSize);
+  const adjustedCurrentPage = clamp(currentPage, totalPages - 1);
+  if (currentPage !== adjustedCurrentPage) {
+    setTimeout(() => setCurrentPage(adjustedCurrentPage));
+  }
+  return adjustedCurrentPage;
+};
+
+export const IntegratedPaging = {
+  name: 'IntegratedPaging',
+  render() {
+    return (
+      <Plugin
+        name="IntegratedPaging"
+        dependencies={pluginDependencies}
+      >
+        <Getter name="rows" computed={rowsWithHeadersComputed} />
+        <Getter name="totalCount" computed={totalCountComputed} />
+        <Getter name="currentPage" computed={currentPageComputed} />
+        <Getter name="rows" computed={paginatedRowsComputed} />
+      </Plugin>
+    );
+  },
+};

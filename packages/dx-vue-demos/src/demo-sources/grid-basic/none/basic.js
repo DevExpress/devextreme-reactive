aacc55f5ae892612a7f4050302f4063dc083f93a
Yury Orlov <mpcteam.kvet@gmail.com>
2018-04-17 16:53:34 +0300

    chore(vue-grid): use templates instead of jsx in demos

--- packages/dx-vue-demos/src/demo-sources/grid-basic/none/basic.js
+++ packages/dx-vue-demos/src/demo-sources/grid-basic/none/basic.js
@@ -1,4 +1,8 @@
-import { Grid, Table, TableHeaderRow } from '@devexpress/dx-vue-grid-bootstrap4';
+import {
+  Grid as DxGrid,
+  Table as DxTable,
+  TableHeaderRow as DxTableHeaderRow,
+} from '@devexpress/dx-vue-grid-bootstrap4';
 
 import {
   generateRows,
@@ -6,24 +10,33 @@ import {
 } from '../../../demo-data/generator';
 
 export default {
-  render() {
-    return (
-      <div class="card">
-        <Grid
-          rows={generateRows({ columnValues: globalSalesValues, length: 8 })}
-          columns={[
-            { name: 'region', title: 'Region' },
-            { name: 'sector', title: 'Sector' },
-            { name: 'channel', title: 'Channel' },
-            { name: 'customer', title: 'Customer' },
-            { name: 'product', title: 'Product' },
-            { name: 'amount', title: 'Sale Amount' },
-          ]}
-        >
-          <Table />
-          <TableHeaderRow />
-        </Grid>
-      </div>
-    );
+  data() {
+    return {
+      columns: [
+        { name: 'region', title: 'Region' },
+        { name: 'sector', title: 'Sector' },
+        { name: 'channel', title: 'Channel' },
+        { name: 'customer', title: 'Customer' },
+        { name: 'product', title: 'Product' },
+        { name: 'amount', title: 'Sale Amount' },
+      ],
+      rows: generateRows({ columnValues: globalSalesValues, length: 8 }),
+    };
+  },
+  template: `
+    <div class="card">
+      <dx-grid
+        :rows="rows"
+        :columns="columns"
+      >
+        <dx-table />
+        <dx-table-header-row />
+      </dx-grid>
+    </div>
+  `,
+  components: {
+    DxGrid,
+    DxTable,
+    DxTableHeaderRow,
   },
 };
--- packages/dx-vue-demos/src/demo-sources/grid-sorting/none/basic.js
+++ packages/dx-vue-demos/src/demo-sources/grid-sorting/none/basic.js
@@ -1,11 +1,11 @@
 import {
-  SortingState,
-  IntegratedSorting,
+  SortingState as DxSortingState,
+  IntegratedSorting as DxIntegratedSorting,
 } from '@devexpress/dx-vue-grid';
 import {
-  Grid,
-  Table,
-  TableHeaderRow,
+  Grid as DxGrid,
+  Table as DxTable,
+  TableHeaderRow as DxTableHeaderRow,
 } from '@devexpress/dx-vue-grid-bootstrap4';
 
 import { generateRows } from '../../../demo-data/generator';
@@ -14,28 +14,37 @@ export default {
   data() {
     return {
       sorting: [],
+      columns: [
+        { name: 'name', title: 'Name' },
+        { name: 'sex', title: 'Sex' },
+        { name: 'city', title: 'City' },
+        { name: 'car', title: 'Car' },
+      ],
+      rows: generateRows({ length: 8 }),
     };
   },
-  render() {
-    return (
-      <div class="card">
-        <Grid
-          rows={generateRows({ length: 8 })}
-          columns={[
-            { name: 'name', title: 'Name' },
-            { name: 'sex', title: 'Sex' },
-            { name: 'city', title: 'City' },
-            { name: 'car', title: 'Car' },
-          ]}
-        >
-          <SortingState
-            sorting$sync={this.sorting}
-          />
-          <IntegratedSorting />
-          <Table />
-          <TableHeaderRow showSortingControls />
-        </Grid>
-      </div>
-    );
+  template: `
+    <div class="card">
+      <dx-grid
+        :rows="rows"
+        :columns="columns"
+      >
+        <dx-sorting-state
+          :sorting.sync="sorting"
+        />
+        <dx-integrated-sorting />
+        <dx-table />
+        <dx-table-header-row
+          showSortingControls
+        />
+      </dx-grid>
+    </div>
+  `,
+  components: {
+    DxSortingState,
+    DxIntegratedSorting,
+    DxGrid,
+    DxTable,
+    DxTableHeaderRow,
   },
 };

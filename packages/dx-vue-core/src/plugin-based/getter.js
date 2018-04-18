6b6b0d2a5f54200aaf7fed7790d1a6454efe6f10
Yury Orlov <mpcteam.kvet@gmail.com>
2018-04-18 12:23:31 +0300

    chore(vue-core): calculate getter values once (#943)

--- packages/dx-vue-core/src/plugin-based/getter.js
+++ packages/dx-vue-core/src/plugin-based/getter.js
@@ -1,9 +1,13 @@
 import {
   getAvailableGetters,
+  isTrackedDependenciesChanged,
   getAvailableActions,
 } from './helpers';
 import { PLUGIN_HOST_CONTEXT, POSITION_CONTEXT } from './constants';
 
+const GLOBAL_SHIFT = 0xffff;
+
+let globalGetterId = 0;
 export const Getter = {
   name: 'Getter',
   props: {
@@ -21,21 +25,50 @@ export const Getter = {
     pluginHost: { from: PLUGIN_HOST_CONTEXT },
     position: { from: POSITION_CONTEXT },
   },
+  created() {
+    this.globalId = globalGetterId;
+    globalGetterId += 1;
+    this.internalId = 0;
+    this.generateId();
+  },
   beforeMount() {
     const { pluginHost, name } = this;
+
+    let lastComputed;
+    let lastTrackedDependencies = {};
+    let lastResult;
+    let unwatch;
 
     this.plugin = {
       position: () => this.position(),
       [`${name}Getter`]: (original) => {
         const { value, computed } = this;
-        if (value !== undefined) return value;
+        if (computed === null) return { id: this.id, value };
+
         const getGetterValue = getterName => ((getterName === name)
           ? original
           : pluginHost.get(`${getterName}Getter`, this.plugin));
 
-        const getters = getAvailableGetters(pluginHost, getGetterValue);
+        if (computed === lastComputed &&
+          !isTrackedDependenciesChanged(pluginHost, lastTrackedDependencies, getGetterValue)) {
+          return { id: this.id, value: lastResult };
+        }
+
+        const { getters, trackedDependencies } = getAvailableGetters(pluginHost, getGetterValue);
         const actions = getAvailableActions(pluginHost);
-        return computed(getters, actions);
+
+        lastComputed = computed;
+        lastTrackedDependencies = trackedDependencies;
+        if (unwatch) unwatch();
+        unwatch = this.$watch(
+          () => computed(getters, actions),
+          (newValue) => {
+            this.generateId();
+            lastResult = newValue;
+          },
+          { immediate: true },
+        );
+        return { id: this.id, value: lastResult };
       },
     };
 
@@ -46,5 +79,17 @@ export const Getter = {
   },
   render() {
     return null;
+  },
+  methods: {
+    generateId() {
+      this.internalId = this.internalId + 1 < GLOBAL_SHIFT ? this.internalId + 1 : 0;
+      // eslint-disable-next-line no-bitwise
+      this.id = (this.globalId << GLOBAL_SHIFT) + this.internalId;
+    },
+  },
+  watch: {
+    value() {
+      this.generateId();
+    },
   },
 };

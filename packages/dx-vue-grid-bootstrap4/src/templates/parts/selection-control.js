f2ca195cfb0c168cedc898f8078be557e1873cf5
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2018-04-20 09:54:04 +0300

    Add TableSelectrCell

---
+++ packages/dx-vue-grid-bootstrap4/src/templates/parts/selection-control.js
@@ -0,0 +1,48 @@
+
+export const SelectionControl = {
+  props: {
+    disabled: {
+      type: Boolean,
+    },
+    checked: {
+      type: Boolean,
+    },
+    indeterminate: {
+      type: Boolean,
+    },
+  },
+  attached() {
+    this.$root.indeterminate = this.indeterminate;
+  },
+  render() {
+    const { disabled, checked, indeterminate } = this;
+    const { change: onChange } = this.$listeners;
+    // this.$el = indeterminate;
+    this.$options.indeterminate = indeterminate;
+    this.$root.indeterminate = indeterminate;
+    debugger
+    return (
+      <input
+        class={{
+          'd-inline-block': true,
+          'dx-g-bs4-cursor-pointer': !disabled,
+        }}
+        type="checkbox"
+        disabled={disabled}
+        checked={checked}
+        indeterminate={indeterminate}
+        // ref={(ref) => {
+        //   console.log(this);
+        //   if (!ref) return;
+        //   ref.indeterminate = indeterminate; // eslint-disable-line no-param-reassign
+        // }}
+        ref="my-ref"
+        onChange={() => {
+          if (disabled) return;
+          onChange();
+        }}
+        onClick={e => e.stopPropagation()}
+      />
+    );
+  },
+};

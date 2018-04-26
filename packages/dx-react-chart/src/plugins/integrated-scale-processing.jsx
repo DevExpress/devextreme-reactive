c1f423bb12564d675f9e3cca091af69c44b9de27
Alexey Semikozov <forworknoname@gmail.com>
2018-04-26 14:59:15 +0300

    Refactor integrated scale processing

--- packages/dx-react-chart/src/plugins/integrated-scale-processing.jsx
+++ packages/dx-react-chart/src/plugins/integrated-scale-processing.jsx
@@ -2,13 +2,12 @@ import * as React from 'react';
 import { Plugin, Getter } from '@devexpress/dx-react-core';
 import { domains } from '@devexpress/dx-chart-core';
 
-export const IntegratedScaleProcessing = () => {
-  const computedDomain = ({
-    axes, series, data, argumentAxisName,
-  }) => domains(axes, series, data, argumentAxisName);
-  return ((
-    <Plugin name="IntegratedScaleProcessing">
-      <Getter name="domains" computed={computedDomain} />
-    </Plugin>
-  ));
-};
+const computedDomain = ({
+  axes, series, data, argumentAxisName,
+}) => domains(axes, series, data, argumentAxisName);
+
+export const IntegratedScaleProcessing = () => ((
+  <Plugin name="IntegratedScaleProcessing">
+    <Getter name="domains" computed={computedDomain} />
+  </Plugin>
+));

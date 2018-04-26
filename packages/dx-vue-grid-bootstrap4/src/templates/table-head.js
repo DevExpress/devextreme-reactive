0618c67fd12c57400dcb20694ee9c571a1de107a
Yury Orlov <mpcteam.kvet@gmail.com>
2018-04-26 14:29:40 +0300

    fix formatiing

--- packages/dx-vue-grid-bootstrap4/src/templates/table-head.js
+++ packages/dx-vue-grid-bootstrap4/src/templates/table-head.js
@@ -2,8 +2,8 @@ export const TableHead = {
   render() {
     return (
       <thead
-      {...{ attrs: this.$attrs, on: this.$listeners }}
-    >
+        {...{ attrs: this.$attrs, on: this.$listeners }}
+      >
         {this.$slots.default}
       </thead>
     );

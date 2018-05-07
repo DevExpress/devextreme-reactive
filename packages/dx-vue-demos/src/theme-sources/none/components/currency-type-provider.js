d2e9b338c1d0143264489b696f22d29446b70372
Yury Orlov <mpcteam.kvet@gmail.com>
2018-05-07 17:42:12 +0300

    chore(vue-grid): add remote data demo (#1038)

---
+++ packages/dx-vue-demos/src/theme-sources/none/components/currency-type-provider.js
@@ -0,0 +1,57 @@
+import { DxDataTypeProvider } from '@devexpress/dx-vue-grid';
+
+const CurrencyEditor = {
+  props: {
+    value: {
+      type: Number,
+    },
+  },
+  methods: {
+    handleChange(event) {
+      const { value: targetValue } = event.target;
+      if (targetValue.trim() === '') {
+        this.$emit('valueChange');
+        return;
+      }
+      this.$emit('valueChange', parseInt(targetValue, 10));
+    },
+  },
+  template: `
+    <input
+      type="number"
+      class="form-control text-right"
+      style="'width': '100%'"
+      :value="value === undefined ? '' : value"
+      min="0"
+      @change="handleChange"
+    />
+  `,
+};
+
+const CurrencyFormatter = {
+  props: {
+    value: {
+      type: Number,
+    },
+  },
+  template: `
+    <span>
+      \${{value}}
+    </span>
+  `,
+};
+
+export const CurrencyTypeProvider = {
+  template: `
+    <dx-data-type-provider
+      :formatterComponent="$options.components.CurrencyFormatter"
+      :editorComponent="$options.components.CurrencyEditor"
+      :for="$attrs.for"
+    />
+  `,
+  components: {
+    DxDataTypeProvider,
+    CurrencyFormatter,
+    CurrencyEditor,
+  },
+};

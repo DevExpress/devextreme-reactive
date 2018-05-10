f910b0b660b9f8eb3c57a5aa3293cf971755409f
Alexey Semikozov <forworknoname@gmail.com>
2018-05-10 13:49:09 +0300

    Refactor chart index files

--- packages/dx-react-chart-material-ui/src/index.js
+++ packages/dx-react-chart-material-ui/src/index.js
@@ -1,12 +1,13 @@
 export { Chart } from './chart';
-export { Grid } from './plugins/grid';
 export { Legend } from './plugins/legend';
-export { ValueAxis } from './plugins/value-axis';
+
 export { PieSeries } from './plugins/pie-series';
 export { BarSeries } from './plugins/bar-series';
 export { LineSeries } from './plugins/line-series';
 export { AreaSeries } from './plugins/area-series';
 export { SplineSeries } from './plugins/spline-series';
-export { ArgumentAxis } from './plugins/argument-axis';
 export { ScatterSeries } from './plugins/scatter-series';
 
+export { Grid } from './plugins/grid';
+export { ValueAxis } from './plugins/value-axis';
+export { ArgumentAxis } from './plugins/argument-axis';

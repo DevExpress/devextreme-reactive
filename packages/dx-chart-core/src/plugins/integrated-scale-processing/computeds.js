34259ebf9d411b213be616cf57913ba03457d0dd
Smirnova Yuliya <Krijovnick@users.noreply.github.com>
2018-05-15 16:12:05 +0300

    fix(react-chart): bar rendering with negative values (#1077)

--- packages/dx-chart-core/src/plugins/integrated-scale-processing/computeds.js
+++ packages/dx-chart-core/src/plugins/integrated-scale-processing/computeds.js
@@ -17,12 +17,16 @@ const getAxesDomains = axes =>
     {},
   );
 
-const calculateDomainField = (field, data, domain = [], type) => {
-  const getFieldItem = object => object[field];
+const calculateDomainField = (field1, field2, data, domain = [], type) => {
+  const getFieldItem = field => object => object[field];
   if (type === BAND) {
-    return [...domain, ...data.map(getFieldItem)];
+    return [...domain, ...data.map(getFieldItem(field1))];
   }
-  return extent([...domain, ...extent(data, getFieldItem)]);
+  return extent([
+    ...domain,
+    ...extent(data, getFieldItem(field1)),
+    ...extent(data, getFieldItem(field2)),
+  ]);
 };
 
 const calculateDomain = (series, data, axesDomains, argumentAxisName) =>
@@ -34,6 +38,7 @@ const calculateDomain = (series, data, axesDomains, argumentAxisName) =>
       [axisName]: {
         domain: calculateDomainField(
           `${valueField}-${name}-end`,
+          `${valueField}-${name}-start`,
           data,
           domains[axisName] && domains[axisName].domain,
           domains[axisName] && domains[axisName].type,
@@ -44,6 +49,7 @@ const calculateDomain = (series, data, axesDomains, argumentAxisName) =>
       [argumentAxisName]: {
         domain: calculateDomainField(
           argumentField,
+          undefined,
           data,
           domains[argumentAxisName] && domains[argumentAxisName].domain,
           domains[argumentAxisName] && domains[argumentAxisName].type,

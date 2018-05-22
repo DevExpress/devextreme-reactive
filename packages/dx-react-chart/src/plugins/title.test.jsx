777c0756812675251754b2c12002cb9c680eb7d9
alexey-semikozov <forworknoname@gmail.com>
2018-05-22 09:22:23 +0300

    chore(react-chart): add title component (#1105)
    * Add title component

---
+++ packages/dx-react-chart/src/plugins/title.test.jsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import { PluginHost, Template } from '@devexpress/dx-react-core';
+import { pluginDepsToComponents } from '@devexpress/dx-react-core/test-utils';
+import { Title } from './title';
+
+const textComponent = () => null;
+
+const defaultDeps = {
+  template: {
+    top: {},
+  },
+};
+
+const defaultProps = {
+  text: 'chart',
+  textComponent,
+};
+
+describe('Title', () => {
+  it('should render text', () => {
+    const tree = mount((
+      <PluginHost>
+        {pluginDepsToComponents(defaultDeps)}
+
+        <Title
+          {...defaultProps}
+        />
+      </PluginHost>));
+
+    expect(tree.find(textComponent).props().text)
+      .toBe('chart');
+  });
+
+  it('should render other template with same name', () => {
+    const tree = mount((
+      <PluginHost>
+        {pluginDepsToComponents(defaultDeps)}
+
+        <Template name="top">
+          <h1>other template</h1>
+        </Template>
+        <Title
+          {...defaultProps}
+        />
+      </PluginHost>));
+
+    expect(tree.find(textComponent).props().text)
+      .toBe('chart');
+    expect(tree.find('h1').text())
+      .toBe('other template');
+  });
+});

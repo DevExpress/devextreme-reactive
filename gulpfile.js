ec79296a1725229dd880e87cf97839079743e3cd
alexey-semikozov <forworknoname@gmail.com>
2018-06-05 12:06:30 +0300

    chore(react-chart): add contributions demo (#1134)
    * Add demo
    
    * Add demos to react/chart site
    
    * Fix doc
    
    * Fix bar point shape
    
    * Refactor demo
    
    * Refactor demo
    
    * Fix after review

--- gulpfile.js
+++ gulpfile.js
@@ -105,6 +105,7 @@ gulp.task('site:docs', function() {
       'packages/dx-react-core/docs/*/*.md',
       'packages/dx-react-grid/demos/*/*.md',
       'packages/dx-react-grid/docs/*/*.md',
+      'packages/dx-react-chart/demos/*/*.md',
       'packages/dx-react-chart/docs/*/*.md',
       'packages/dx-vue-grid/demos/*/*.md',
       'packages/dx-vue-grid/docs/*/*.md',

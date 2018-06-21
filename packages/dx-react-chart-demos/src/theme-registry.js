85add12413692bd1f5dda7dacefe85185f97e51b
Smirnova Yuliya <yuliya.smirnova@devexpress.com>
2018-06-21 17:58:02 +0300

    chore(react-chart): split react and chart demos

---
+++ packages/dx-react-chart-demos/src/theme-registry.js
@@ -0,0 +1,43 @@
+/* eslint-disable global-require */
+
+export const themes = [{
+  name: 'material-ui',
+  title: 'Material UI',
+  variants: [{
+    name: 'light',
+    title: 'Material UI Light',
+    DemoContainer: require('./theme-sources/material-ui/demo-container').default,
+  }, {
+    name: 'dark',
+    title: 'Material UI Dark',
+    DemoContainer: require('./theme-sources/material-ui/demo-container').Dark,
+  }],
+}, {
+  name: 'bootstrap4',
+  title: 'Bootstrap 4',
+  variants: [{
+    name: 'default',
+    title: 'Bootstrap 4',
+    DemoContainer: require('./theme-sources/bootstrap4/demo-container').default,
+    links: [
+      'https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.css',
+      'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
+    ],
+  }, {
+    name: 'cyborg',
+    title: 'Bootstrap 4 (Bootswatch Cyborg)',
+    DemoContainer: require('./theme-sources/bootstrap4/demo-container').default,
+    links: [
+      'https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.css',
+      'https://bootswatch.com/4/cyborg/bootstrap.min.css',
+    ],
+  }, {
+    name: 'custom',
+    title: 'Bootstrap 4 (Custom)',
+    DemoContainer: require('./theme-sources/bootstrap4/demo-container').default,
+    links: [
+      'https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.css',
+    ],
+    editableLink: 'https://bootswatch.com/4/sketchy/bootstrap.min.css',
+  }],
+}];
---
+++ packages/dx-react-grid-demos/src/theme-sources/bootstrap4/demo-container.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import '@devexpress/dx-react-grid-bootstrap4/dist/dx-react-grid-bootstrap4.css';
+
+const DemoContainer = ({ children }) => <div>{children}</div>;
+
+DemoContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default DemoContainer;

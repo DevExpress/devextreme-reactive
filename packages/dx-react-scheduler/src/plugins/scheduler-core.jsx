2da61c51cd3ef05a13a7fe7ac210baaeca461c0e
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-06-22 11:45:41 +0300

    Index files

---
+++ packages/dx-react-scheduler/src/plugins/scheduler-core.jsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import { Plugin, Getter, Template, TemplatePlaceholder } from '@devexpress/dx-react-core';
+
+export class SchedulerCore extends React.PureComponent {
+  render() {
+    const {
+      data,
+      rootComponent: Root,
+    } = this.props;
+
+    return (
+      <Plugin>
+        <Getter name="data" value={data} />
+        <Template name="root">
+          <Root>
+            <TemplatePlaceholder name="header" />
+            <TemplatePlaceholder name="body" />
+            <TemplatePlaceholder name="footer" />
+          </Root>
+        </Template>
+      </Plugin>
+    );
+  }
+}
+
+SchedulerCore.propTypes = {
+  data: PropTypes.array.isRequired,
+  rootComponent: PropTypes.func.isRequired,
+};

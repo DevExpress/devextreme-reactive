fdc511c12f23cd110beeaa8aec64c004756e6573
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2018-06-25 19:11:29 +0300

    Add the dayUnits computed

--- packages/dx-scheduler-core/src/plugins/view/computeds.js
+++ packages/dx-scheduler-core/src/plugins/view/computeds.js
@@ -16,3 +16,23 @@ export const timeUnits = (startDayHour = 0, endDayHour = 24, cellDuration = 30) 
 
   return result;
 };
+
+export const dayUnits = (
+  currentDate = new Date(),
+  firsDayOfWeek = 0,
+  dayCount = 7,
+  except = [],
+) => {
+  const result = [];
+  const date = moment(currentDate).startOf('hour');
+  date.day(firsDayOfWeek);
+
+  for (let index = 0; index < dayCount; index += 1) {
+    if (except.findIndex(item => item === date.day()) === -1) {
+      result.push(date.format());
+    }
+    date.add(1, 'days');
+  }
+
+  return result;
+};

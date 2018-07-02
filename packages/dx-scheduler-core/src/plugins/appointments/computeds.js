74ba5d07ed22d584ebe9134cee150efb63c300b6
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2018-07-02 18:43:10 +0300

    Add sliceAppointments computed

--- packages/dx-scheduler-core/src/plugins/appointments/computeds.js
+++ packages/dx-scheduler-core/src/plugins/appointments/computeds.js
@@ -24,3 +24,49 @@ export const filteredAppointments = (
     );
   })
 );
+
+export const sliceAppointments = (appointments, startViewDate, endViewDate) => {
+  const nextAppointments = [];
+  appointments.forEach((appointment) => {
+    const appointmentStartViewStart = moment(startViewDate).date(moment(appointment.start).date());
+    const appointmentStartViewEnd = moment(endViewDate).date(moment(appointment.start).date());
+    const appointmentEndViewStart = moment(startViewDate).date(moment(appointment.end).date());
+    const appointmentEndViewEnd = moment(endViewDate).date(moment(appointment.end).date());
+
+    if (moment(appointment.start).isBefore(startViewDate)) { // start before startViewDate
+      if (moment(appointment.end).isBefore(appointmentEndViewEnd)) {
+        nextAppointments.push({
+          dataItem: appointment.dataItem, start: startViewDate, end: appointment.end,
+        });
+      } else {
+        nextAppointments.push({
+          dataItem: appointment.dataItem, start: startViewDate, end: appointmentEndViewEnd.toDate(),
+        });
+      }
+    } else if (moment(appointment.end).isAfter(endViewDate)) { // end after endViewDate
+      if (moment(appointment.start).isBefore(appointmentStartViewStart)) {
+        nextAppointments.push({
+          dataItem: appointment.dataItem,
+          start: appointmentStartViewStart.toDate(),
+          end: endViewDate,
+        });
+      } else {
+        nextAppointments.push({
+          dataItem: appointment.dataItem,
+          start: appointment.start,
+          end: endViewDate,
+        });
+      }
+    } else if (moment(appointment.start).isBefore(appointmentStartViewEnd)) {
+      if (moment(appointment.end).isBefore(appointmentStartViewEnd)) {
+        nextAppointments.push(appointment);
+      } else if (moment(appointment.end).isAfter(appointmentStartViewEnd) && moment(appointment.end).isBefore(appointmentEndViewStart)) {
+        nextAppointments.push({ dataItem: appointment.dataItem, start: appointment.start, end: appointmentStartViewEnd.toDate() });
+      } else {
+        nextAppointments.push({ dataItem: appointment.dataItem, start: appointment.start, end: appointmentStartViewEnd.toDate() });
+        nextAppointments.push({ dataItem: appointment.dataItem, start: appointmentEndViewStart.toDate(), end: appointment.end });
+      }
+    }
+  });
+  return nextAppointments;
+};
--- packages/dx-scheduler-core/src/plugins/appointments/computeds.test.js
+++ packages/dx-scheduler-core/src/plugins/appointments/computeds.test.js
@@ -1,4 +1,4 @@
-import { filteredAppointments } from './computeds';
+import { filteredAppointments, sliceAppointments } from './computeds';
 
 describe('Appointment computeds', () => {
   describe('#filteredAppointments', () => {
@@ -29,6 +29,67 @@ describe('Appointment computeds', () => {
         appointments[1],
         appointments[2],
         appointments[3],
+      ]);
+    });
+  });
+
+  describe('#sliceAppointments', () => {
+    const startViewDate = new Date(2018, 5, 27, 10);
+    const endViewDate = new Date(2018, 5, 29, 15);
+    const timeScale = [new Date(2018, 6, 22, 10), new Date(2018, 6, 23, 15)];
+    it('should slice if appointment `start` before startViewDate and `end` before endViewHour', () => {
+      const appointments = [
+        { start: new Date(2018, 5, 26, 9), end: new Date(2018, 5, 27, 11), dataItem: {} },
+      ];
+      const slicedAppointments = sliceAppointments(appointments, startViewDate, endViewDate, timeScale);
+
+      expect(slicedAppointments).toEqual([
+        { start: new Date(2018, 5, 27, 10), end: new Date(2018, 5, 27, 11), dataItem: {} },
+      ]);
+    });
+
+    it('should slice if appointment `start` before endViewHour and `end` after endViewDate', () => {
+      const appointments = [
+        { start: new Date(2018, 5, 29, 12), end: new Date(2018, 5, 30, 11), dataItem: {} },
+      ];
+      const slicedAppointments = sliceAppointments(appointments, startViewDate, endViewDate, timeScale);
+
+      expect(slicedAppointments).toEqual([
+        { start: new Date(2018, 5, 29, 12), end: new Date(2018, 5, 29, 15), dataItem: {} },
+      ]);
+    });
+
+    it('should slice if appointment `start` before endViewDate and `end` after endViewHour', () => {
+      const appointments = [
+        { start: new Date(2018, 5, 26, 12), end: new Date(2018, 5, 27, 16), dataItem: {} },
+      ];
+      const slicedAppointments = sliceAppointments(appointments, startViewDate, endViewDate, timeScale);
+
+      expect(slicedAppointments).toEqual([
+        { start: new Date(2018, 5, 27, 10), end: new Date(2018, 5, 27, 15), dataItem: {} },
+      ]);
+    });
+
+    it('should slice if appointment `start` before startViewHour and `end` after endViewDate', () => {
+      const appointments = [
+        { start: new Date(2018, 5, 26, 12), end: new Date(2018, 5, 27, 16), dataItem: {} },
+      ];
+      const slicedAppointments = sliceAppointments(appointments, startViewDate, endViewDate, timeScale);
+
+      expect(slicedAppointments).toEqual([
+        { start: new Date(2018, 5, 27, 10), end: new Date(2018, 5, 27, 15), dataItem: {} },
+      ]);
+    });
+
+    it('should slice if appointment `start` before endViewHour and `end` after startViewHour', () => {
+      const appointments = [
+        { start: new Date(2018, 5, 27, 14), end: new Date(2018, 5, 28, 12), dataItem: {} },
+      ];
+      const slicedAppointments = sliceAppointments(appointments, startViewDate, endViewDate, timeScale);
+
+      expect(slicedAppointments).toEqual([
+        { start: new Date(2018, 5, 27, 14), end: new Date(2018, 5, 27, 15), dataItem: {} },
+        { start: new Date(2018, 5, 28, 10), end: new Date(2018, 5, 28, 12), dataItem: {} },
       ]);
     });
   });

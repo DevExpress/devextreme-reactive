37d1a1de3bd3db747ba95c4d7bfc9f89d9198be2
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2018-07-05 11:43:24 +0300

    Add the Container component for appointments

--- packages/dx-react-scheduler-material-ui/src/plugins/appointments.jsx
+++ packages/dx-react-scheduler-material-ui/src/plugins/appointments.jsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import { Appointments as AppointmentsBase } from '@devexpress/dx-react-scheduler';
 import { Appointment as AppointmentComponent } from '../templates/appointment/appointment';
+import { Container } from '../templates/appointment/container';
 
 export class Appointments extends React.PureComponent {
   render() {
     return (
       <AppointmentsBase
         appointmentComponent={AppointmentComponent}
+        containerComponent={Container}
         {...this.props}
       />
     );

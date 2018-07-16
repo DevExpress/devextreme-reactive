a5e17e5b4431f448678b830197e15f010bcbd44f
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-07-16 12:09:05 +0300

    Calculate viewBoundTitle

--- packages/dx-scheduler-core/src/index.js
+++ packages/dx-scheduler-core/src/index.js
@@ -3,3 +3,4 @@ export * from './plugins/scheduler-core/computeds';
 export * from './plugins/week-view/computeds';
 export * from './plugins/month-view/computeds';
 export * from './plugins/view-state/reducers';
+export * from './plugins/date-navigator/computeds';

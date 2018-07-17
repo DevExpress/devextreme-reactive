800b0c495642335ba99c70a4c4014d8646f29b08
MaximKudriavtsev <maxim_71rus_wow@mail.ru>
2018-07-17 15:10:09 +0300

    Initial commit

--- packages/dx-react-scheduler-demos/src/demo-data/month-appointments.js
+++ packages/dx-react-scheduler-demos/src/demo-data/month-appointments.js
@@ -1,94 +1,94 @@
 export const appointments = [
   {
-    text: 'Website Re-Design Plan',
+    title: 'Website Re-Design Plan',
     startDate: new Date(2018, 6, 24, 9, 30),
     endDate: new Date(2018, 6, 24, 11, 30),
   }, {
-    text: 'Book Flights to San Fran for Sales Trip',
+    title: 'Book Flights to San Fran for Sales Trip',
     startDate: new Date(2018, 6, 24, 12, 0),
     endDate: new Date(2018, 6, 24, 13, 0),
   }, {
-    text: 'Install New Router in Dev Room',
+    title: 'Install New Router in Dev Room',
     startDate: new Date(2018, 6, 24, 14, 30),
     endDate: new Date(2018, 6, 24, 15, 30),
   }, {
-    text: 'Approve Personal Computer Upgrade Plan',
+    title: 'Approve Personal Computer Upgrade Plan',
     startDate: new Date(2018, 6, 25, 10, 0),
     endDate: new Date(2018, 6, 25, 11, 0),
   }, {
-    text: 'Final Budget Review',
+    title: 'Final Budget Review',
     startDate: new Date(2018, 6, 25, 12, 0),
     endDate: new Date(2018, 6, 25, 13, 35),
   }, {
-    text: 'New Brochures',
+    title: 'New Brochures',
     startDate: new Date(2018, 6, 25, 14, 30),
     endDate: new Date(2018, 6, 25, 15, 45),
   }, {
-    text: 'Install New Database',
+    title: 'Install New Database',
     startDate: new Date(2018, 6, 26, 9, 45),
     endDate: new Date(2018, 6, 26, 11, 15),
   }, {
-    text: 'Approve New Online Marketing Strategy',
+    title: 'Approve New Online Marketing Strategy',
     startDate: new Date(2018, 6, 26, 12, 0),
     endDate: new Date(2018, 6, 26, 14, 0),
   }, {
-    text: 'Upgrade Personal Computers',
+    title: 'Upgrade Personal Computers',
     startDate: new Date(2018, 6, 26, 15, 15),
     endDate: new Date(2018, 6, 26, 16, 30),
   }, {
-    text: 'Customer Workshop',
+    title: 'Customer Workshop',
     startDate: new Date(2018, 6, 27, 11, 0),
     endDate: new Date(2018, 6, 27, 12, 0),
   }, {
-    text: 'Prepare 2015 Marketing Plan',
+    title: 'Prepare 2015 Marketing Plan',
     startDate: new Date(2018, 6, 27, 11, 0),
     endDate: new Date(2018, 6, 27, 13, 30),
   }, {
-    text: 'Brochure Design Review',
+    title: 'Brochure Design Review',
     startDate: new Date(2018, 6, 27, 14, 0),
     endDate: new Date(2018, 6, 27, 15, 30),
   }, {
-    text: 'Create Icons for Website',
+    title: 'Create Icons for Website',
     startDate: new Date(2018, 6, 28, 10, 0),
     endDate: new Date(2018, 6, 28, 11, 30),
   }, {
-    text: 'Upgrade Server Hardware',
+    title: 'Upgrade Server Hardware',
     startDate: new Date(2018, 6, 28, 14, 30),
     endDate: new Date(2018, 6, 28, 16, 0),
   }, {
-    text: 'Submit New Website Design',
+    title: 'Submit New Website Design',
     startDate: new Date(2018, 6, 28, 16, 30),
     endDate: new Date(2018, 6, 28, 18, 0),
   }, {
-    text: 'Launch New Website',
+    title: 'Launch New Website',
     startDate: new Date(2018, 6, 27, 12, 20),
     endDate: new Date(2018, 6, 27, 14, 0),
   }, {
-    text: 'Website Re-Design Plan',
+    title: 'Website Re-Design Plan',
     startDate: new Date(2018, 6, 17, 9, 30),
     endDate: new Date(2018, 6, 17, 15, 30),
   }, {
-    text: 'Book Flights to San Fran for Sales Trip',
+    title: 'Book Flights to San Fran for Sales Trip',
     startDate: new Date(2018, 6, 17, 12, 0),
     endDate: new Date(2018, 6, 17, 13, 0),
   }, {
-    text: 'Install New Database',
+    title: 'Install New Database',
     startDate: new Date(2018, 6, 18, 15, 45),
     endDate: new Date(2018, 6, 19, 12, 15),
   }, {
-    text: 'Approve New Online Marketing Strategy',
+    title: 'Approve New Online Marketing Strategy',
     startDate: new Date(2018, 6, 19, 12, 35),
     endDate: new Date(2018, 6, 19, 14, 15),
   }, {
-    text: 'Upgrade Personal Computers',
+    title: 'Upgrade Personal Computers',
     startDate: new Date(2018, 6, 20, 15, 15),
     endDate: new Date(2018, 6, 21, 20, 30),
   }, {
-    text: 'Prepare 2015 Marketing Plan',
+    title: 'Prepare 2015 Marketing Plan',
     startDate: new Date(2018, 6, 21, 20, 0),
     endDate: new Date(2018, 6, 21, 13, 30),
   }, {
-    text: 'Brochure Design Review',
+    title: 'Brochure Design Review',
     startDate: new Date(2018, 6, 21, 14, 10),
     endDate: new Date(2018, 6, 21, 15, 30),
   },
---
+++ packages/dx-react-scheduler-demos/src/demo-sources/sheduler-view-switcher/material-ui/view-switcher-uncontrolled.jsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import Paper from '@material-ui/core/Paper';
+
+import { ViewState } from '@devexpress/dx-react-scheduler';
+
+import {
+  Scheduler,
+  WeekView,
+  Toolbar,
+  DateNavigator,
+  Appointments,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+import { appointments } from '../../../demo-data/month-appointments';
+
+export default class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: appointments,
+    };
+  }
+  render() {
+    const { data } = this.state;
+
+    return (
+      <Paper>
+        <Scheduler
+          data={data}
+        >
+          <ViewState
+            defaultCurrentDate="2018-07-25"
+          />
+          <Toolbar />
+          <WeekView
+            startDayHour={9}
+            endDayHour={19}
+            firstDayOfWeek={1}
+          />
+          <DateNavigator />
+          <Appointments />
+        </Scheduler>
+      </Paper>
+    );
+  }
+}

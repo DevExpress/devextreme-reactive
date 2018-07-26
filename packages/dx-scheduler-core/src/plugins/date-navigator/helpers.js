3bac33468e71c64c889edcbd8d706c2a39732326
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-07-26 15:19:55 +0300

    Reduce moment usage

--- packages/dx-scheduler-core/src/plugins/date-navigator/helpers.js
+++ packages/dx-scheduler-core/src/plugins/date-navigator/helpers.js
@@ -1,23 +1,25 @@
 import moment from 'moment';
 
 export const viewBoundTitle = (startViewDate, endViewDate, step) => {
+  const momentStartViewDate = moment(startViewDate);
+  const momentEndViewDate = moment(endViewDate);
   if (step !== 'month') {
-    if (moment(startViewDate).isSame(endViewDate, 'day')) {
-      return moment(startViewDate).format('D MMMM YYYY');
+    if (momentStartViewDate.isSame(momentEndViewDate, 'day')) {
+      return momentStartViewDate.format('D MMMM YYYY');
     }
-    if (moment(startViewDate).isSame(endViewDate, 'year')) {
-      if (moment(startViewDate).isSame(endViewDate, 'month')) {
-        return `${moment(startViewDate).date()}-${moment(endViewDate).format('D MMMM YYYY')}`;
+    if (momentStartViewDate.isSame(momentEndViewDate, 'year')) {
+      if (momentStartViewDate.isSame(momentEndViewDate, 'month')) {
+        return `${momentStartViewDate.date()}-${momentEndViewDate.format('D MMMM YYYY')}`;
       }
-      return `${moment(startViewDate).format('D MMM')} - ${moment(endViewDate).format('D MMM YYYY')}`;
+      return `${momentStartViewDate.format('D MMM')} - ${momentEndViewDate.format('D MMM YYYY')}`;
     }
-    return `${moment(startViewDate).format('D MMM YY')} - ${moment(endViewDate).format('D MMM YY')}`;
+    return `${momentStartViewDate.format('D MMM YY')} - ${momentEndViewDate.format('D MMM YY')}`;
   }
-  if (moment(startViewDate).isSame(endViewDate, 'year')) {
-    if (moment(startViewDate).isSame(endViewDate, 'month')) {
-      return moment(startViewDate).format('MMMM YYYY');
+  if (momentStartViewDate.isSame(momentEndViewDate, 'year')) {
+    if (momentStartViewDate.isSame(momentEndViewDate, 'month')) {
+      return momentStartViewDate.format('MMMM YYYY');
     }
-    return `${moment(startViewDate).format('MMM')}-${moment(endViewDate).format('MMM YYYY')}`;
+    return `${momentStartViewDate.format('MMM')}-${momentEndViewDate.format('MMM YYYY')}`;
   }
-  return `${moment(startViewDate).format('MMM YY')} - ${moment(endViewDate).format('MMM YY')}`;
+  return `${momentStartViewDate.format('MMM YY')} - ${momentEndViewDate.format('MMM YY')}`;
 };

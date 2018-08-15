672e97d39a20c7acfac15607e3e048e931aefc3d
Yury Orlov <mpcteam.kvet@gmail.com>
2018-08-15 14:26:49 +0300

    chore: allow to specify tests for specific demos (#1302)

--- packages/dx-react-grid-demos/src/demo-sources/grid-featured-chart-integration/material-ui/demo.test.jsx
+++ packages/dx-react-grid-demos/src/demo-sources/grid-featured-chart-integration/material-ui/demo.test.jsx
@@ -1,9 +1,17 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
 import { setupConsole } from '@devexpress/dx-testing';
-import Demo from './<%&demoName%>';
+import Demo from './demo';
 
 setupConsole();
+
+Element.prototype.getBBox = () => {};
+jest.spyOn(Element.prototype, 'getBBox').mockImplementation(() => ({
+  x: 0,
+  y: 0,
+  width: 800,
+  height: 600,
+}));
 
 jest.spyOn(Element.prototype, 'getBoundingClientRect').mockImplementation(() => ({
   top: 0,

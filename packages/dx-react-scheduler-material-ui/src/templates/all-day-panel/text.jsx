eebbf18ded64a23a871fd7174ed303003c5c687a
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-08-24 09:44:09 +0300

    getBorderColor -> getBorder

--- packages/dx-react-scheduler-material-ui/src/templates/all-day-panel/text.jsx
+++ packages/dx-react-scheduler-material-ui/src/templates/all-day-panel/text.jsx
@@ -3,7 +3,7 @@ import * as PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
-import { getBorderColor } from '../utils';
+import { getBorder } from '../utils';
 
 const styles = theme => ({
   container: {
@@ -14,7 +14,7 @@ const styles = theme => ({
     position: 'relative',
   },
   content: {
-    borderTop: getBorderColor(theme),
+    borderTop: getBorder(theme),
     height: theme.spacing.unit * 7,
     textAlign: 'right',
     display: 'flex',

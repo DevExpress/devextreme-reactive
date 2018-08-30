006a1e7fc3c11dac87ff67f5d0921dd6f115d24a
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-08-30 16:56:57 +0300

    Build bundles separately

--- packages/dx-react-scheduler-material-ui/rollup.config.js
+++ packages/dx-react-scheduler-material-ui/rollup.config.js
@@ -1,32 +1,53 @@
 import babel from 'rollup-plugin-babel';
 import license from 'rollup-plugin-license';
 import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import { banner, external, babelrc } from '../../tools/rollup-utils';
+import { banner, external, babelrc, stubProcess } from '../../tools/rollup-utils';
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.js',
-  sourcemap: true,
-  output: [
-    { file: pkg.main, format: 'cjs' },
-    { file: pkg.module, format: 'es' },
-  ],
-  external: external(__dirname),
-  plugins: [
-    resolve({
-      main: false,
-      extensions: ['.js', '.jsx'],
-    }),
-    babel(Object.assign({
-      babelrc: false,
-      runtimeHelpers: true,
-      exclude: 'node_modules/**',
-    }, babelrc(__dirname))),
-    replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-    license({
-      banner,
-    }),
-  ],
-};
-
+export default [
+  {
+    input: 'src/index.js',
+    sourcemap: true,
+    output: [
+      { file: pkg.main, format: 'cjs' },
+    ],
+    external: external(__dirname),
+    intro: stubProcess,
+    plugins: [
+      resolve({
+        main: false,
+        extensions: ['.js', '.jsx'],
+      }),
+      babel(Object.assign({
+        babelrc: false,
+        runtimeHelpers: true,
+        exclude: 'node_modules/**',
+      }, babelrc(__dirname))),
+      license({
+        banner,
+      }),
+    ],
+  },
+  {
+    input: 'src/index.js',
+    sourcemap: true,
+    output: [
+      { file: pkg.module, format: 'es' },
+    ],
+    external: external(__dirname),
+    plugins: [
+      resolve({
+        main: false,
+        extensions: ['.js', '.jsx'],
+      }),
+      babel(Object.assign({
+        babelrc: false,
+        runtimeHelpers: true,
+        exclude: 'node_modules/**',
+      }, babelrc(__dirname))),
+      license({
+        banner,
+      }),
+    ],
+  },
+];

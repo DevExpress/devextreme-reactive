bb33d6ad186b2a36bb7d6cfbdb553fc196238ccf
Yury Orlov <mpcteam.kvet@gmail.com>
2018-09-11 19:40:07 +0300

    chore: add test watch task that runs all packages

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  projects: [
+    'packages/dx-chart-core/',
+    'packages/dx-core/',
+    'packages/dx-grid-core/',
+    'packages/dx-react-chart/jest.config.js',
+    'packages/dx-react-chart-bootstrap4/jest.config.js',
+    'packages/dx-react-chart-demos/jest.config.js',
+    'packages/dx-react-chart-material-ui/jest.config.js',
+    'packages/dx-react-core/jest.config.js',
+    'packages/dx-react-grid/jest.config.js',
+    'packages/dx-react-grid-bootstrap3/jest.config.js',
+    'packages/dx-react-grid-bootstrap4/jest.config.js',
+    'packages/dx-react-grid-material-ui/jest.config.js',
+    'packages/dx-react-scheduler/jest.config.js',
+    'packages/dx-react-scheduler-demos/jest.config.js',
+    'packages/dx-react-scheduler-material-ui/jest.config.js',
+    'packages/dx-scheduler-core/',
+    'packages/dx-vue-core/jest.config.js',
+    'packages/dx-vue-grid/jest.config.js',
+    'packages/dx-vue-grid-bootstrap4/jest.config.js',
+    'packages/dx-vue-grid-demos/jest.config.js',
+  ]
+}

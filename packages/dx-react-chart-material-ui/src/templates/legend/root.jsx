c790805d9526f71af34806bf6aeb4ca7b316fd3d
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-09-14 09:34:39 +0300

    Use existing theme related HOCs in Legend components

--- packages/dx-react-chart-material-ui/src/templates/legend/root.jsx
+++ packages/dx-react-chart-material-ui/src/templates/legend/root.jsx
@@ -1,8 +1,5 @@
-import * as React from 'react';
-import * as PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
+import { withClassName } from '../utils';
 
 const styles = () => ({
   root: {
@@ -10,33 +7,5 @@ const styles = () => ({
     paddingBottom: 16,
   },
 });
-class RootBase extends React.PureComponent {
-  render() {
-    const {
-      children,
-      classes,
-      className,
-      ...restProps
-    } = this.props;
-    return (
-      <List
-        className={classNames(classes.root, className)}
-        {...restProps}
-      >
-        {children}
-      </List>
-    );
-  }
-}
 
-RootBase.propTypes = {
-  children: PropTypes.node.isRequired,
-  classes: PropTypes.object.isRequired,
-  className: PropTypes.string,
-};
-
-RootBase.defaultProps = {
-  className: undefined,
-};
-
-export const Root = withStyles(styles, { name: 'LegendRoot' })(RootBase);
+export const Root = withClassName(styles, { name: 'LegendRoot' })(List);
--- packages/dx-react-chart-material-ui/src/templates/utils.jsx
+++ packages/dx-react-chart-material-ui/src/templates/utils.jsx
@@ -9,11 +9,11 @@ export const getBorderColor = theme => (
     : darken(fade(theme.palette.divider, 1), 0.8)
 );
 
-export const withClassName = (styles) => {
+export const withClassName = (...args) => {
   const setClassName = ({ className, classes, ...restProps }) => ({
     ...restProps,
     className: classNames(classes.root, className),
   });
   // TODO: First candidate to `compose` util?
-  return Target => withStyles(styles)(withPatchedProps(setClassName)(Target));
+  return Target => withStyles(...args)(withPatchedProps(setClassName)(Target));
 };

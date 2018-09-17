e3bc39341633d651ac0ebbfc9bb500896daa3a88
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2018-09-17 10:54:31 +0300

    Remove excess files

--- packages/dx-react-scheduler-material-ui/src/plugins/day-view.jsx
+++ packages/dx-react-scheduler-material-ui/src/plugins/day-view.jsx
@@ -3,14 +3,17 @@ import { DayView as DayViewBase } from '@devexpress/dx-react-scheduler';
 import { WeekLayout } from '../templates/views/week-layout';
 
 import { Row } from '../templates/day-view/row';
-import { EmptySpace } from '../templates/day-view/empty-space';
+import { EmptySpace } from '../templates/week-view/empty-space';
 import { Container } from '../templates/appointment/container';
 
 import { Layout as TimePanelLayout } from '../templates/day-view/time-panel/layout';
 import { Cell as TimePanelCell } from '../templates/day-view/time-panel/cell';
 
-import { Layout as DateTableLayout } from '../templates/day-view/date-table/layout';
-import { Cell as DateTableCell } from '../templates/day-view/date-table/cell';
+import { Layout as DateTableLayout } from '../templates/week-view/date-table/layout';
+import { Cell as DateTableCell } from '../templates/week-view/date-table/cell';
+
+import { Layout as DayPanelLayout } from '../templates/week-view/day-panel/layout';
+import { Cell as DayPanelCell } from '../templates/week-view/day-panel/cell';
 
 export class DayView extends React.PureComponent {
   render() {
@@ -22,6 +25,8 @@ export class DayView extends React.PureComponent {
         timePanelLayoutComponent={TimePanelLayout}
         timePanelCellComponent={TimePanelCell}
         timePanelRowComponent={Row}
+        dayPanelLayoutComponent={DayPanelLayout}
+        dayPanelCellComponent={DayPanelCell}
         dayPanelRowComponent={Row}
         dateTableLayoutComponent={DateTableLayout}
         dateTableCellComponent={DateTableCell}

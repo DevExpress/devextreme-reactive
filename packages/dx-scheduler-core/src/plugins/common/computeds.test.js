da8dc458ddcc71230eb6fa05c42f2679d6573f19
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2018-09-17 10:51:59 +0300

    Change dayScale computed

--- packages/dx-scheduler-core/src/plugins/common/computeds.test.js
+++ packages/dx-scheduler-core/src/plugins/common/computeds.test.js
@@ -3,17 +3,17 @@ import { dayScale, availableViews } from './computeds';
 describe('#dayScale', () => {
   const currentDate = new Date(2018, 5, 24);
   it('should return default day units', () => {
-    const units = dayScale();
+    const units = dayScale(currentDate, 0, 7);
     expect(units).toHaveLength(7);
   });
 
   it('should return day units depend on first day of week', () => {
-    let units = dayScale('2018-07-01', 1);
+    let units = dayScale('2018-07-01', 1, 7);
 
     expect(units[0]).toEqual(new Date(2018, 5, 25));
     expect(units[6]).toEqual(new Date(2018, 6, 1));
 
-    units = dayScale(currentDate, 3);
+    units = dayScale(currentDate, 3, 7);
 
     expect(units[0]).toEqual(new Date(2018, 5, 20));
     expect(units[6]).toEqual(new Date(2018, 5, 26));
@@ -45,6 +45,20 @@ describe('#dayScale', () => {
     expect(units).toHaveLength(3);
     expect(units[0]).toEqual(currentDate);
     expect(units[units.length - 1]).toEqual(new Date(2018, 5, 28));
+  });
+
+  it('should return day units for one day', () => {
+    const units = dayScale('2018-07-01', undefined, 1);
+
+    expect(units[0]).toEqual(new Date(2018, 6, 1));
+  });
+
+  it('should return day units for day depend on day count', () => {
+    const units = dayScale('2018-07-01', undefined, 3);
+
+    expect(units[0]).toEqual(new Date(2018, 6, 1));
+    expect(units[1]).toEqual(new Date(2018, 6, 2));
+    expect(units[2]).toEqual(new Date(2018, 6, 3));
   });
 });
 describe('#availableViews', () => {

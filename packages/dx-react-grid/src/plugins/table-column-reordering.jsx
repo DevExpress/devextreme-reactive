4e188f30be52b735bd49f0821a80908622fa8726
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-09-17 16:28:46 +0300

    Update TableColumnReordering

--- packages/dx-react-grid/src/plugins/table-column-reordering.jsx
+++ packages/dx-react-grid/src/plugins/table-column-reordering.jsx
@@ -6,6 +6,7 @@ import {
   Template,
   TemplatePlaceholder,
   TemplateConnector,
+  DropTarget
 } from '@devexpress/dx-react-core';
 import {
   TABLE_DATA_TYPE,
@@ -16,6 +17,7 @@ import {
   tableHeaderRowsWithReordering,
   draftOrder as draftOrderComputed,
 } from '@devexpress/dx-grid-core';
+import { withComponents } from '../utils/with-components';
 
 const pluginDependencies = [
   { name: 'Table' },
@@ -25,7 +27,7 @@ const tableHeaderRowsComputed = (
   { tableHeaderRows },
 ) => tableHeaderRowsWithReordering(tableHeaderRows);
 
-export class TableColumnReordering extends React.PureComponent {
+class RawTableColumnReordering extends React.PureComponent {
   constructor(props) {
     super(props);
 
@@ -224,7 +226,7 @@ export class TableColumnReordering extends React.PureComponent {
   }
 }
 
-TableColumnReordering.propTypes = {
+RawTableColumnReordering.propTypes = {
   order: PropTypes.arrayOf(PropTypes.string),
   defaultOrder: PropTypes.arrayOf(PropTypes.string),
   onOrderChange: PropTypes.func,
@@ -233,8 +235,28 @@ TableColumnReordering.propTypes = {
   cellComponent: PropTypes.func.isRequired,
 };
 
-TableColumnReordering.defaultProps = {
+RawTableColumnReordering.defaultProps = {
   order: undefined,
   defaultOrder: [],
   onOrderChange: undefined,
 };
+
+RawTableColumnReordering.components = {
+  tableContainerComponent: 'TableContainer',
+  rowComponent: 'Row',
+  cellComponent: 'Cell',
+};
+
+const TableContainer = ({
+  onOver, onLeave, onDrop, children, // eslint-disable-line react/prop-types
+}) => (
+  <DropTarget
+    onOver={onOver}
+    onLeave={onLeave}
+    onDrop={onDrop}
+  >
+    {children}
+  </DropTarget>
+);
+
+export const TableColumnReordering = withComponents({ TableContainer })(RawTableColumnReordering);

aba62e34401e3cc9fe74366cb87f40729e6e9e78
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-09-17 16:22:06 +0300

    Update TableBandHeader

--- packages/dx-react-grid-bootstrap3/src/plugins/table-band-header.jsx
+++ packages/dx-react-grid-bootstrap3/src/plugins/table-band-header.jsx
@@ -1,24 +1,9 @@
-import * as React from 'react';
-import { TableBandHeader as TableBandHeaderBase } from '@devexpress/dx-react-grid';
+import { TableBandHeader as TableBandHeaderBase, withComponents } from '@devexpress/dx-react-grid';
 import { Cell } from '../templates/table-band-header/cell';
 import { BandedHeaderCell } from '../templates/table-band-header/banded-header-cell';
 import { InvisibleCell } from '../templates/table-band-header/invisible-cell';
-import { TableRow } from '../templates/table-row';
+import { TableRow as Row } from '../templates/table-row';
 
-export class TableBandHeader extends React.PureComponent {
-  render() {
-    return (
-      <TableBandHeaderBase
-        cellComponent={Cell}
-        rowComponent={TableRow}
-        bandedHeaderCellComponent={BandedHeaderCell}
-        invisibleCellComponent={InvisibleCell}
-        {...this.props}
-      />
-    );
-  }
-}
-
-TableBandHeader.Cell = Cell;
-TableBandHeader.Row = TableRow;
-TableBandHeader.BandedHeaderCell = BandedHeaderCell;
+export const TableBandHeader = withComponents({
+  Cell, Row, BandedHeaderCell, InvisibleCell,
+})(TableBandHeaderBase);

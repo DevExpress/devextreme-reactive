a50593d34672b62c9c0fc2350fa490abaa169812
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-09-17 15:42:44 +0300

    Update DragDropProvider

--- packages/dx-react-grid-material-ui/src/plugins/drag-drop-provider.jsx
+++ packages/dx-react-grid-material-ui/src/plugins/drag-drop-provider.jsx
@@ -1,18 +1,4 @@
-import * as React from 'react';
-import { DragDropProvider as DragDropProviderBase } from '@devexpress/dx-react-grid';
+import { DragDropProvider as DragDropProviderBase, withComponents } from '@devexpress/dx-react-grid';
 import { Container, Column } from '../templates/drag-drop';
 
-export class DragDropProvider extends React.PureComponent {
-  render() {
-    return (
-      <DragDropProviderBase
-        containerComponent={Container}
-        columnComponent={Column}
-        {...this.props}
-      />
-    );
-  }
-}
-
-DragDropProvider.Container = Container;
-DragDropProvider.Column = Column;
+export const DragDropProvider = withComponents({ Container, Column })(DragDropProviderBase);

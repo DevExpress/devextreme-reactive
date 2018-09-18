85989110859e4cc1e9923d68214e9373e2613ba4
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-09-18 11:11:07 +0300

    Update Toolbar

--- packages/dx-react-grid-bootstrap3/src/plugins/toolbar.jsx
+++ packages/dx-react-grid-bootstrap3/src/plugins/toolbar.jsx
@@ -1,18 +1,5 @@
-import * as React from 'react';
-import { Toolbar as ToolbarBase } from '@devexpress/dx-react-grid';
+import { Toolbar as ToolbarBase, withComponents } from '@devexpress/dx-react-grid';
 import { Toolbar as Root } from '../templates/toolbar/toolbar';
 import { FlexibleSpace } from '../templates/toolbar/flexible-space';
 
-export class Toolbar extends React.PureComponent {
-  render() {
-    return (
-      <ToolbarBase
-        rootComponent={Root}
-        flexibleSpaceComponent={FlexibleSpace}
-        {...this.props}
-      />
-    );
-  }
-}
-
-Toolbar.Root = Root;
+export const Toolbar = withComponents({ Root, FlexibleSpace })(ToolbarBase);

7f464ff0685b9828c27bd343c921c2278b896e84
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-09-18 15:04:03 +0300

    Update ViewSwitcher

--- packages/dx-react-scheduler-material-ui/src/plugins/view-switcher.jsx
+++ packages/dx-react-scheduler-material-ui/src/plugins/view-switcher.jsx
@@ -1,17 +1,4 @@
-import * as React from 'react';
-import { ViewSwitcher as ViewSwitcherBase } from '@devexpress/dx-react-scheduler';
+import { ViewSwitcher as ViewSwitcherBase, withComponents } from '@devexpress/dx-react-scheduler';
 import { Switcher } from '../templates/view-switcher/switcher';
 
-
-export class ViewSwitcher extends React.PureComponent {
-  render() {
-    return (
-      <ViewSwitcherBase
-        switcherComponent={Switcher}
-        {...this.props}
-      />
-    );
-  }
-}
-
-ViewSwitcher.Switcher = Switcher;
+export const ViewSwitcher = withComponents({ Switcher })(ViewSwitcherBase);

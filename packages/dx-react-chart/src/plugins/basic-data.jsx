fd4bc7dbd3300fbb79dc73d9140bd765e019a135
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-09-21 08:51:15 +0300

    Add BasicData plugin

---
+++ packages/dx-react-chart/src/plugins/basic-data.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import { Plugin, Getter } from '@devexpress/dx-react-core';
+
+export const BasicData = ({ data }) => (
+  <Plugin name="Basis">
+    <Getter name="data" value={data} />
+    <Getter name="series" value={[]} />
+  </Plugin>
+);
+
+BasicData.propTypes = {
+  data: PropTypes.array.isRequired,
+};

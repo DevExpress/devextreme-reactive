58b58c1a0be762d01933c1fca24e53ac51467747
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-09-25 14:47:55 +0300

    Fix ArgumentGrid and ValueGrid export

--- packages/dx-react-chart-bootstrap4/src/index.js
+++ packages/dx-react-chart-bootstrap4/src/index.js
@@ -11,6 +11,7 @@ export { PieSeries } from './plugins/pie-series';
 export { ScatterSeries } from './plugins/scatter-series';
 export { SplineSeries } from './plugins/spline-series';
 
-export { Grid } from './plugins/grid';
+export { ValueGrid } from './plugins/value-grid';
+export { ArgumentGrid } from './plugins/argument-grid';
 export { ValueAxis } from './plugins/value-axis';
 export { ArgumentAxis } from './plugins/argument-axis';

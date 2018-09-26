fd386f57eee606be3cc82ee83980448c11f66356
Dmitry Bogomolov <Dmitry.Bogomolov.mail@gmail.com>
2018-09-26 12:20:18 +0300

    fix(chart): fix crash in production caused by PropTypes reference

--- packages/dx-react-chart/src/utils/series-maker.test.jsx
+++ packages/dx-react-chart/src/utils/series-maker.test.jsx
@@ -1,10 +1,9 @@
-import * as PropTypes from 'prop-types';
 import { makeSeries } from './series-maker';
 
 jest.mock('@devexpress/dx-chart-core', () => ({}));
 
 describe('makeSeries', () => {
-  it('should set `components` and `propTypes`', () => {
+  it('should set *components*', () => {
     const components = {
       testComponent1: 'test-1',
       testComponent2: 'test-2',
@@ -12,9 +11,5 @@ describe('makeSeries', () => {
     const Series = makeSeries('TestSeries', 'test-type', () => null, () => null, components);
 
     expect(Series.components).toEqual(components);
-    // eslint-disable-next-line react/forbid-foreign-prop-types
-    expect(Series.propTypes.testComponent1).toEqual(PropTypes.func.isRequired);
-    // eslint-disable-next-line react/forbid-foreign-prop-types
-    expect(Series.propTypes.testComponent2).toEqual(PropTypes.func.isRequired);
   });
 });

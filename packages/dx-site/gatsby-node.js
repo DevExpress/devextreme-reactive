74b0ee1cbde7366c56b73088399ac6b9190a6fc6
Yury Orlov <mpcteam.kvet@gmail.com>
2018-09-27 12:56:53 +0300

    add redirects

--- packages/dx-site/gatsby-node.js
+++ packages/dx-site/gatsby-node.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { createFilePath } = require('gatsby-source-filesystem');
 const pageNavigation = require('./page-navigation.json');
+const { pathPrefix } = require('./gatsby-config');
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions;
@@ -34,11 +35,6 @@ exports.createPages = ({ graphql, actions }) => {
       if (pageNavigation[technology].demos) {
         createRedirect({
           fromPath: `/${technology}/demos/`,
-          toPath: pageNavigation[technology].demos.find(item => item.title === 'Demos').items[0].path,
-          redirectInBrowser: true,
-        });
-        createRedirect({
-          fromPath: `/${technology}/demos/featured/`,
           toPath: pageNavigation[technology].demos.find(item => item.title === 'Demos').items[0].path,
           redirectInBrowser: true,
         });
@@ -112,4 +108,43 @@ exports.onPostBootstrap = () => {
   setupFileSynchronization('../dx-react-chart-demos/dist/index.js', './public/static/react-chart-demos.js');
   setupFileSynchronization('../dx-react-grid-demos/dist/index.js', './public/static/react-grid-demos.js');
   setupFileSynchronization('../dx-vue-grid-demos/dist/index.js', './public/static/vue-grid-demos.js');
+};
+
+const createRedirectPage = ({ fromPath, toPath }) => {
+  const page = `
+    <!doctype html>
+    <html lang="en">
+    <head>
+      <meta http-equiv="refresh" content="0; url=${pathPrefix}${toPath}" />
+    </head>
+    <body></body>
+    </html>
+  `;
+  fs.writeFileSync(path.join(__dirname, 'public', fromPath, 'index.html'), page);
+};
+
+exports.onPostBuild = () => {
+  Object.keys(pageNavigation)
+    .forEach((technology) => {
+      if (pageNavigation[technology].demos) {
+        createRedirectPage({
+          fromPath: `/${technology}/demos/`,
+          toPath: pageNavigation[technology].demos.find(item => item.title === 'Demos').items[0].path,
+        });
+      }
+      if (pageNavigation[technology].docs) {
+        createRedirectPage({
+          fromPath: `/${technology}/docs/`,
+          toPath: pageNavigation[technology].docs.find(item => item.title === 'Guides').items[0].path,
+        });
+        createRedirectPage({
+          fromPath: `/${technology}/docs/guides/`,
+          toPath: pageNavigation[technology].docs.find(item => item.title === 'Guides').items[0].path,
+        });
+        createRedirectPage({
+          fromPath: `/${technology}/docs/reference/`,
+          toPath: pageNavigation[technology].docs.find(item => item.title === 'API Reference').items[0].path,
+        });
+      }
+    });
 };

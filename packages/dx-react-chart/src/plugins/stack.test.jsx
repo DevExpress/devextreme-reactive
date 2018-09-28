ea6b931f7afccea91021b00a5107c594d039bdee
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-09-28 19:13:19 +0300

    Restore 'dx-react-chart' tests

--- packages/dx-react-chart/src/plugins/stack.test.jsx
+++ packages/dx-react-chart/src/plugins/stack.test.jsx
@@ -1,38 +1,53 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
 import { PluginHost } from '@devexpress/dx-react-core';
-import { processData, seriesWithStacks, stacks } from '@devexpress/dx-chart-core';
 import { pluginDepsToComponents, getComputedState } from '@devexpress/dx-react-core/test-utils';
+import { stackOrderNone, stackOffsetDiverging } from 'd3-shape';
+import {
+  buildStackedSeries, buildStackedDataProcessor, clearStackedSeries, getStacks,
+} from '@devexpress/dx-chart-core';
 import { Stack } from './stack';
 
-jest.mock('@devexpress/dx-chart-core', () => ({
-  processData: jest.fn(),
-  seriesWithStacks: jest.fn(),
-  stacks: jest.fn(),
+jest.mock('d3-shape', () => ({
+  stackOrderNone: jest.fn(),
+  stackOffsetDiverging: jest.fn(),
 }));
 
-describe('Series Family', () => {
-  beforeEach(() => {
-    processData.mockImplementation(() => 'processData');
-    seriesWithStacks.mockImplementation(() => 'seriesWithStacks');
-    stacks.mockImplementation(() => 'stacks');
-  });
-  afterEach(() => {
-    jest.resetAllMocks();
-  });
+jest.mock('@devexpress/dx-chart-core', () => ({
+  buildStackedSeries: jest.fn().mockReturnValue('stacked-series'),
+  buildStackedDataProcessor: jest.fn().mockReturnValue(jest.fn().mockReturnValue('stacked-data')),
+  clearStackedSeries: jest.fn().mockReturnValue('cleared-series'),
+  getStacks: jest.fn().mockReturnValue('stacks'),
+}));
 
-  it('should provide optinos', () => {
+describe('Stack', () => {
+  afterEach(jest.clearAllMocks);
+
+  it('should provide options', () => {
+    const deps = {
+      getter: {
+        series: 'test-series',
+        data: 'test-data',
+      },
+    };
     const tree = mount((
       <PluginHost>
+        {pluginDepsToComponents(deps)}
         <Stack />
-        {pluginDepsToComponents({})}
       </PluginHost>
     ));
 
     expect(getComputedState(tree)).toEqual({
-      processingData: 'processData',
-      series: 'seriesWithStacks',
+      data: 'stacked-data',
+      series: 'cleared-series',
       stacks: 'stacks',
     });
+    expect(buildStackedSeries).toHaveBeenLastCalledWith('test-series');
+    expect(clearStackedSeries).toHaveBeenLastCalledWith('stacked-series');
+    expect(getStacks).toHaveBeenLastCalledWith('cleared-series');
+    expect(buildStackedDataProcessor)
+      .toHaveBeenLastCalledWith(stackOffsetDiverging, stackOrderNone);
+    expect(buildStackedDataProcessor.mock.results[0].value)
+      .toHaveBeenLastCalledWith('test-data', 'stacked-series');
   });
 });

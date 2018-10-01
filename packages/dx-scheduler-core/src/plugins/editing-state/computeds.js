ec5c182919779f8d2db3644964f9487f7f93f512
Maxim Kudriavtsev <maxim_71rus_wow@mail.ru>
2018-10-01 10:47:41 +0300

    Add computed tests

--- packages/dx-scheduler-core/src/plugins/editing-state/computeds.js
+++ packages/dx-scheduler-core/src/plugins/editing-state/computeds.js
@@ -2,6 +2,6 @@ export const addedAppointmentById = addedAppointment => addedAppointment;
 
 export const changedAppointmentById = (changes, appointmentId) => { // !
   const result = {};
-  result[appointmentId] = changes;
+  result[appointmentId] = changes[appointmentId];
   return result;
 };

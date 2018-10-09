6a3bda651b8efa2ea0b17f2e7f39d7e5f37bb865
SergeyAlexeev <SergeyAlexeev@users.noreply.github.com>
2018-10-09 16:27:23 +0300

    chore(react-grid): fix virtual scrolling on high dpi screens (#1476)

--- packages/dx-react-grid/src/components/table-layout/virtual-table-layout.jsx
+++ packages/dx-react-grid/src/components/table-layout/virtual-table-layout.jsx
@@ -148,11 +148,13 @@ export class VirtualTableLayout extends React.PureComponent {
       return;
     }
 
-    // NOTE: prevent iOS to flicker in bounces
+    // NOTE: prevent iOS to flicker in bounces and correct rendering on high dpi screens
+    const nodeHorizontalOffset = parseInt(node.scrollLeft + node.clientWidth, 10);
+    const nodeVerticalOffset = parseInt(node.scrollTop + node.clientHeight, 10);
     if (node.scrollTop < 0
       || node.scrollLeft < 0
-      || node.scrollLeft + node.clientWidth > Math.max(node.scrollWidth, node.clientWidth)
-      || node.scrollTop + node.clientHeight > Math.max(node.scrollHeight, node.clientHeight)) {
+      || nodeHorizontalOffset > Math.max(node.scrollWidth, node.clientWidth)
+      || nodeVerticalOffset > Math.max(node.scrollHeight, node.clientHeight)) {
       return;
     }
 

d730faa7eddfda0db0d0343b0de4cb3d688da10b
Maxim Kudriavtsev <maxim_71rus_wow@mail.ru>
2018-10-16 16:22:29 +0300

    Remove excess variable

--- packages/dx-scheduler-core/src/plugins/month-view/helpers.test.js
+++ packages/dx-scheduler-core/src/plugins/month-view/helpers.test.js
@@ -4,64 +4,8 @@ import {
 } from './helpers';
 
 describe('MonthView Helpers', () => {
-  const viewCellsData = [
-    [
-      { startDate: moment('2018-06-28'), endDate: moment('2018-06-29') },
-      { startDate: moment('2018-06-29'), endDate: moment('2018-06-30') },
-      { startDate: moment('2018-06-30'), endDate: moment('2018-07-01') },
-      { startDate: moment('2018-07-01'), endDate: moment('2018-07-02') },
-      { startDate: moment('2018-07-02'), endDate: moment('2018-07-03') },
-      { startDate: moment('2018-07-03'), endDate: moment('2018-07-04') },
-      { startDate: moment('2018-07-04'), endDate: moment('2018-07-05') },
-    ],
-    [
-      { startDate: moment('2018-07-05'), endDate: moment('2018-07-06') },
-      { startDate: moment('2018-07-06'), endDate: moment('2018-07-07') },
-      { startDate: moment('2018-07-07'), endDate: moment('2018-07-08') },
-      { startDate: moment('2018-07-08'), endDate: moment('2018-07-09') },
-      { startDate: moment('2018-07-09'), endDate: moment('2018-07-10') },
-      { startDate: moment('2018-07-10'), endDate: moment('2018-07-11') },
-      { startDate: moment('2018-07-11'), endDate: moment('2018-07-12') },
-    ],
-    [
-      { startDate: moment('2018-07-12'), endDate: moment('2018-07-13') },
-      { startDate: moment('2018-07-13'), endDate: moment('2018-07-14') },
-      { startDate: moment('2018-07-14'), endDate: moment('2018-07-15') },
-      { startDate: moment('2018-07-15'), endDate: moment('2018-07-16') },
-      { startDate: moment('2018-07-16'), endDate: moment('2018-07-17') },
-      { startDate: moment('2018-07-17'), endDate: moment('2018-07-18') },
-      { startDate: moment('2018-07-18'), endDate: moment('2018-07-19') },
-    ],
-    [
-      { startDate: moment('2018-07-19'), endDate: moment('2018-07-20') },
-      { startDate: moment('2018-07-20'), endDate: moment('2018-07-21') },
-      { startDate: moment('2018-07-21'), endDate: moment('2018-07-22') },
-      { startDate: moment('2018-07-22'), endDate: moment('2018-07-23') },
-      { startDate: moment('2018-07-23'), endDate: moment('2018-07-24') },
-      { startDate: moment('2018-07-24'), endDate: moment('2018-07-25') },
-      { startDate: moment('2018-07-25'), endDate: moment('2018-07-26') },
-    ],
-    [
-      { startDate: moment('2018-07-26'), endDate: moment('2018-07-27') },
-      { startDate: moment('2018-07-27'), endDate: moment('2018-07-28') },
-      { startDate: moment('2018-07-28'), endDate: moment('2018-07-29') },
-      { startDate: moment('2018-07-29'), endDate: moment('2018-07-30') },
-      { startDate: moment('2018-07-30'), endDate: moment('2018-07-31') },
-      { startDate: moment('2018-07-31'), endDate: moment('2018-08-01') },
-      { startDate: moment('2018-08-01'), endDate: moment('2018-08-02') },
-    ],
-    [
-      { startDate: moment('2018-08-02'), endDate: moment('2018-08-03') },
-      { startDate: moment('2018-08-03'), endDate: moment('2018-08-04') },
-      { startDate: moment('2018-08-04'), endDate: moment('2018-08-05') },
-      { startDate: moment('2018-08-05'), endDate: moment('2018-08-06') },
-      { startDate: moment('2018-08-06'), endDate: moment('2018-08-07') },
-      { startDate: moment('2018-08-07'), endDate: moment('2018-08-08') },
-      { startDate: moment('2018-08-08'), endDate: moment('2018-08-09') },
-    ],
-  ];
   describe('#sliceAppointmentByWeek', () => {
-    const bounds = { left: viewCellsData[0][0].startDate, right: viewCellsData[5][6].startDate };
+    const bounds = { left: moment('2018-06-28'), right: moment('2018-08-09') };
 
     it('should not slice appointments if they are short', () => {
       const appointment1 = { start: moment('2018-07-05'), end: moment('2018-07-12'), dataItem: {} };

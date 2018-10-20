cd69b46065d13f558860addaf2c732a843aaa451
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-10-20 20:14:43 +0300

    Update domains calculation

--- packages/dx-react-chart/src/plugins/chart-core.jsx
+++ packages/dx-react-chart/src/plugins/chart-core.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Plugin, Getter } from '@devexpress/dx-react-core';
 import { computeDomains, buildScales, scaleSeriesPoints } from '@devexpress/dx-chart-core';
 
-const getDomains = ({ axes, series, data }) => computeDomains(axes, series, data);
+const getDomains = ({ axes, series }) => computeDomains(axes, series);
 
 const getScales = ({ domains, layouts, scaleExtension }) => buildScales(
   domains, scaleExtension, layouts.pane,

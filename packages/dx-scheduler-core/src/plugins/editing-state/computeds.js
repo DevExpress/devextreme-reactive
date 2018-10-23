1afd8c390ac5260e610831d8fe21947572023eaa
Maxim Kudryavtsev <maxim_71rus_wow@mail.ru>
2018-10-23 12:57:30 +0200

    chore(react-shceduler): remove excess the createAppointmentChange getter (#1521)

--- packages/dx-scheduler-core/src/plugins/editing-state/computeds.js
+++ packages/dx-scheduler-core/src/plugins/editing-state/computeds.js
@@ -3,11 +3,3 @@ export const addedAppointmentById = addedAppointment => addedAppointment;
 export const changedAppointmentById = (changes, appointmentId) => (
   { [appointmentId]: changes }
 );
-
-const defaultCreateAppointmentChange = (
-  appointment, field, value,
-) => ({ [field]: value });
-
-export const createAppointmentChangeGetter = (
-  createAppointmentChange = defaultCreateAppointmentChange,
-) => (appointment, field, value) => createAppointmentChange(appointment, field, value);

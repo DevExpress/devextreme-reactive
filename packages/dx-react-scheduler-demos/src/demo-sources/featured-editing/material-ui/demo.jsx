e6b7afaf5c3a7e6b3987dfda51442155146f4be2
Maxim Kudriavtsev <maxim_71rus_wow@mail.ru>
2018-10-23 18:44:42 +0300

    Initial commit

---
+++ packages/dx-react-scheduler-demos/src/demo-sources/featured-editing/material-ui/demo.jsx
@@ -0,0 +1,179 @@
+import * as React from 'react';
+import Paper from '@material-ui/core/Paper';
+import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  Toolbar,
+  AllDayPanel,
+  DayView,
+  WeekView,
+  ViewSwitcher,
+  Appointments,
+  AppointmentForm,
+} from '@devexpress/dx-react-scheduler-material-ui';
+import { withStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import { appointments } from '../../../demo-data/appointments';
+
+const styles = theme => ({
+  addButton: {
+    position: 'absolute',
+    bottom: theme.spacing.unit * 3,
+    right: theme.spacing.unit * 3,
+  },
+  popper: {
+    paddingRight: theme.spacing.unit,
+  },
+});
+
+
+class Demo extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: appointments,
+      currentDate: '2018-06-28',
+      deletedAppointmentId: null,
+      confirmationVisible: false,
+      open: false,
+    };
+
+    this.commitChanges = this.commitChanges.bind(this);
+    this.toggleConfirmationVisible = this.toggleConfirmationVisible.bind(this);
+    this.commitDeletedAppointment = this.commitDeletedAppointment.bind(this);
+    this.openForm = this.openForm.bind(this);
+  }
+
+  setDeletedAppointmentId(id) {
+    this.setState({ deletedAppointmentId: id });
+  }
+
+  openForm() {
+    const { open } = this.state;
+    this.setState({
+      open: !open,
+    });
+  }
+
+
+  toggleConfirmationVisible() {
+    const { confirmationVisible } = this.state;
+    this.setState({ confirmationVisible: !confirmationVisible });
+  }
+
+  commitDeletedAppointment() {
+    const { data, deletedAppointmentId } = this.state;
+    const nextData = data.filter(appointment => appointment.id !== deletedAppointmentId);
+    this.setState({ data: nextData, deletedAppointmentId: null });
+    this.toggleConfirmationVisible();
+  }
+
+  commitChanges({ added, changed, deleted }) {
+    let { data } = this.state;
+    if (added) {
+      const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+      data = [
+        ...data,
+        {
+          id: startingAddedId,
+          ...added,
+        },
+      ];
+    }
+    if (changed) {
+      data = data.map(appointment => (
+        changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
+    }
+    if (deleted) {
+      this.setDeletedAppointmentId(deleted);
+      this.toggleConfirmationVisible();
+    }
+    this.setState({ data });
+  }
+
+  render() {
+    const {
+      currentDate, data, confirmationVisible, open,
+    } = this.state;
+    const { classes } = this.props;
+
+    return (
+      <Paper>
+        <Scheduler
+          data={data}
+        >
+          <ViewState
+            currentDate={currentDate}
+          />
+          <EditingState
+            onCommitChanges={this.commitChanges}
+          />
+
+          <WeekView
+            startDayHour={9}
+            endDayHour={19}
+          />
+          <DayView
+            startDayHour={9}
+            endDayHour={19}
+          />
+          <Appointments />
+          <Toolbar />
+          <AllDayPanel />
+          <ViewSwitcher />
+          <AppointmentForm
+            visible={open}
+            onVisibilityChange={this.openForm}
+          />
+        </Scheduler>
+
+        <Dialog
+          open={confirmationVisible}
+          onClose={this.cancelDelete}
+        >
+          <DialogTitle>
+            Delete Appointment
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Are you sure you want to delete this appointment?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.toggleConfirmationVisible} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.commitDeletedAppointment} color="secondary">
+              Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+        <Tooltip
+          title="Add Appointment"
+          placement="left"
+          disableFocusListener
+        >
+          <Button
+            variant="fab"
+            color="secondary"
+            className={classes.addButton}
+            onClick={this.openForm}
+          >
+            <AddIcon />
+          </Button>
+        </Tooltip>
+      </Paper>
+    );
+  }
+}
+
+export default withStyles(styles, { name: 'EditingDemo' })(Demo);

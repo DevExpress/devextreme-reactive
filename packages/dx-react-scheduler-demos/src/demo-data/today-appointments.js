38d29de6ee85edd007b5e6098e355824b01c1eb2
Maxim Kudriavtsev <maxim_71rus_wow@mail.ru>
2018-10-23 15:42:54 +0300

    Change the makeToday function

--- packages/dx-react-scheduler-demos/src/demo-data/today-appointments.js
+++ packages/dx-react-scheduler-demos/src/demo-data/today-appointments.js
@@ -1,23 +1,28 @@
+import moment from 'moment';
 import { appointments } from './appointments';
 
-const today = new Date();
-const todayArgs = [
-  today.getFullYear(),
-  today.getMonth(),
-];
 let date = 1;
-const makeToday = prevDate => (
-  new Date(
-    ...todayArgs,
-    date,
-    prevDate.getHours(),
-    prevDate.getMinutes(),
-  )
-);
+const makeTodayAppointment = (startDate, endDate) => {
+  const currentDate = moment();
+  const days = moment(startDate).diff(endDate, 'days');
+  const nextStartDate = moment(startDate)
+    .year(currentDate.year())
+    .month(currentDate.month())
+    .day(date);
+  const nextEndDate = moment(endDate)
+    .year(currentDate.year())
+    .month(currentDate.month())
+    .day(date + days);
+
+  return {
+    startDate: nextStartDate.toDate(),
+    endDate: nextEndDate.toDate(),
+  };
+};
+
 export default appointments.map(({ startDate, endDate, ...restArgs }) => {
   const result = {
-    startDate: makeToday(startDate),
-    endDate: makeToday(endDate),
+    ...makeTodayAppointment(startDate, endDate),
     ...restArgs,
   };
   date += 1;

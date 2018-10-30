e23dbec7eadaeb9c69e55f102e0b64363d93c008
Maxim Kudriavtsev <maxim_71rus_wow@mail.ru>
2018-10-30 10:46:43 +0300

    Fix test

--- packages/dx-scheduler-core/src/plugins/scheduler-core/computeds.js
+++ packages/dx-scheduler-core/src/plugins/scheduler-core/computeds.js
@@ -1,15 +1,11 @@
 export const appointments = (
   data,
-  // getAppointmentStartDate,
-  // getAppointmentEndDate,
-  // getAppointmentAllDay,
   appointmentMapping,
 ) => data.map(appointment => ({
   start: appointmentMapping(appointment).startDate,
   end: appointmentMapping(appointment).endDate,
   ...appointmentMapping(appointment).allDay !== undefined && {
     allDay: appointmentMapping(appointment).allDay,
-    // ...appointmentMapping(appointment),
   },
   dataItem: appointment,
 }));

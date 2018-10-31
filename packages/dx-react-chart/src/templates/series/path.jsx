878ba47d0dbe8a055841acf2f0dc4d4b4322dad9
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-10-31 16:48:25 +0300

    Add states for Path template

--- packages/dx-react-chart/src/templates/series/path.jsx
+++ packages/dx-react-chart/src/templates/series/path.jsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import { getAreaAnimationStyle } from '@devexpress/dx-chart-core';
+import {
+  getAreaAnimationStyle, DEFAULT, HOVERED, SELECTED,
+} from '@devexpress/dx-chart-core';
+import { withStates } from '../../utils/with-states';
 
-export class Path extends React.PureComponent {
+class RawPath extends React.PureComponent {
   render() {
     const {
       path,
       coordinates,
       color,
+      index,
       style,
       getAnimatedStyle,
       scales,
@@ -26,14 +30,23 @@ export class Path extends React.PureComponent {
   }
 }
 
-Path.propTypes = {
+RawPath.propTypes = {
   path: PropTypes.func.isRequired,
   coordinates: PropTypes.array.isRequired,
   color: PropTypes.string,
   style: PropTypes.object,
 };
 
-Path.defaultProps = {
+RawPath.defaultProps = {
   color: undefined,
   style: undefined,
 };
+
+// Shouldn't *RawPath* have something like *thickness*  property so that HOVERED and SELECTED
+// states would address it rather then *strokeWidth* property?
+
+export const Path = withStates({
+  [DEFAULT]: props => props,
+  [HOVERED]: props => ({ strokeWidth: 4, ...props }),
+  [SELECTED]: props => ({ strokeWidth: 4, ...props }),
+})(RawPath);

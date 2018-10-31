5eae62c9b80587af346469368dccbf25b046af31
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-10-31 16:39:56 +0300

    Add states for Area template

--- packages/dx-react-chart/src/templates/series/area.jsx
+++ packages/dx-react-chart/src/templates/series/area.jsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import { getAreaAnimationStyle } from '@devexpress/dx-chart-core';
+import {
+  getAreaAnimationStyle, DEFAULT, HOVERED, SELECTED,
+} from '@devexpress/dx-chart-core';
+import { Pattern } from '../pattern';
+import { withStates } from '../../utils/with-states';
 
-export class Area extends React.PureComponent {
+class RawArea extends React.PureComponent {
   render() {
     const {
       path,
       coordinates,
       color,
+      index,
       style,
       getAnimatedStyle,
       scales,
@@ -25,14 +30,59 @@ export class Area extends React.PureComponent {
   }
 }
 
-Area.propTypes = {
+RawArea.propTypes = {
   path: PropTypes.func.isRequired,
   coordinates: PropTypes.array.isRequired,
   color: PropTypes.string,
   style: PropTypes.object,
 };
 
-Area.defaultProps = {
+RawArea.defaultProps = {
   color: undefined,
   style: undefined,
 };
+
+const getPatternIdUrl = (prefix, id) => {
+  const value = `${prefix}-${id}`;
+  return [value, `url(#${value})`];
+};
+
+export const Area = withStates({
+  [DEFAULT]: props => ({ key: 'element', ...props }),
+  [HOVERED]: ({ color, index, ...restProps }) => {
+    const [patternId, patternUrl] = getPatternIdUrl('series-hover', index);
+    return (
+      <React.Fragment>
+        <Pattern
+          key="pattern"
+          id={patternId}
+          color={color}
+          opacity={0.75}
+        />
+        <RawArea
+          key="element"
+          color={patternUrl}
+          {...restProps}
+        />
+      </React.Fragment>
+    );
+  },
+  [SELECTED]: ({ color, index, ...restProps }) => {
+    const [patternId, patternUrl] = getPatternIdUrl('series-selection', index);
+    return (
+      <React.Fragment>
+        <Pattern
+          key="pattern"
+          id={patternId}
+          color={color}
+          opacity={0.85}
+        />
+        <RawArea
+          key="element"
+          color={patternUrl}
+          {...restProps}
+        />
+      </React.Fragment>
+    );
+  },
+})(RawArea);

227e338e27d4c8927822558ff62bd241988c14d9
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-10-31 17:04:16 +0300

    Add states for Point template

--- packages/dx-react-chart/src/templates/series/point.jsx
+++ packages/dx-react-chart/src/templates/series/point.jsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
+import { DEFAULT, HOVERED, SELECTED } from '@devexpress/dx-chart-core';
+import { withStates } from '../../utils/with-states';
 
-export class Point extends React.PureComponent {
+class RawPoint extends React.PureComponent {
   render() {
     const {
       argument, value, index, x, y, color, ...restProps
     } = this.props;
+    // *d* attribute is calculated during points scaling.
+    // TODO: Do it here - d={path().size(size).type(type)()}
     return (
       <path
         fill={color}
@@ -17,13 +21,31 @@ export class Point extends React.PureComponent {
   }
 }
 
-Point.propTypes = {
+RawPoint.propTypes = {
   x: PropTypes.number.isRequired,
   y: PropTypes.number.isRequired,
   value: PropTypes.number.isRequired,
   color: PropTypes.string,
 };
 
-Point.defaultProps = {
+RawPoint.defaultProps = {
   color: undefined,
 };
+
+export const Point = withStates({
+  [DEFAULT]: props => props,
+  [HOVERED]: ({ color, ...restProps }) => ({
+    stroke: color,
+    strokeWidth: 4,
+    fill: 'none',
+    // size: 12, Awaiting TODO from above.
+    ...restProps,
+  }),
+  [SELECTED]: ({ color, ...restProps }) => ({
+    stroke: color,
+    strokeWidth: 4,
+    fill: 'none',
+    // size: 12, Awaiting TODO from above.
+    ...restProps,
+  }),
+})(RawPoint);

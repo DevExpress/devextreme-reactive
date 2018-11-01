9c58195ab1a624a0f0538fa4a5fe167d728188b1
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-11-01 23:24:44 +0300

    Set point hole color to background color

---
+++ packages/dx-react-chart-material-ui/src/templates/series/point.jsx
@@ -0,0 +1,26 @@
+import { ScatterSeries, withPatchedProps } from '@devexpress/dx-react-chart';
+import { withStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
+
+const styles = theme => ({
+  point: {
+    fill: theme.palette.background.paper,
+  },
+});
+
+const setClassName = ({ classes, ...restProps }) => {
+  if (restProps.state) {
+    const { className, ...rest } = restProps;
+    return {
+      className: classNames(classes.point, className),
+      ...rest,
+    };
+  }
+  return restProps;
+};
+
+export const Point = withStyles(styles)(
+  withPatchedProps(setClassName)(
+    ScatterSeries.Point,
+  ),
+);

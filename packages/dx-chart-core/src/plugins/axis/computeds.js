43097cc6be5c332a690210c524ace59b4b790d8f
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-11-09 16:49:01 +0300

    Remove some copy-paste

--- packages/dx-chart-core/src/plugins/axis/computeds.js
+++ packages/dx-chart-core/src/plugins/axis/computeds.js
@@ -18,63 +18,59 @@ const getFormat = (scale, tickFormat) => {
   return tick => tick;
 };
 
-const createHorizontalProcessor = (scale, position, tickSize, indentFromAxis, formatTick) => {
+const createHorizontalOptions = (position, tickSize, indentFromAxis) => {
+  // Make *position* should be orientation agnostic - START or END.
   const isStart = position === BOTTOM;
-  const dominantBaseline = isStart ? 'hanging' : 'baseline';
-  const textAnchor = MIDDLE;
-  return (tick, index) => {
-    const coordinates = scale(tick);
-    return {
-      key: index,
-      x1: coordinates,
-      x2: coordinates,
-      y1: 0,
-      y2: isStart ? +tickSize : -tickSize,
-      xText: coordinates,
-      yText: isStart ? +indentFromAxis : -indentFromAxis,
-      dominantBaseline,
-      textAnchor,
-      text: formatTick(tick),
-    };
+  return {
+    y1: 0,
+    y2: isStart ? +tickSize : -tickSize,
+    yText: isStart ? +indentFromAxis : -indentFromAxis,
+    dominantBaseline: isStart ? 'hanging' : 'baseline',
+    textAnchor: MIDDLE,
   };
 };
 
-const createVerticalProcessor = (scale, position, tickSize, indentFromAxis, formatTick) => {
+const createVerticalOptions = (position, tickSize, indentFromAxis) => {
+  // Make *position* should be orientation agnostic - START or END.
   const isStart = position === LEFT;
-  const dominantBaseline = MIDDLE;
-  const textAnchor = isStart ? END : START;
-  return (tick, index) => {
-    const coordinates = scale(tick);
-    return {
-      key: index,
-      x1: 0,
-      x2: isStart ? -tickSize : +tickSize,
-      y1: coordinates,
-      y2: coordinates,
-      xText: isStart ? -indentFromAxis : +indentFromAxis,
-      yText: coordinates,
-      dominantBaseline,
-      textAnchor,
-      text: formatTick(tick),
-    };
+  return {
+    x1: 0,
+    x2: isStart ? -tickSize : +tickSize,
+    xText: isStart ? -indentFromAxis : +indentFromAxis,
+    dominantBaseline: MIDDLE,
+    textAnchor: isStart ? END : START,
   };
 };
 
 // It is called for grid (which does not have labels) - how is it handled here?
 export const axisCoordinates = ({
   scale,
-  orientation,
+  orientation, // TODO: Replace it with *isHorizontal*.
   position,
   tickSize,
   tickFormat,
   indentFromAxis,
 }) => {
   const isHorizontal = orientation === HORIZONTAL;
-  const ticks = getTicks(scale);
-  const processTick = (isHorizontal ? createHorizontalProcessor : createVerticalProcessor)(
-    fixScaleOffset(scale), position, tickSize, indentFromAxis, getFormat(scale, tickFormat),
+  const options = (isHorizontal ? createHorizontalOptions : createVerticalOptions)(
+    position, tickSize, indentFromAxis,
   );
-  return ticks.map(processTick);
+  const fixedScale = fixScaleOffset(scale);
+  const formatTick = getFormat(scale, tickFormat);
+  return getTicks(scale).map((tick, index) => {
+    const coordinates = fixedScale(tick);
+    return {
+      key: index,
+      x1: coordinates,
+      x2: coordinates,
+      y1: coordinates,
+      y2: coordinates,
+      xText: coordinates,
+      yText: coordinates,
+      text: formatTick(tick),
+      ...options,
+    };
+  });
 };
 
 export const axesData = (axes, axisProps) => [...axes, axisProps];

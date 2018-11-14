fd73ff9b9057d2c6ac4f9cd8a90732372bb6b261
Nikita Ushkal <nikita.ushkal@devexpress.com>
2018-11-14 16:05:02 +0300

    fix(react-grid-bootstrap4): prevent edge from overlapping a scrollbar

--- packages/dx-react-grid-bootstrap4/src/templates/table-container.jsx
+++ packages/dx-react-grid-bootstrap4/src/templates/table-container.jsx
@@ -2,9 +2,15 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export const TableContainer = ({ children, className, ...restProps }) => (
+export const TableContainer = ({
+  children, className, style, ...restProps
+}) => (
   <div
     className={classNames('table-responsive dx-g-bs4-table-container', className)}
+    style={{
+      msOverflowStyle: 'auto',
+      ...style,
+    }}
     {...restProps}
   >
     {children}

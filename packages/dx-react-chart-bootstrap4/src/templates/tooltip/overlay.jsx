f0f3f217b865f08b60d0d54ab61d4a1bf9b43651
Smirnova Yuliya <yuliya.smirnova@devexpress.com>
2018-11-16 12:25:12 +0300

    fix tooltip in the bootstrap

--- packages/dx-react-chart-bootstrap4/src/templates/tooltip/overlay.jsx
+++ packages/dx-react-chart-bootstrap4/src/templates/tooltip/overlay.jsx
@@ -2,20 +2,35 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import { Popover, PopoverBody } from 'reactstrap';
 
-export const Overlay = ({
-  children, target, ...restProps
-}) => (
-  <Popover
-    placement="top"
-    isOpen
-    target={target}
-    {...restProps}
-  >
-    <PopoverBody>
-      {children}
-    </PopoverBody>
-  </Popover>
-);
+// Popover component calls *target* function from render.
+// For the first time it is returns null as expected and Popover crashes.
+// Because of it the following HOC is used to unwrap DOM element from *target* function.
+export class Overlay extends React.PureComponent {
+  componentDidMount() {
+    this.forceUpdate();
+  }
+
+  render() {
+    const { children, target, ...restProps } = this.props;
+    const domElement = target();
+    return (
+      domElement
+        ? (
+          <Popover
+            placement="top"
+            isOpen
+            target={domElement}
+            {...restProps}
+          >
+            <PopoverBody>
+              {children}
+            </PopoverBody>
+          </Popover>
+        )
+        : null
+    );
+  }
+}
 
 Overlay.propTypes = {
   children: PropTypes.node.isRequired,

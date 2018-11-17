906e4887499895e8765c975f9bd907587d951f87
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-11-17 23:21:30 +0300

    Make proper file name

--- packages/dx-react-chart/src/plugins/bar-series.jsx
+++ packages/dx-react-chart/src/plugins/bar-series.jsx
@@ -3,7 +3,7 @@ import {
   createBarHitTester as createHitTester,
 } from '@devexpress/dx-chart-core';
 import { declareSeries } from '../utils';
-import { PointCollection as Path } from '../templates/series/point-collection-e';
+import { PointCollection as Path } from '../templates/series/point-collection';
 import { Bar as Point } from '../templates/series/bar';
 
 export const BarSeries = declareSeries('BarSeries', {
--- packages/dx-react-chart/src/plugins/scatter-series.jsx
+++ packages/dx-react-chart/src/plugins/scatter-series.jsx
@@ -3,7 +3,7 @@ import {
   createScatterHitTester as createHitTester,
 } from '@devexpress/dx-chart-core';
 import { declareSeries } from '../utils';
-import { PointCollection as Path } from '../templates/series/point-collection-e';
+import { PointCollection as Path } from '../templates/series/point-collection';
 import { Point } from '../templates/series/point';
 
 export const ScatterSeries = declareSeries('ScatterSeries', {

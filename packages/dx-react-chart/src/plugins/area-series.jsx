eab723d5836eb67e2206822c450d1f833f7d1bc4
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-11-18 12:14:04 +0300

    Add Line and Spline series components

--- packages/dx-react-chart/src/plugins/area-series.jsx
+++ packages/dx-react-chart/src/plugins/area-series.jsx
@@ -1,14 +1,11 @@
 import {
-  dArea as path,
-  getAreaPointTransformer as getPointTransformer,
-  createAreaHitTester as createHitTester,
+  getAreaPointTransformer as getPointTransformer, createAreaHitTester as createHitTester,
 } from '@devexpress/dx-chart-core';
 import { declareSeries } from '../utils';
 import { Area as Path } from '../templates/series/area';
 
 export const AreaSeries = declareSeries('AreaSeries', {
   components: { Path },
-  path,
   getPointTransformer,
   createHitTester,
 });

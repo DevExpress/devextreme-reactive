fa79ce37bd64fb5ccf16d8d8f96cb88ba7b63eff
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-11-28 11:50:16 +0300

    Fold particular lines

--- packages/dx-demo-shell/src/demo-viewer/source-code.jsx
+++ packages/dx-demo-shell/src/demo-viewer/source-code.jsx
@@ -2,38 +2,49 @@ import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import CodeMirror from 'codemirror';
 import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/jsx/jsx';
 import 'codemirror/addon/fold/foldcode';
 import 'codemirror/addon/fold/foldgutter';
 import 'codemirror/addon/fold/brace-fold';
 import 'codemirror/addon/fold/xml-fold';
 import 'codemirror/addon/fold/indent-fold';
 import 'codemirror/addon/fold/comment-fold';
+import 'codemirror/addon/search/searchcursor';
 
 export class SourceCode extends React.PureComponent {
   constructor(props) {
     super(props);
     this.textarea = null;
+    this.source = '';
   }
 
   componentDidMount() {
-    CodeMirror.fromTextArea(this.textarea, {
+    const editor = CodeMirror.fromTextArea(this.textarea, {
       lineNumbers: true,
       lineWrapping: true,
       readOnly: true,
-      mode: 'javascript',
+      mode: 'jsx',
       foldGutter: true,
       gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter']
     });
+    const cursor = editor.getSearchCursor('// #foldBlock');
+    const arr = [];
+    while (cursor.findNext()) {
+      cursor.replace('');
+      arr.push(cursor.from().line);
+      editor.foldCode(CodeMirror.Pos(cursor.from().line + 1, 0));
+    }
   }
 
   render() {
     const { themeName, sectionName, demoName } = this.props;
     const { embeddedDemoOptions } = this.context;
     const { demoSources } = embeddedDemoOptions;
+    this.source = demoSources[sectionName][demoName][themeName].source || '';
     return (
       <textarea
         ref={(ref) => { this.textarea = ref; }}
-        value={demoSources[sectionName][demoName][themeName].source || ''}
+        value={this.source}
         onChange={() => {}}
       />
     );

568f69e0cb9af6f943b3c76211eaa8afa231c26e
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-11-28 10:59:46 +0300

    Add codemirror

--- packages/dx-demo-shell/src/demo-viewer/source-code.jsx
+++ packages/dx-demo-shell/src/demo-viewer/source-code.jsx
@@ -1,26 +1,41 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import prism from 'prismjs';
-import 'prismjs/components/prism-jsx';
+import CodeMirror from 'codemirror';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/fold/foldcode';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/brace-fold';
+import 'codemirror/addon/fold/xml-fold';
+import 'codemirror/addon/fold/indent-fold';
+import 'codemirror/addon/fold/comment-fold';
 
 export class SourceCode extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.textarea = null;
+  }
+
+  componentDidMount() {
+    CodeMirror.fromTextArea(this.textarea, {
+      lineNumbers: true,
+      lineWrapping: true,
+      readOnly: true,
+      mode: 'javascript',
+      foldGutter: true,
+      gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter']
+    });
+  }
+
   render() {
     const { themeName, sectionName, demoName } = this.props;
     const { embeddedDemoOptions } = this.context;
     const { demoSources } = embeddedDemoOptions;
     return (
-      <pre className="highlight language-jsx">
-        <code
-          className="language-jsx"
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{
-            __html: prism.highlight(
-              demoSources[sectionName][demoName][themeName].source || '',
-              prism.languages.jsx,
-            ),
-          }}
-        />
-      </pre>
+      <textarea
+        ref={(ref) => { this.textarea = ref; }}
+        value={demoSources[sectionName][demoName][themeName].source || ''}
+        onChange={() => {}}
+      />
     );
   }
 }

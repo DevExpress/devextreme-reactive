1cdffb64579db45e4e1f05877d26757bf0c030e2
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-11-29 16:37:02 +0300

    Refactor view switcher

--- packages/dx-scheduler-core/src/plugins/view-state/reducers.js
+++ packages/dx-scheduler-core/src/plugins/view-state/reducers.js
@@ -9,4 +9,4 @@ export const changeCurrentDate = (currentDate, {
   nextDate || moment(currentDate)[back ? 'subtract' : 'add'](amount, step).toDate()
 );
 
-export const setCurrentViewName = (currentViewName, { nextViewName }) => nextViewName;
+export const setCurrentViewName = (currentViewName, nextViewName) => nextViewName;

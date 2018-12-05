7191f0311339520c6caa8f3a62c75446690e7f62
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2018-12-05 11:55:14 +0300

    Windows related fix

--- packages/dx-site/gatsby-node.js
+++ packages/dx-site/gatsby-node.js
@@ -12,7 +12,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     createNodeField({
       node,
       name: 'slug',
-      value: path.join(technology, section, createFilePath({ node, getNode })),
+      value: path.posix.join(technology, section, createFilePath({ node, getNode })),
     });
     createNodeField({
       node,

be0856ae0986360db7b6ea436b4be9e680b88e9e
Nikita Ushkal <nikita.ushkal@devexpress.com>
2018-12-05 12:34:09 +0300

    remove transform from config

--- packages/dx-react-bootstrap4/jest.config.js
+++ packages/dx-react-bootstrap4/jest.config.js
@@ -2,9 +2,6 @@ const path = require('path');
 
 module.exports = {
   verbose: true,
-  transform: {
-    '^.+\\.(js|jsx)?$': 'babel-jest',
-  },
   setupFiles: [
     path.join(__dirname, './setup-enzyme.js'),
   ],

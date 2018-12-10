85665b7fe33dd4798cc2b2a999f82b5a29736413
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-12-10 11:44:45 +0300

    Add comment

--- packages/dx-react-chart/src/templates/axis/root.jsx
+++ packages/dx-react-chart/src/templates/axis/root.jsx
@@ -7,9 +7,7 @@ const getSize = (position, delta) => (position >= 0 ? position + delta : -positi
 export class Root extends React.PureComponent {
   constructor(props) {
     super(props);
-    this.ref = (node) => {
-      this.node = node;
-    };
+    this.ref = React.createRef();
     this.state = {
       x: 0, y: 0,
     };
@@ -21,20 +19,27 @@ export class Root extends React.PureComponent {
   }
 
   componentDidUpdate() {
-    // *setState* can be called unconditionally because it contains proper check inside.
+    // *setState* is called unconditionally because PureComponent is expected to break the cycle.
     this.setState(this.adjust); // eslint-disable-line react/no-did-update-set-state
   }
 
+  // Since calculated state does not depend on current state non-callback version of *setState*
+  // might have been expected - it can't be done.
+  // Parent component (Axis) accesses its DOM content in *onSizeChange* handler. When
+  // this component is mounted parent is not yet - it crashes on DOM access.
+  // *setState* callback is invoked later then *componentDidMount* - by that time parent component
+  // is already mounted and can access its DOM.
+  // Because of it callback version of *setState* has to be used here.
+  // Can we rely on the fact that by the time of callback parent is mounted?
+  // For now we stick with it, but need to find a more solid solution.
   adjust(_, { dx, dy, onSizeChange }) {
-    const bbox = this.node.getBBox();
+    const bbox = this.ref.current.getBBox();
     const width = dx ? bbox.width : getSize(bbox.x, bbox.width);
     const height = dy ? bbox.height : getSize(bbox.y, bbox.height);
     const x = dx ? 0 : getOffset(bbox.x);
     const y = dy ? 0 : getOffset(bbox.y);
     onSizeChange({ width, height });
-    return {
-      x, y,
-    };
+    return { x, y };
   }
 
   render() {

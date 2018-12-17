8ff4006d6c886f8f6b3b1c731f0fc0a5ee9bef2c
Dmitry Bogomolov <Dmitry.Bogomolov.mail@gmail.com>
2018-12-17 15:08:37 +0300

    chore(react-chart-material-ui): replace Popoper with Popper (#1703)

--- packages/dx-react-chart-material-ui/src/templates/tooltip/overlay.test.jsx
+++ packages/dx-react-chart-material-ui/src/templates/tooltip/overlay.test.jsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { createMount, getClasses } from '@material-ui/core/test-utils';
 import { Overlay } from './overlay';
 
 describe('Overlay', () => {
   const defaultProps = {
-    target: () => {},
+    target: () => ({ }),
   };
 
+  const classes = getClasses(<Overlay {...defaultProps}>Test</Overlay>);
+  const mount = createMount();
+
   it('should render Popover', () => {
-    const tree = shallow((
+    const tree = mount((
       <Overlay
         {...defaultProps}
       >
@@ -16,6 +19,45 @@ describe('Overlay', () => {
       </Overlay>
     ));
 
-    expect(tree.find('.content')).toBeTruthy();
+    expect(tree.find('Popper').props()).toMatchObject({
+      open: true,
+      anchorEl: defaultProps.target,
+      placement: 'top',
+      className: classes.popper,
+    });
+    expect(tree.find('Paper').props()).toMatchObject({
+      className: classes.paper,
+    });
+    expect(tree.find('div').get(3).props).toMatchObject({
+      className: classes.arrow,
+    });
+    expect(tree.find('.content').exists()).toBeTruthy();
+  });
+
+  it('should pass custom class to the root element', () => {
+    const tree = mount((
+      <Overlay
+        {...defaultProps}
+        className="custom-class"
+      >
+        <div className="content" />
+      </Overlay>
+    ));
+
+    expect(tree.find('Popper').is('.custom-class')).toBeTruthy();
+    expect(tree.find('Popper').is(`.${classes.popper}`)).toBeTruthy();
+  });
+
+  it('should pass rest props to the root element', () => {
+    const tree = mount((
+      <Overlay
+        {...defaultProps}
+        custom={10}
+      >
+        <div className="content" />
+      </Overlay>
+    ));
+
+    expect(tree.find('Popper').props().custom).toEqual(10);
   });
 });

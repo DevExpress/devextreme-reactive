504f434f84c8a2fab5277529075ae9f4a1459dcc
ushkal <44223690+ushkal@users.noreply.github.com>
2018-12-17 13:31:18 +0300

    fix(react-grid): fix header layout when middle band column is fixed (#1619)

--- packages/dx-grid-core/src/plugins/table-header-row/helpers.test.js
+++ packages/dx-grid-core/src/plugins/table-header-row/helpers.test.js
@@ -3,6 +3,7 @@ import { TABLE_DATA_TYPE } from '../table/constants';
 import {
   isHeadingTableCell,
   isHeadingTableRow,
+  splitHeaderColumnChains,
 } from './helpers';
 
 describe('TableHeaderRow Plugin helpers', () => {
@@ -22,6 +23,34 @@ describe('TableHeaderRow Plugin helpers', () => {
         .toBeTruthy();
       expect(isHeadingTableRow({ type: 'undefined' }))
         .toBeFalsy();
+    });
+  });
+
+  describe('#splitHeaderColumnChains', () => {
+    it('should work', () => {
+      const columns = [
+        { key: 'a0' }, { key: 'a1' }, { key: 'b0' }, { key: 'b1' },
+      ];
+      const existingChains = [
+        [{ start: 0, columns }],
+      ];
+      const shouldSplitChain = (currentChain, column) => (
+        !currentChain || column.key.indexOf('b') > -1
+      );
+      const extendChainProps = () => ({
+        extended: true,
+      });
+
+      const chains = splitHeaderColumnChains(
+        existingChains, columns, shouldSplitChain, extendChainProps,
+      );
+
+      expect(chains[0]).toHaveLength(3);
+      expect(chains[0]).toMatchObject([
+        { start: 0, extended: true, columns: columns.slice(0, 2) },
+        { start: 2, extended: true, columns: columns.slice(2, 3) },
+        { start: 3, extended: true, columns: columns.slice(3, 4) },
+      ]);
     });
   });
 });

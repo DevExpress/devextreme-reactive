78d93ee44273864c10d81af7fc8792c431a9b5cc
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2018-12-17 11:54:10 +0300

    Replace the way arrow is rendered

--- packages/dx-react-chart-material-ui/src/templates/tooltip/overlay.jsx
+++ packages/dx-react-chart-material-ui/src/templates/tooltip/overlay.jsx
@@ -1,87 +1,57 @@
 import * as React from 'react';
-import * as PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Popper from '@material-ui/core/Popper';
 import Paper from '@material-ui/core/Paper';
 import classNames from 'classnames';
-import { getBorderColor } from '../utils';
 
-const styles = theme => ({
-  popper: {
-    zIndex: 1,
-    marginBottom: '.5rem',
-  },
-  paper: {
-    padding: `${theme.spacing.unit * 0.5}px ${theme.spacing.unit}px`,
-  },
-  arrow: {
-    position: 'absolute',
-    display: 'block',
-    width: '1rem',
-    height: '.5rem',
-    margin: '0 .3rem',
-    bottom: '-.5rem', // placement:"top" specific
-
-    '&::before, &::after': {
+const styles = (theme) => {
+  const { unit } = theme.spacing;
+  const arrowSize = unit * 1.2;
+  return {
+    popper: {
+      zIndex: 1,
+      marginBottom: `${arrowSize}px`,
+    },
+    paper: {
+      padding: `${unit * 0.5}px ${unit}px`,
+    },
+    arrow: {
+      width: `${arrowSize * 5}px`,
+      height: `${arrowSize * 2.5}px`,
       position: 'absolute',
-      display: 'block',
-      content: '""',
-      borderColor: 'transparent',
-      borderStyle: 'solid',
-      borderWidth: '.5rem .5rem 0', // placement:"top" specific
-    },
-    '&::before': {
-      bottom: 0,
-      borderTopColor: getBorderColor(theme),
-    },
-    '&::after': {
-      bottom: '1px',
-      borderTopColor: theme.palette.background.paper,
-    },
-  },
-});
+      top: '100%',
+      left: '50%',
+      transform: 'translateX(-50%)',
+      overflow: 'hidden',
 
-class RawOverlay extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    // *React.createRef* is not used because it cannot be accessed (*current* is null)
-    // in *componentDidMount* (looks like Popper does not render its children immediately).
-    // Accesing it in *componentDidUpdate* requires additional checks.
-    // The callback version is just simpler.
-    this.arrowRef = element => this.setState({
-      modifiers: {
-        arrow: { element },
+      '&::after': {
+        content: '""',
+        position: 'absolute',
+        width: `${arrowSize}px`,
+        height: `${arrowSize}px`,
+        background: theme.palette.background.paper,
+        transform: 'translateX(-50%) translateY(-50%) rotate(45deg)',
+        top: 0,
+        left: '50%',
+        boxShadow: theme.shadows[2],
       },
-    });
-  }
-
-  render() {
-    const {
-      classes, className, children, target, ...restProps
-    } = this.props;
-    const { modifiers } = this.state;
-    return (
-      <Popper
-        open
-        anchorEl={target}
-        placement="top"
-        className={classNames(classes.popper, className)}
-        modifiers={modifiers}
-        {...restProps}
-      >
-        <Paper className={classes.paper}>
-          {children}
-        </Paper>
-        <div ref={this.arrowRef} className={classes.arrow} />
-      </Popper>
-    );
-  }
-}
-
-RawOverlay.propTypes = {
-  children: PropTypes.node.isRequired,
-  target: PropTypes.func.isRequired,
+    },
+  };
 };
 
-export const Overlay = withStyles(styles)(RawOverlay);
+export const Overlay = withStyles(styles)(({
+  classes, className, children, target, ...restProps
+}) => (
+  <Popper
+    open
+    anchorEl={target}
+    placement="top"
+    className={classNames(classes.popper, className)}
+    {...restProps}
+  >
+    <Paper className={classes.paper}>
+      {children}
+    </Paper>
+    <div className={classes.arrow} />
+  </Popper>
+));

d7f345cc4020e77c5eb8ff64a8015eba8ca5de70
Maxim Kudryavtsev <maxim_71rus_wow@mail.ru>
2018-12-20 16:55:42 +0300

    refactor(react-scheduler-material-ui): refactor source code (#1722)

--- packages/dx-react-scheduler-material-ui/src/scheduler.jsx
+++ packages/dx-react-scheduler-material-ui/src/scheduler.jsx
@@ -3,10 +3,10 @@ import * as PropTypes from 'prop-types';
 import { Scheduler as SchedulerBase } from '@devexpress/dx-react-scheduler';
 import { Root } from './templates/layout';
 
-export const Scheduler = ({ children, ...props }) => (
+export const Scheduler = ({ children, ...restProps }) => (
   <SchedulerBase
     rootComponent={Root}
-    {...props}
+    {...restProps}
   >
     {children}
   </SchedulerBase>

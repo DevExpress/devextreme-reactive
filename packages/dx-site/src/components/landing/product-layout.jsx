f328807fd251a36971f86a2516882176ef640fee
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2019-01-21 11:39:11 +0300

    Hide some elements on small screens

--- packages/dx-site/src/components/landing/product-layout.jsx
+++ packages/dx-site/src/components/landing/product-layout.jsx
@@ -5,24 +5,18 @@ import styles from './product-layout.module.scss';
 
 const ProductLayout = ({ position, children }) => (
   <React.Fragment>
-    {position === 'footer' ? (
-      <div className={styles.footerTop} />
-    ) : null}
-    <div className={styles.container}>
+    <div className={styles[position]}>
       <div className="container">
         <div className={`row ${position === 'footer' ? 'justify-content-center' : ''}`}>
           {children}
         </div>
       </div>
     </div>
-    {position === 'footer' ? (
-      <div className={styles.footerBottom} />
-    ) : null}
   </React.Fragment>
 );
 
 ProductLayout.propTypes = {
-  position: PropTypes.string.isRequired,
+  position: PropTypes.oneOf(['header', 'PropTypes.oneOf']).isRequired,
   children: PropTypes.node.isRequired,
 };
 

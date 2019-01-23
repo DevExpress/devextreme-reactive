19a220416330bd22c0ab1fc69c5e3cbc9113052b
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2019-01-23 16:35:52 +0300

    Simplify perf HOC

--- packages/dx-demo-shell/src/demo-viewer/perf-wrapper.jsx
+++ packages/dx-demo-shell/src/demo-viewer/perf-wrapper.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as PropTypes from 'prop-types';
 
 const analyze = (list) => {
   const items = list.slice().sort((a, b) => a - b);
@@ -30,50 +29,25 @@ const spanStyle = {
   padding: '4px',
 };
 
-export const wrapDemo = (Demo) => {
+export const wrapDemo = (Demo, count = 16) => {
   class WrappedDemo extends React.PureComponent {
     constructor(props) {
       super(props);
-      this.node = React.createElement(Demo, {});
-      this.state = { refs: [] };
-    }
-
-    static getDerivedStateFromProps({ count }, { refs }) {
-      return refs.length !== count ? {
-        refs: Array.from({ length: count }).map(React.createRef),
-      } : null;
+      this.state = {};
+      this.itemsRefs = Array.from({ length: count }).map(React.createRef);
     }
 
     componentDidMount() {
-      const { refs } = this.state;
-      this.createSamples(refs);
-    }
-
-    componentDidUpdate(prevProps, { refs: prevRefs }) {
-      const { refs } = this.state;
-      if (prevRefs !== refs) {
-        this.destroySamples(prevRefs);
-        this.createSamples(refs);
-      } else {
-        this.updateSamples(refs);
-      }
-    }
-
-    componentWillUnmount() {
-      const { refs } = this.state;
-      this.destroySamples(refs);
-    }
-
-    createSamples(refs) {
       // Looks like that when `ReactDOM.render` is called inside a "thread" where another component
       // is already being rendered function returns almost immediately (end ~= start) and render
       // request is scheduled.
       // Calling `ReactDOM.render` in another "thread" seems to help.
       this.timeout = setTimeout(() => {
         this.initialized = true;
-        const times = refs.map((ref) => {
+        const node = React.createElement(Demo, {});
+        const times = this.itemsRefs.map((ref) => {
           const start = performance.now();
-          ReactDOM.render(this.node, ref.current);
+          ReactDOM.render(node, ref.current);
           const end = performance.now();
           return end - start;
         });
@@ -83,26 +57,26 @@ export const wrapDemo = (Demo) => {
       }, 10);
     }
 
-    updateSamples(refs) {
+    componentDidUpdate() {
       if (this.initialized) {
-        refs.forEach((ref) => {
-          ReactDOM.render(this.node, ref.current);
+        const node = React.createElement(Demo, {});
+        this.itemsRefs.forEach((ref) => {
+          ReactDOM.render(node, ref.current);
         });
       }
     }
 
-    destroySamples(refs) {
+    componentWillUnmount() {
       clearTimeout(this.timeout);
       if (this.initialized) {
-        refs.forEach((ref) => {
+        this.itemsRefs.forEach((ref) => {
           ReactDOM.unmountComponentAtNode(ref.current);
         });
-        this.initialized = false;
       }
     }
 
     render() {
-      const { refs, metrics } = this.state;
+      const { metrics } = this.state;
       return (
         <div className="performance-metrics">
           <div>
@@ -115,7 +89,7 @@ export const wrapDemo = (Demo) => {
             ))}
           </div>
           <div className="demo-list" style={listStyle}>
-            {refs.map((ref, i) => (
+            {this.itemsRefs.map((ref, i) => (
               // eslint-disable-next-line react/no-array-index-key
               <div ref={ref} key={i} className="demo-item" style={itemStyle} />
             ))}
@@ -124,15 +98,6 @@ export const wrapDemo = (Demo) => {
       );
     }
   }
-
-  WrappedDemo.propTypes = {
-    // False alarm - used in `getDerivedStateFromProps`.
-    count: PropTypes.number, // eslint-disable-line react/no-unused-prop-types
-  };
-
-  WrappedDemo.defaultProps = {
-    count: 16,
-  };
 
   return WrappedDemo;
 };

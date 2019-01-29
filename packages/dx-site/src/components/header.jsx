ffb194caf4b4b07a087128068d32a22241fba1a2
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2019-01-29 11:33:49 +0300

    Make menu responsive

--- packages/dx-site/src/components/header.jsx
+++ packages/dx-site/src/components/header.jsx
@@ -3,44 +3,72 @@ import * as PropTypes from 'prop-types';
 
 import styles from './header.module.scss';
 
-const Header = ({ logo, links, addon }) => (
-  <header className={styles.header}>
-    <div className={`container ${styles.headerContainer}`}>
-      <div className="row align-items-center">
-        <div className="col-auto mr-auto">
-          {logo}
-        </div>
-        <div className="col-auto">
-          <div className={styles.links}>
-            {links}
-            <a
-              href="https://community.devexpress.com/tags/DevExtreme+Reactive/default.aspx"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Blog
-            </a>
-            <a
-              href="https://github.com/DevExpress/devextreme-reactive"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-            <a
-              href="https://js.devexpress.com/Buy/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Buy
-            </a>
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false,
+    };
+    this.toggleVisibility = this.toggleVisibility.bind(this);
+  }
+
+  toggleVisibility() {
+    const { visibility } = this.state;
+    this.setState({ visibility: !visibility });
+  }
+
+  render() {
+    const { logo, links, addon } = this.props;
+    const { visibility } = this.state;
+    return (
+      <header className={styles.header}>
+        <div className={`container ${styles.headerContainer}`}>
+          <div className="row align-items-center">
+            <div className="col-auto mr-auto">
+              {logo}
+            </div>
+            <div className="col-auto">
+              <button
+                className="d-block d-sm-none"
+                type="button"
+                onClick={this.toggleVisibility}
+              >
+                Menu
+              </button>
+              <div
+                className={`${styles.links} ${visibility ? styles.opened : ''}`}
+              >
+                {links}
+                <a
+                  href="https://community.devexpress.com/tags/DevExtreme+Reactive/default.aspx"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Blog
+                </a>
+                <a
+                  href="https://github.com/DevExpress/devextreme-reactive"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+                <a
+                  href="https://js.devexpress.com/Buy/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Buy
+                </a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    {addon}
-  </header>
-);
+        {addon}
+      </header>
+    );
+  }
+}
 
 Header.propTypes = {
   logo: PropTypes.node.isRequired,

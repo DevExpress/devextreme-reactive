aff58a843f499f37f766ec020890dc408fa5118b
Nikita Ushkal <nikita.ushkal@devexpress.com>
2019-01-29 18:12:29 +0300

    fix test

--- packages/dx-core/src/memoize.ts
+++ packages/dx-core/src/memoize.ts
@@ -1,11 +1,10 @@
 import { argumentsShallowEqual } from './shallow-equal';
-import { Memoized } from '@devexpress/dx-core';
 
 /** @internal */
 export const memoize = (func) => {
   let lastArgs: any = null;
   let lastResult: ReturnType<typeof func> = null;
-  return (...args): Memoized<typeof func> => {
+  return (...args) => {
     if (lastArgs === null || !argumentsShallowEqual(lastArgs, args)) {
       lastResult = func(...args);
     }

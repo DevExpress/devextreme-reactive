82221a4982ad6cad47763f247662e23f6688f53f
Sergey Alexeev <SergeyAlexeev@users.noreply.github.com>
2019-02-13 14:43:22 +0300

    Upd config files

--- packages/dx-react-scheduler/rollup.config.js
+++ packages/dx-react-scheduler/rollup.config.js
@@ -1,24 +1,31 @@
-import babel from 'rollup-plugin-babel';
 import license from 'rollup-plugin-license';
+import { default as typescriptRollup } from 'rollup-plugin-typescript2';
+import replace from "rollup-plugin-replace";
+import typescript from 'typescript';
 import resolve from 'rollup-plugin-node-resolve';
-import { banner, external, globals, stubProcess } from '../../tools/rollup-utils';
+import {
+  banner, external, globals, stubProcess,
+} from '../../tools/rollup-utils';
 import pkg from './package.json';
 
 export default {
-  input: 'src/index.js',
+  input: 'src/index.ts',
   output: [
-    { file: pkg.main, format: 'umd', name: pkg.globalName, sourcemap: true, globals: globals(), intro: stubProcess },
+    {
+      file: pkg.main, format: 'umd', name: pkg.globalName, sourcemap: true, globals: globals(), intro: stubProcess
+    },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   external: external(__dirname),
   plugins: [
-    resolve({
-      main: false,
-      extensions: ['.js', '.jsx'],
+    typescriptRollup({
+      typescript,
+      useTsconfigDeclarationDir: true,
+      abortOnError: false,
     }),
-    babel({
-      runtimeHelpers: false,
-      exclude: 'node_modules/**',
+    replace({
+      "/** @class */": "/*#__PURE__*/",
+      delimiters: ["", ""],
     }),
     license({
       banner,

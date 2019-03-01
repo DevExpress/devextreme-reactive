9e2ed6e260e51a564a9c40db76d5815542a7de16
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-03-01 12:12:41 +0300

    Add tests for helpers

---
+++ packages/dx-scheduler-core/src/plugins/drag-drop-provider/helpers.test.ts
@@ -0,0 +1,54 @@
+import { intervalDuration, cellIndex, cellData } from './helpers';
+
+describe('DragDropProvider', () => {
+  describe('#intervalDuration', () => {
+    const data = {
+      startDate: new Date('2019-3-1 10:00'),
+      endDate: new Date('2019-3-1 11:00'),
+    };
+
+    it('should work with minutes', () => {
+      expect(intervalDuration(data, 'minutes'))
+        .toEqual(60);
+    });
+
+    it('should work with seconds', () => {
+      expect(intervalDuration(data, 'seconds'))
+        .toEqual(60 * 60);
+    });
+  });
+
+  describe('#cellIndex', () => {
+    const cells = [
+      { getBoundingClientRect: () => ({ top: 0, left: 0, right: 50, bottom: 50 }) },
+      { getBoundingClientRect: () => ({ top: 0, left: 50, right: 100, bottom: 50 }) },
+    ];
+
+    it('should work', () => {
+      expect(cellIndex(cells as Element[], { x: 10, y: 10 }))
+        .toEqual(0);
+    });
+
+    it('should take only one cell by condition', () => {
+      expect(cellIndex(cells as Element[], { x: 50, y: 10 }))
+        .toEqual(0);
+    });
+  });
+
+  describe('#cellData', () => {
+    const cellsData = [
+      [{ startDate: new Date('2019-3-1 10:00') }, { startDate: new Date('2019-3-2 10:00') }],
+      [{ startDate: new Date('2019-3-1 11:00') }, { startDate: new Date('2019-3-2 11:00') }],
+    ];
+
+    it('should work with both indexes', () => {
+      expect(cellData(1, 1, cellsData).startDate)
+        .toEqual(new Date('2019-3-2 00:00'));
+    });
+
+    it('should work with only time table index', () => {
+      expect(cellData(2, -1, cellsData).startDate)
+        .toEqual(new Date('2019-3-1 11:00'));
+    });
+  });
+});

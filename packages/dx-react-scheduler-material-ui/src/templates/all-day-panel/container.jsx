fde96dc20b9ad4cc8975772740c12153d2632e9e
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-03-11 13:39:23 +0300

    Refactor container component

--- packages/dx-react-scheduler-material-ui/src/templates/all-day-panel/container.jsx
+++ packages/dx-react-scheduler-material-ui/src/templates/all-day-panel/container.jsx
@@ -1,7 +1,5 @@
-import * as React from 'react';
-import * as PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
+import { ContainerBase } from '../common/container';
 
 const styles = () => ({
   container: {
@@ -9,22 +7,4 @@ const styles = () => ({
   },
 });
 
-const ContainerBase = ({
-  children, classes, className, ...restProps
-}) => (
-  <div className={classNames(classes.container, className)} {...restProps}>
-    {children}
-  </div>
-);
-
-ContainerBase.propTypes = {
-  classes: PropTypes.object.isRequired,
-  children: PropTypes.node.isRequired,
-  className: PropTypes.string,
-};
-
-ContainerBase.defaultProps = {
-  className: undefined,
-};
-
-export const Container = withStyles(styles, { name: 'TitleCell' })(ContainerBase);
+export const Container = withStyles(styles, { name: 'AllDayContainer' })(ContainerBase);
--- packages/dx-react-scheduler-material-ui/src/templates/drag-drop/container.jsx
+++ packages/dx-react-scheduler-material-ui/src/templates/drag-drop/container.jsx
@@ -1,7 +1,5 @@
-import * as React from 'react';
-import * as PropTypes from 'prop-types';
-import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
+import { ContainerBase } from '../common/container';
 
 const containerStyles = () => ({
   container: {
@@ -14,27 +12,4 @@ const containerStyles = () => ({
   },
 });
 
-const ContainerBase = ({
-  classes, className, children,
-  ...restProps
-}) => (
-  <div
-    className={classNames(classes.container, className)}
-    {...restProps}
-  >
-    {children}
-  </div>
-);
-
-ContainerBase.propTypes = {
-  children: PropTypes.node,
-  classes: PropTypes.object.isRequired,
-  className: PropTypes.string,
-};
-
-ContainerBase.defaultProps = {
-  className: undefined,
-  children: undefined,
-};
-
-export const Container = withStyles(containerStyles, { name: 'Container' })(ContainerBase);
+export const Container = withStyles(containerStyles, { name: 'DragDropContainer' })(ContainerBase);

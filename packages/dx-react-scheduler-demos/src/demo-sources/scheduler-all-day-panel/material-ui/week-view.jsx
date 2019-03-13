16e7e2d57e63f6f9dd3c4634b8dc69d64062b151
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-03-13 10:52:56 +0300

    Remove all-day demo changes

--- packages/dx-react-scheduler-demos/src/demo-sources/scheduler-all-day-panel/material-ui/week-view.jsx
+++ packages/dx-react-scheduler-demos/src/demo-sources/scheduler-all-day-panel/material-ui/week-view.jsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
-import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
+import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler,
   WeekView,
   Appointments,
   AllDayPanel,
-  DragDropProvider,
-  ViewSwitcher,
-  Toolbar,
-  MonthView,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 import { appointments } from '../../../demo-data/appointments';
@@ -22,30 +18,6 @@ export default class Demo extends React.PureComponent {
       data: appointments,
       currentDate: new Date('2018-06-27'),
     };
-
-    this.onCommitChanges = this.commitChanges.bind(this);
-  }
-
-  commitChanges({ added, changed, deleted }) {
-    let { data } = this.state;
-    if (added) {
-      const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
-      data = [
-        ...data,
-        {
-          id: startingAddedId,
-          ...added,
-        },
-      ];
-    }
-    if (changed) {
-      data = data.map(appointment => (
-        changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
-    }
-    if (deleted) {
-      data = data.filter(appointment => appointment.id !== deleted);
-    }
-    this.setState({ data });
   }
 
   render() {
@@ -59,29 +31,12 @@ export default class Demo extends React.PureComponent {
           <ViewState
             defaultCurrentDate={currentDate}
           />
-          <EditingState
-            onCommitChanges={this.onCommitChanges}
-          />
           <WeekView
             startDayHour={9}
             endDayHour={19}
           />
-          <WeekView
-            name="Work Week"
-            startDayHour={9}
-            endDayHour={19}
-            excludedDays={[0, 6]}
-          />
-          <MonthView />
           <Appointments />
           <AllDayPanel />
-
-          <Toolbar />
-          <ViewSwitcher />
-
-          <DragDropProvider
-            allowDrag={({ title }) => title !== 'DRAGGING DISABLED'}
-          />
         </Scheduler>
       </Paper>
     );

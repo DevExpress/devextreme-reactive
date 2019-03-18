0dac771590ad328eced967e0ddfb061bdb450dea
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-03-18 17:05:17 +0300

    Refactor

--- packages/dx-react-grid/src/components/table-layout.tsx
+++ packages/dx-react-grid/src/components/table-layout.tsx
@@ -65,8 +65,7 @@ class TableLayoutBase extends React.PureComponent<TableLayoutCoreProps, TableLay
 
     const isFixedWidth = columns.filter(column => column.width === undefined).length === 0;
     if (isFixedWidth) {
-      result = Array.prototype.slice.call(result);
-      result.push({ key: TABLE_FLEX_TYPE.toString(), type: TABLE_FLEX_TYPE });
+      result = [...result, { key: TABLE_FLEX_TYPE.toString(), type: TABLE_FLEX_TYPE }];
     }
 
     if (animationState.size) {

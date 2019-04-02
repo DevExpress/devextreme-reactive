38423478f29c6f41503973df2f30b03efe250ddf
Nikita Ushkal <nikita.ushkal@devexpress.com>
2019-04-02 14:08:08 +0300

    fix performance - omit unnecessary animation

--- packages/dx-react-grid/src/components/table-layout.tsx
+++ packages/dx-react-grid/src/components/table-layout.tsx
@@ -8,11 +8,12 @@ import {
   TABLE_FLEX_TYPE,
   ColumnAnimationMap,
 } from '@devexpress/dx-grid-core';
+import { shallowEqual } from '@devexpress/dx-core';
 import { TableLayoutCoreProps, TableLayoutCoreState } from '../types';
 
 class TableLayoutBase extends React.PureComponent<TableLayoutCoreProps, TableLayoutCoreState> {
   animations: ColumnAnimationMap;
-  savedScrolldWidth: { [key: number]: number };
+  savedScrollWidth: { [key: number]: number };
   savedOffsetWidth = -1;
   tableRef: React.RefObject<HTMLTableElement>;
   raf = -1;
@@ -25,15 +26,30 @@ class TableLayoutBase extends React.PureComponent<TableLayoutCoreProps, TableLay
     };
 
     this.animations = new Map();
-    this.savedScrolldWidth = {};
+    this.savedScrollWidth = {};
     this.tableRef = React.createRef();
   }
 
   componentDidUpdate(prevProps) {
     const { columns } = this.props;
     const { columns: prevColumns } = prevProps;
+    const { animationState } = this.state;
 
+    const activeAnimationExists = !shallowEqual(columns, prevColumns)
+      || !!animationState.size || !!this.animations.size;
+
+    // NOTE: animation should be recomputed only when columns are changed or
+    // an active animation is in progress. Otherwise it will be recalculated on
+    // each scroll event.
+    if (activeAnimationExists) {
+      this.processAnimation(prevColumns);
+    }
+  }
+
+  processAnimation(prevColumns) {
+    const { columns } = this.props;
     const tableWidth = this.getTableWidth(prevColumns, columns);
+
     this.animations = getAnimations(prevColumns, columns, tableWidth, this.animations);
 
     cancelAnimationFrame(this.raf);
@@ -45,16 +61,16 @@ class TableLayoutBase extends React.PureComponent<TableLayoutCoreProps, TableLay
     const { animationState } = this.state;
 
     const widthChanged = this.savedOffsetWidth !== offsetWidth
-      || !this.savedScrolldWidth[columns.length];
+      || !this.savedScrollWidth[columns.length];
     const columnCountChanged = columns.length !== prevColumns.length;
 
     if (columnCountChanged || (widthChanged && !animationState.size)) {
-      this.savedScrolldWidth = {};
-      this.savedScrolldWidth[columns.length] = scrollWidth;
+      this.savedScrollWidth = {};
+      this.savedScrollWidth[columns.length] = scrollWidth;
       this.savedOffsetWidth = offsetWidth;
     }
 
-    return this.savedScrolldWidth[columns.length];
+    return this.savedScrollWidth[columns.length];
   }
 
   getColumns() {

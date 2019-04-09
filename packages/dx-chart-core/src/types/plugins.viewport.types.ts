6013aa3600ae3bfa41ffcc6a6f166ad8ced6e8e7
Smirnova Yuliya <Krijovnick@users.noreply.github.com>
2019-04-09 16:30:04 +0300

    feat(react-chart): add zoom and pan for chart (#1917)

--- packages/dx-chart-core/src/types/plugins.viewport.types.ts
+++
@@ -1,7 +0,0 @@
-export type DomainBounds = Readonly<[any, any]>;
-
-export type ViewportOptions = {
-  readonly argumentBounds?: DomainBounds;
-  readonly scaleName?: string;
-  readonly valueBounds?: DomainBounds;
-};

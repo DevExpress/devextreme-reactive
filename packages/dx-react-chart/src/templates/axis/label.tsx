b7edcccb7495328fe17763456024731eab96e094
Smirnova Yuliya <yuliya.smirnova@devexpress.com>
2019-05-06 11:08:15 +0300

    fix label template

--- packages/dx-react-chart/src/templates/axis/label.tsx
+++ packages/dx-react-chart/src/templates/axis/label.tsx
@@ -4,15 +4,11 @@ import { Axis } from '../../types';
 export class Label extends React.PureComponent<Axis.LabelProps> {
   render() {
     const {
-      text, x, y, dy, textAnchor, ...restProps
+      text, ...restProps
     } = this.props;
 
     return (
       <text
-        dy={dy}
-        textAnchor={textAnchor}
-        x={x}
-        y={y}
         {...restProps}
       >
         {text}

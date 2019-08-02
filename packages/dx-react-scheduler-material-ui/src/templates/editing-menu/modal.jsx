827dae1669d081e5585075b22729f2ee68799dd4
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-08-02 20:00:40 +0300

    Refactor templates

--- packages/dx-react-scheduler-material-ui/src/templates/editing-menu/modal.jsx
+++ packages/dx-react-scheduler-material-ui/src/templates/editing-menu/modal.jsx
@@ -1,22 +1,44 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-// import classNames from 'classnames';
+import classNames from 'classnames';
 import Dialog from '@material-ui/core/Dialog';
+import { withStyles } from '@material-ui/core/styles';
 
-export const Modal = ({
-  children, open, onClose, containerRef, onBackdropClick, ...restProps
-}) => {
-  return (
-    <Dialog
-      open={open}
-      onClose={onClose}
-      style={{ position: 'absolute' }}
-      BackdropProps={{ style: { position: 'absolute' } }}
-      container={containerRef.current}
-      onBackdropClick={onBackdropClick}
-      {...restProps}
-    >
-      {children}
-    </Dialog>
-  );
+const styles = {
+  modal: {
+    position: 'absolute',
+  },
 };
+
+const ModalBase = ({
+  children, open, onClose, containerRef, onBackdropClick, classes, className, ...restProps
+}) => (
+  <Dialog
+    open={open}
+    onClose={onClose}
+    className={classNames(classes.modal, className)}
+    BackdropProps={{ className: classes.modal }}
+    container={containerRef.current}
+    onBackdropClick={onBackdropClick}
+    {...restProps}
+  >
+    {children}
+  </Dialog>
+);
+
+ModalBase.propTypes = {
+  children: PropTypes.node.isRequired,
+  classes: PropTypes.object.isRequired,
+  onClose: PropTypes.func.isRequired,
+  containerRef: PropTypes.object.isRequired,
+  onBackdropClick: PropTypes.func.isRequired,
+  open: PropTypes.bool,
+  className: PropTypes.string,
+};
+
+ModalBase.defaultProps = {
+  open: false,
+  className: undefined,
+};
+
+export const Container = withStyles(styles, { name: 'Container' })(ModalBase);

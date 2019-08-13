0852339b3c20a3e4f5c676ada815162416888c7b
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-08-13 16:40:52 +0300

    Refactor form base layout

--- packages/dx-react-scheduler-material-ui/src/templates/appointment-form/basic/layout.jsx
+++ packages/dx-react-scheduler-material-ui/src/templates/appointment-form/basic/layout.jsx
@@ -11,9 +11,13 @@ import {
 
 const styles = theme => ({
   root: {
+    width: '65%',
     overflowY: 'auto',
     paddingTop: theme.spacing(3),
     paddingBottom: 0,
+  },
+  fullSize: {
+    width: '100%',
   },
 });
 
@@ -21,7 +25,6 @@ const LayoutBase = ({
   children,
   classes,
   className,
-  style,
   textEditorComponent: TextEditor,
   dateTimeEditorComponent: DateTimeEditor,
   recurrenceSwitcherComponent: RecurrenceSwitcher,
@@ -32,81 +35,74 @@ const LayoutBase = ({
   changedAppointment,
   readOnly,
   ...restProps
-}) => {
-  const layoutStyle = !changedAppointment.rRule ? {
-    width: '100%',
-  } : {
-    width: '65%',
-  };
-  return (
-    <div
-      className={classNames(classes.root, className)}
-      style={{ ...layoutStyle, ...style }}
-      {...restProps}
-    >
-      <Label
-        label={getMessage('detailsLabel')}
-        id={TITLE_LABEL}
-      />
-      <TextEditor
-        label={getMessage('titleLabel')}
-        readOnly={readOnly}
-        id={TITLE_TEXT_EDITOR}
-        value={changedAppointment.title}
-        onValueChange={title => onAppointmentFieldChange({ change: { title } })}
-      />
-      <DateTimeEditor
-        disabled={readOnly}
-        firstDate={changedAppointment.startDate}
-        secondDate={changedAppointment.endDate}
-        onFirstDateValueChange={startDate => onAppointmentFieldChange({
-          change: { startDate: startDate.toDate() },
-        })}
-        onSecondDateValueChange={endDate => onAppointmentFieldChange({
-          change: { endDate: endDate.toDate() },
-        })}
-        id={FULL_DATE_TIME_EDITOR}
-      />
-      <Label
-        label={getMessage('moreInformationLabel')}
-        id={TITLE_LABEL}
-      />
-      <TextEditor
-        label={getMessage('additionalInformationLabel')}
-        readOnly={readOnly}
-        value={changedAppointment.additionalInformation}
-        onValueChange={additionalInformation => onAppointmentFieldChange({
-          change: { additionalInformation },
-        })}
-      />
-      <TextEditor
-        label={getMessage('notesLabel')}
-        readOnly={readOnly}
-        id={NOTES_TEXT_EDITOR}
-        value={changedAppointment.notes}
-        onValueChange={notes => onAppointmentFieldChange({ change: { notes } })}
-      />
-      <AllDay
-        label={getMessage('allDayLabel')}
-        readOnly={readOnly}
-        value={changedAppointment.allDay}
-        onValueChange={allDay => onAppointmentFieldChange({ change: { allDay } })}
-      />
-      {
-        (!changedAppointment.rRule) && (
-          <React.Fragment>
-            <Label
-              label={getMessage('repeatLabel')}
-              id={TITLE_LABEL}
-            />
-            <RecurrenceSwitcher />
-          </React.Fragment>
-        )
-      }
-      {children}
-    </div>
-  );
-};
+}) => (
+  <div
+    className={classNames({
+      [classes.root]: true,
+      [classes.fullSize]: !changedAppointment.rRule,
+    }, className)}
+    {...restProps}
+  >
+    <Label
+      label={getMessage('detailsLabel')}
+      id={TITLE_LABEL}
+    />
+    <TextEditor
+      label={getMessage('titleLabel')}
+      readOnly={readOnly}
+      id={TITLE_TEXT_EDITOR}
+      value={changedAppointment.title}
+      onValueChange={title => onAppointmentFieldChange({ change: { title } })}
+    />
+    <DateTimeEditor
+      disabled={readOnly}
+      firstDate={changedAppointment.startDate}
+      secondDate={changedAppointment.endDate}
+      onFirstDateValueChange={startDate => onAppointmentFieldChange({
+        change: { startDate: startDate.toDate() },
+      })}
+      onSecondDateValueChange={endDate => onAppointmentFieldChange({
+        change: { endDate: endDate.toDate() },
+      })}
+      id={FULL_DATE_TIME_EDITOR}
+    />
+    <Label
+      label={getMessage('moreInformationLabel')}
+      id={TITLE_LABEL}
+    />
+    <TextEditor
+      label={getMessage('additionalInformationLabel')}
+      readOnly={readOnly}
+      value={changedAppointment.additionalInformation}
+      onValueChange={additionalInformation => onAppointmentFieldChange({
+        change: { additionalInformation },
+      })}
+    />
+    <TextEditor
+      label={getMessage('notesLabel')}
+      readOnly={readOnly}
+      id={NOTES_TEXT_EDITOR}
+      value={changedAppointment.notes}
+      onValueChange={notes => onAppointmentFieldChange({ change: { notes } })}
+    />
+    <AllDay
+      label={getMessage('allDayLabel')}
+      readOnly={readOnly}
+      value={changedAppointment.allDay}
+      onValueChange={allDay => onAppointmentFieldChange({ change: { allDay } })}
+    />
+    {(!changedAppointment.rRule) && (
+      <React.Fragment>
+        <Label
+          label={getMessage('repeatLabel')}
+          id={TITLE_LABEL}
+        />
+        <RecurrenceSwitcher />
+      </React.Fragment>
+    )}
+    {children}
+  </div>
+);
 
 LayoutBase.propTypes = {
   textEditorComponent: PropTypes.oneOfType([PropTypes.func, PropTypes.object]).isRequired,
@@ -117,7 +113,6 @@ LayoutBase.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
   classes: PropTypes.object.isRequired,
-  style: PropTypes.object,
   getMessage: PropTypes.func.isRequired,
   onAppointmentFieldChange: PropTypes.func,
   changedAppointment: PropTypes.object.isRequired,
@@ -126,7 +121,6 @@ LayoutBase.propTypes = {
 
 LayoutBase.defaultProps = {
   className: undefined,
-  style: null,
   readOnly: false,
   onAppointmentFieldChange: () => undefined,
 };

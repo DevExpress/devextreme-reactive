ab33cd7c517fd7af04574137fa1e6de5757eb30b
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2019-08-20 11:34:46 +0300

    Test helpers

--- packages/dx-react-scheduler-material-ui/src/templates/appointment-form/helpers.test.jsx
+++ packages/dx-react-scheduler-material-ui/src/templates/appointment-form/helpers.test.jsx
@@ -1,79 +1,141 @@
-import { getDaysOfWeek, getMonths, getNumberLabels } from './helpers';
+import {
+  LONG_WEEK_DAY_OPTIONS, SUNDAY_DATE, MONDAY_DATE, DAY_LONG_MONTH_OPTIONS,
+  TUESDAY_DATE, WEDNESDAY_DATE, THURSDAY_DATE, FRIDAY_DATE, SATURDAY_DATE,
+  JANUARY_DATE, LONG_MONTH_OPTIONS, FEBRUARY_DATE, MARCH_DATE, APRIL_DATE,
+  MAY_DATE, AUGUST_DATE, OCTOBER_DATE, NOVEMBER_DATE, DECEMBER_DATE, SEPTEMBER_DATE,
+  JULY_DATE, JUNE_DATE, getRRuleFrequency, changeRecurrenceFrequency,
+} from '@devexpress/dx-scheduler-core';
+import {
+  getDaysOfWeek, getMonths, getNumberLabels, handleChangeFrequency, getMonthsWithOf,
+} from './helpers';
 
-const defaultMessages = {
-  firstLabel: 'First',
-  secondLabel: 'Second',
-  thirdLabel: 'Third',
-  fourthLabel: 'Fourth',
-  lastLabel: 'Last',
-  januaryLabel: 'January',
-  februaryLabel: 'February',
-  marchLabel: 'March',
-  aprilLabel: 'April',
-  mayLabel: 'May',
-  juneLabel: 'June',
-  julyLabel: 'July',
-  augustLabel: 'August',
-  septemberLabel: 'September',
-  octoberLabel: 'October',
-  novemberLabel: 'November',
-  decemberLabel: 'December',
-  sundayLabel: 'Sunday',
-  mondayLabel: 'Monday',
-  tuesdayLabel: 'Tuesday',
-  wednesdayLabel: 'Wednesday',
-  thursdayLabel: 'Thursday',
-  fridayLabel: 'Friday',
-  saturdayLabel: 'Saturday',
-};
+jest.mock('@devexpress/dx-scheduler-core', () => ({
+  ...require.requireActual('@devexpress/dx-scheduler-core'),
+  getRRuleFrequency: jest.fn(),
+  changeRecurrenceFrequency: jest.fn(),
+}));
 
-describe('AppointmentForm recurrence radio group helpers', () => {
+describe('AppointmentForm helpers', () => {
   const defaultProps = {
-    getMessage: message => defaultMessages[message],
+    getMessage: jest.fn(),
+    formatDate: jest.fn(),
   };
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
   describe('#getMonths', () => {
-    it('should return months depending on getMessage function', () => {
-      expect(getMonths(defaultProps.getMessage))
-        .toEqual([
-          { text: 'January', id: 1 },
-          { text: 'February', id: 2 },
-          { text: 'March', id: 3 },
-          { text: 'April', id: 4 },
-          { text: 'May', id: 5 },
-          { text: 'June', id: 6 },
-          { text: 'July', id: 7 },
-          { text: 'August', id: 8 },
-          { text: 'September', id: 9 },
-          { text: 'October', id: 10 },
-          { text: 'November', id: 11 },
-          { text: 'December', id: 12 },
-        ]);
+    it('should return months depending formatDate function', () => {
+      getMonths(defaultProps.formatDate);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(JANUARY_DATE, LONG_MONTH_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(FEBRUARY_DATE, LONG_MONTH_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(MARCH_DATE, LONG_MONTH_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(APRIL_DATE, LONG_MONTH_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(MAY_DATE, LONG_MONTH_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(JUNE_DATE, LONG_MONTH_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(JULY_DATE, LONG_MONTH_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(AUGUST_DATE, LONG_MONTH_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(SEPTEMBER_DATE, LONG_MONTH_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(OCTOBER_DATE, LONG_MONTH_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(NOVEMBER_DATE, LONG_MONTH_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(DECEMBER_DATE, LONG_MONTH_OPTIONS);
+    });
+  });
+  describe('#getMonthsWithOf', () => {
+    it('should return months with of', () => {
+      const formatDateWithReturn = jest.fn(() => 'test return value');
+      getMonthsWithOf(defaultProps.getMessage, formatDateWithReturn);
+
+      expect(defaultProps.getMessage)
+        .toHaveBeenCalledTimes(12);
+      expect(defaultProps.getMessage)
+        .toHaveBeenCalledWith('ofLabel');
+
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(JANUARY_DATE, DAY_LONG_MONTH_OPTIONS);
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(FEBRUARY_DATE, DAY_LONG_MONTH_OPTIONS);
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(MARCH_DATE, DAY_LONG_MONTH_OPTIONS);
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(APRIL_DATE, DAY_LONG_MONTH_OPTIONS);
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(MAY_DATE, DAY_LONG_MONTH_OPTIONS);
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(JUNE_DATE, DAY_LONG_MONTH_OPTIONS);
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(JULY_DATE, DAY_LONG_MONTH_OPTIONS);
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(AUGUST_DATE, DAY_LONG_MONTH_OPTIONS);
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(SEPTEMBER_DATE, DAY_LONG_MONTH_OPTIONS);
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(OCTOBER_DATE, DAY_LONG_MONTH_OPTIONS);
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(NOVEMBER_DATE, DAY_LONG_MONTH_OPTIONS);
+      expect(formatDateWithReturn)
+        .toHaveBeenCalledWith(DECEMBER_DATE, DAY_LONG_MONTH_OPTIONS);
     });
   });
   describe('#getDaysOfWeek', () => {
     it('should return days of week depending on getMessage function', () => {
-      expect(getDaysOfWeek(defaultProps.getMessage))
-        .toEqual([
-          { text: 'Sunday', id: 0 },
-          { text: 'Monday', id: 1 },
-          { text: 'Tuesday', id: 2 },
-          { text: 'Wednesday', id: 3 },
-          { text: 'Thursday', id: 4 },
-          { text: 'Friday', id: 5 },
-          { text: 'Saturday', id: 6 },
-        ]);
+      getDaysOfWeek(defaultProps.formatDate);
+
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(SUNDAY_DATE, LONG_WEEK_DAY_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(MONDAY_DATE, LONG_WEEK_DAY_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(TUESDAY_DATE, LONG_WEEK_DAY_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(WEDNESDAY_DATE, LONG_WEEK_DAY_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(THURSDAY_DATE, LONG_WEEK_DAY_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(FRIDAY_DATE, LONG_WEEK_DAY_OPTIONS);
+      expect(defaultProps.formatDate)
+        .toHaveBeenCalledWith(SATURDAY_DATE, LONG_WEEK_DAY_OPTIONS);
     });
   });
   describe('#getNumberLabels', () => {
     it('should return days of week depending on getMessage function', () => {
-      expect(getNumberLabels(defaultProps.getMessage))
-        .toEqual([
-          { text: 'First', id: 0 },
-          { text: 'Second', id: 1 },
-          { text: 'Third', id: 2 },
-          { text: 'Fourth', id: 3 },
-          { text: 'Last', id: 4 },
-        ]);
+      getNumberLabels(defaultProps.getMessage);
+
+      expect(defaultProps.getMessage)
+        .toHaveBeenCalledWith('firstLabel');
+      expect(defaultProps.getMessage)
+        .toHaveBeenCalledWith('secondLabel');
+      expect(defaultProps.getMessage)
+        .toHaveBeenCalledWith('thirdLabel');
+      expect(defaultProps.getMessage)
+        .toHaveBeenCalledWith('fourthLabel');
+      expect(defaultProps.getMessage)
+        .toHaveBeenCalledWith('lastLabel');
+    });
+  });
+  describe('#handleChangeFrequency', () => {
+    it('should change frequency', () => {
+      getRRuleFrequency.mockImplementation(() => 'daily');
+      changeRecurrenceFrequency.mockImplementation(() => undefined);
+      const action = jest.fn();
+      handleChangeFrequency('repeat_type', {}, action);
+      expect(getRRuleFrequency)
+        .toBeCalledTimes(1);
+      expect(changeRecurrenceFrequency)
+        .toBeCalledTimes(1);
+      expect(action)
+        .toBeCalledTimes(1);
     });
   });
 });

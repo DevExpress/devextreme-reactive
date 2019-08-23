084514c9cc6ac0b5093d6b3987b5436cd7ebb707
Maxim Kudryavtsev <maxim_71rus_wow@mail.ru>
2019-08-23 16:38:18 +0300

    fix(react-grid): prevent scrolling sticky header on Safari (#2178) (#2269)

--- packages/dx-react-grid-material-ui/src/templates/table-container.test.jsx
+++ packages/dx-react-grid-material-ui/src/templates/table-container.test.jsx
@@ -46,4 +46,16 @@ describe('TableContainer', () => {
     expect(tree.props().data)
       .toMatchObject({ a: 1 });
   });
+
+  it('should render a `div` block as a workaround Safari sticky bug', () => {
+    // https://bugs.webkit.org/show_bug.cgi?id=175029
+    const tree = shallow((
+      <TableContainer>
+        <div className="child" />
+      </TableContainer>
+    ));
+
+    expect(tree.find('div').length)
+      .toBe(3);
+  });
 });

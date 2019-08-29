a1491f1da6ae9b6c5909609b3c94fd324cdaa796
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-08-29 16:57:53 +0300

    Intial commit

---
+++ packages/dx-react-scheduler/src/plugins/basic-view.tsx
@@ -0,0 +1,206 @@
+import * as React from 'react';
+import {
+  Template,
+  Plugin,
+  Getter,
+  TemplateConnector,
+  TemplatePlaceholder,
+  PluginComponents,
+  ComputedFn,
+} from '@devexpress/dx-react-core';
+import {
+  computed,
+  startViewDate as startViewDateCore,
+  monthCellsData,
+  getAppointmentStyle,
+  endViewDate as endViewDateCore,
+  availableViews as availableViewsCore,
+  horizontalTimeTableRects,
+} from '@devexpress/dx-scheduler-core';
+import { memoize } from '@devexpress/dx-core';
+
+class BasicViewBase extends React.PureComponent {
+  state = {
+    rects: [],
+    timeTableElementsMeta: {},
+    scrollingStrategy: {
+      topBoundary: 0,
+      bottomBoundary: 0,
+      changeVerticalScroll: () => undefined,
+    },
+  };
+
+  scrollingStrategyComputed = memoize((viewName, scrollingStrategy) => getters =>
+    computed(getters, viewName!, () => scrollingStrategy, getters.scrollingStrategy));
+
+  timeTableElementsMetaComputed = memoize((viewName, timeTableElementsMeta) => getters =>
+    computed(getters, viewName!, () => timeTableElementsMeta, getters.timeTableElementsMeta));
+
+  firstDayOfWeekComputed = memoize((viewName, firstDayOfWeek) => getters =>
+    computed(getters, viewName!, () => firstDayOfWeek, getters.firstDayOfWeek));
+
+  intervalCountComputed = memoize((viewName, intervalCount) => getters =>
+    computed(getters, viewName!, () => intervalCount, getters.intervalCount));
+
+  availableViewsComputed = memoize((viewName, viewDisplayName) => ({ availableViews }) =>
+    availableViewsCore(availableViews, viewName!, viewDisplayName));
+
+  currentViewComputed = memoize((viewName, viewDisplayName) => ({ currentView }) => (
+    currentView && currentView.name !== viewName
+      ? currentView
+      : { name: viewName, type: TYPE, displayName: viewDisplayName }
+  ));
+
+  endViewDateComputed: ComputedFn = (getters) => {
+    const { name: viewName } = this.props;
+    return computed(
+      getters, viewName!, endViewDateBaseComputed, getters.endViewDate,
+    );
+  }
+
+  startViewDateComputed: ComputedFn = (getters) => {
+    const { name: viewName } = this.props;
+    return computed(
+      getters, viewName!, startViewDateBaseComputed, getters.startViewDate,
+    );
+  }
+
+  viewCellsDataComputed: ComputedFn = (getters) => {
+    const { name: viewName } = this.props;
+    return computed(
+      getters,
+      viewName!,
+      viewCellsDataBaseComputed(getters.firstDayOfWeek, getters.intervalCount),
+      getters.viewCellsData,
+    );
+  }
+
+  updateRects = memoize((
+    appointments, startViewDate, endViewDate, viewCellsData,
+  ) => (cellElementsMeta) => {
+    const rects = horizontalTimeTableRects(
+      appointments, startViewDate, endViewDate,
+      viewCellsData, cellElementsMeta,
+    );
+
+    this.setState({ rects, timeTableElementsMeta: cellElementsMeta });
+  });
+
+  setScrollingStrategy = (scrollingStrategy) => {
+    this.setState({ scrollingStrategy });
+  }
+  render() {
+    return (
+      <Plugin name="basicView">
+        <Getter
+          name="availableViews"
+          computed={this.availableViewsComputed(viewName, viewDisplayName)}
+        />
+        <Getter name="currentView" computed={this.currentViewComputed(viewName, viewDisplayName)} />
+
+        <Getter
+          name="firstDayOfWeek"
+          computed={this.firstDayOfWeekComputed(viewName, firstDayOfWeek)}
+        />
+        <Getter
+          name="intervalCount"
+          computed={this.intervalCountComputed(viewName, intervalCount)}
+        />
+        <Getter name="viewCellsData" computed={this.viewCellsDataComputed} />
+        <Getter name="startViewDate" computed={this.startViewDateComputed} />
+        <Getter name="endViewDate" computed={this.endViewDateComputed} />
+
+        <Getter
+          name="timeTableElementsMeta"
+          computed={this.timeTableElementsMetaComputed(viewName, timeTableElementsMeta)}
+        />
+        <Getter
+          name="scrollingStrategy"
+          computed={this.scrollingStrategyComputed(viewName, scrollingStrategy)}
+        />
+
+        <Template name="body">
+          <TemplateConnector>
+            {({ currentView }) => {
+              if (currentView.name !== viewName) return <TemplatePlaceholder />;
+              return (
+                <Layout
+                  dayScaleComponent={DayScalePlaceholder}
+                  timeTableComponent={TimeTablePlaceholder}
+                  setScrollingStrategy={this.setScrollingStrategy}
+                />
+              );
+            }}
+          </TemplateConnector>
+        </Template>
+
+        <Template name="dayScale">
+          <TemplateConnector>
+            {({ currentView, viewCellsData, formatDate }) => {
+              if (currentView.name !== viewName) return <TemplatePlaceholder />;
+              return (
+                <DayScale
+                  cellComponent={DayScaleCell}
+                  rowComponent={DayScaleRow}
+                  cellsData={viewCellsData}
+                  formatDate={formatDate}
+                />
+              );
+            }}
+          </TemplateConnector>
+        </Template>
+        <Template name="timeTable">
+          <TemplateConnector>
+            {({
+              appointments, startViewDate, endViewDate, currentView, viewCellsData, formatDate,
+            }) => {
+              if (currentView.name !== viewName) return <TemplatePlaceholder />;
+              const setRects = this.updateRects(
+                appointments, startViewDate, endViewDate, viewCellsData,
+              );
+              return (
+                <React.Fragment>
+                  <TimeTableLayout
+                    cellsData={viewCellsData}
+                    rowComponent={timeTableRowComponent}
+                    cellComponent={CellPlaceholder}
+                    formatDate={formatDate}
+                    setCellElementsMeta={setRects}
+                  />
+                  <AppointmentLayer>
+                    {rects.map(({
+                      dataItem, type, fromPrev, toNext, ...geometry
+                    }, index) => (
+                      <AppointmentPlaceholder
+                        key={index.toString()}
+                        type={type}
+                        data={dataItem}
+                        fromPrev={fromPrev}
+                        toNext={toNext}
+                        style={getAppointmentStyle(geometry)}
+                      />
+                    ))}
+                  </AppointmentLayer>
+                </React.Fragment>
+              );
+            }}
+          </TemplateConnector>
+        </Template>
+
+        <Template name="cell">
+          {params => (
+            <TemplateConnector>
+              {({ currentView }) => {
+                if (currentView.name !== viewName) return <TemplatePlaceholder params={params} />;
+                return (
+                  <TimeTableCell {...params} />
+                );
+              }}
+            </TemplateConnector>
+          )}
+        </Template>
+      </Plugin>
+    );
+  }
+}
+export const BasicView: React.ComponentType = BasicViewBase;

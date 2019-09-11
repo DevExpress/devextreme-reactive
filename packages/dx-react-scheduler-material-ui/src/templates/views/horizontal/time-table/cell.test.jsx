317c1b42a2b9c103bcd56c021ac211cc723f4349
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2019-09-11 10:19:52 +0300

    Change test description

--- packages/dx-react-scheduler-material-ui/src/templates/views/horizontal/time-table/cell.test.jsx
+++ packages/dx-react-scheduler-material-ui/src/templates/views/horizontal/time-table/cell.test.jsx
@@ -79,7 +79,7 @@ describe('Horizontal view TimeTable', () => {
     expect(tree.find(`.${classes.text}`).props().children)
       .toBeTruthy();
   });
-  it('should call date format function', () => {
+  it('should call date format function with month and day parameter', () => {
     const formatDate = jest.fn();
     formatDate.mockImplementation(() => 'time');
     const startDate = new Date(2018, 6, 1, 16);

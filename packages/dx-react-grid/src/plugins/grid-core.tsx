dcd1c01de2484cb4ef0df8ab0ac169d4984c7c02
Nikita Ushkal <nikita.ushkal@devexpress.com>
2019-09-12 13:39:06 +0300

    wip

--- packages/dx-react-grid/src/plugins/grid-core.tsx
+++ packages/dx-react-grid/src/plugins/grid-core.tsx
@@ -1,11 +1,30 @@
 import * as React from 'react';
 import {
-  Plugin, Getter, Template, TemplatePlaceholder,
+  Plugin, Getter, Template, TemplatePlaceholder, Action,
 } from '@devexpress/dx-react-core';
 import { rowIdGetter, cellValueGetter } from '@devexpress/dx-grid-core';
 import { GridProps } from '../types';
 
-export class GridCore extends React.PureComponent<GridProps> {
+export class GridCore extends React.PureComponent<GridProps, any> {
+  endExportAction: () => void;
+  startExportAction: (_: any, __: any, { startExport }: any) => void;
+  constructor(props) {
+    super(props);
+    this.state = {
+      isExporting: false,
+    };
+
+    this.startExportAction = () => {
+      console.log('grid core start export')
+      this.setState({ isExporting: true });
+      this.forceUpdate();
+      // startExport();
+    };
+    this.endExportAction = () => {
+      console.log('grid core finish export')
+      this.setState({ isExporting: false });
+    }
+  }
   render() {
     const {
       rows,
@@ -14,6 +33,8 @@ export class GridCore extends React.PureComponent<GridProps> {
       getCellValue,
       rootComponent: Root,
     } = this.props;
+    const { isExporting } = this.state;
+    console.log('isExporting', isExporting)
 
     return (
       <Plugin>
@@ -23,6 +44,11 @@ export class GridCore extends React.PureComponent<GridProps> {
         <Getter name="getRowId" value={rowIdGetter(getRowId!, rows)} />
         <Getter name="columns" value={columns} />
         <Getter name="getCellValue" value={cellValueGetter(getCellValue!, columns)} />
+        <Getter name="isExporting" value={isExporting} />
+        {/* <Action name="startExport" action={() => {}} /> */}
+        <Action name="initiateExport" action={this.startExportAction} />
+        <Action name="finishExport" action={this.endExportAction} />
+
         <Template name="root">
           <Root>
             <TemplatePlaceholder name="header" />

cd73218f0172dde5fbd431b21d0d19b5993ab308
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2019-09-13 14:33:24 +0300

    feat(react-grid): improve column resizing (#2218)

--- packages/dx-react-grid/src/plugins/table-column-resizing.tsx
+++ packages/dx-react-grid/src/plugins/table-column-resizing.tsx
@@ -11,8 +11,9 @@ import {
   cancelTableColumnWidthDraft,
   ColumnWidthPayload,
   TableColumnWidthInfo,
+  TABLE_DATA_TYPE,
 } from '@devexpress/dx-grid-core';
-import { TableColumnResizingProps, TableColumnResizingState } from '../types';
+import { TableColumnResizingProps, TableColumnResizingState, CellWidthGetter } from '../types';
 
 const pluginDependencies = [
   { name: 'Table' },
@@ -22,13 +23,20 @@ const pluginDependencies = [
 class TableColumnResizingBase extends React.PureComponent<TableColumnResizingProps, TableColumnResizingState> {
   static defaultProps = {
     defaultColumnWidths: [],
+    resizingMode: 'widget',
   };
   changeTableColumnWidth: ActionFn<ColumnWidthPayload>;
   draftTableColumnWidth: ActionFn<ColumnWidthPayload>;
   cancelTableColumnWidthDraft: ActionFn<any>;
+  storeWidthGetters: ActionFn<object>;
+  storeCache: (payload) => void;
+  clearCache: () => void;
   tableColumnsComputed: MemoizedComputed<TableColumnWidthInfo[], typeof tableColumnsWithWidths>;
   // tslint:disable-next-line: max-line-length
   tableColumnsDraftComputed: MemoizedComputed<TableColumnWidthInfo[], typeof tableColumnsWithDraftWidths>;
+
+  widthGetters: { [colName: string]: CellWidthGetter } = {};
+  cachedWidths: { [colName: string]: number } = {};
 
   constructor(props) {
     super(props);
@@ -51,35 +59,69 @@ class TableColumnResizingBase extends React.PureComponent<TableColumnResizingPro
     this.tableColumnsComputed = memoize(
       columnWidths => (
         { tableColumns }: Getters,
-      ) => tableColumnsWithWidths(tableColumns, columnWidths),
+      ) => tableColumnsWithWidths(tableColumns, columnWidths, this.props.resizingMode!),
     );
     this.tableColumnsDraftComputed = memoize(
       draftColumnWidths => (
         { tableColumns }: Getters,
-      ) => tableColumnsWithDraftWidths(tableColumns, draftColumnWidths),
+      ) => tableColumnsWithDraftWidths(
+        tableColumns, draftColumnWidths, this.props.resizingMode!,
+      ),
     );
 
     this.changeTableColumnWidth = stateHelper.applyReducer.bind(
       stateHelper, (prevState, payload) => {
-        const { minColumnWidth, maxColumnWidth, columnExtensions } = this.props;
+        const cachedWidths = { ...this.cachedWidths };
+        const { minColumnWidth, maxColumnWidth, columnExtensions, resizingMode } = this.props;
         return changeTableColumnWidth(
           prevState,
-          { ...payload, minColumnWidth, maxColumnWidth, columnExtensions },
+          { ...payload, cachedWidths, resizingMode,
+            minColumnWidth, maxColumnWidth, columnExtensions },
         );
       },
     );
     this.draftTableColumnWidth = stateHelper.applyReducer.bind(
       stateHelper, (prevState, payload) => {
-        const { minColumnWidth, maxColumnWidth, columnExtensions } = this.props;
+        this.storeCache(payload);
+        const cachedWidths = this.cachedWidths;
+        const { minColumnWidth, maxColumnWidth, columnExtensions, resizingMode } = this.props;
         return draftTableColumnWidth(
           prevState,
-          { ...payload, minColumnWidth, maxColumnWidth, columnExtensions },
+          { ...payload, cachedWidths, resizingMode,
+            minColumnWidth, maxColumnWidth, columnExtensions },
         );
       },
     );
     this.cancelTableColumnWidthDraft = stateHelper.applyReducer.bind(
       stateHelper, cancelTableColumnWidthDraft,
     );
+
+    this.storeCache = ({ columnName, nextColumnName }) => {
+      if (Object.keys(this.cachedWidths).length === 0) {
+        this.cachedWidths[columnName] = this.widthGetters[columnName]();
+        if (nextColumnName) {
+          this.cachedWidths[nextColumnName] = this.widthGetters[nextColumnName]();
+        }
+      }
+    };
+    this.clearCache = () => {
+      Object.keys(this.cachedWidths)
+      .forEach(columnName => delete this.cachedWidths[columnName]);
+    };
+
+    this.storeWidthGetters = memoize(({ tableColumn, getter, tableColumns }) => {
+      if (tableColumn.type === TABLE_DATA_TYPE) {
+        this.widthGetters[tableColumn.column!.name] = getter;
+      }
+      Object.keys(this.widthGetters)
+      .forEach((columnName) => {
+        const columnIndex = tableColumns
+          .findIndex(({ type, column }) => type === TABLE_DATA_TYPE && column!.name === columnName);
+        if (columnIndex === -1) {
+          delete this.widthGetters[columnName];
+        }
+      });
+    });
   }
 
   static getDerivedStateFromProps(nextProps, prevState) {
@@ -92,9 +134,17 @@ class TableColumnResizingBase extends React.PureComponent<TableColumnResizingPro
     };
   }
 
+  componentDidUpdate (_, prevState) {
+    const { columnWidths: currentWidths } = this.state;
+    const { columnWidths: prevWidths } = prevState;
+    if (currentWidths !== prevWidths) {
+      this.clearCache();
+    }
+  }
+
   render() {
     const { columnWidths, draftColumnWidths } = this.state;
-
+    const { resizingMode } = this.props;
     const tableColumnsComputed = this.tableColumnsComputed(columnWidths);
     const tableColumnsDraftComputed = this.tableColumnsDraftComputed(draftColumnWidths);
 
@@ -106,9 +156,11 @@ class TableColumnResizingBase extends React.PureComponent<TableColumnResizingPro
         <Getter name="tableColumnResizingEnabled" value />
         <Getter name="tableColumns" computed={tableColumnsComputed} />
         <Getter name="tableColumns" computed={tableColumnsDraftComputed} />
+        <Getter name="columnResizingMode" value={resizingMode} />
         <Action name="changeTableColumnWidth" action={this.changeTableColumnWidth} />
         <Action name="draftTableColumnWidth" action={this.draftTableColumnWidth} />
         <Action name="cancelTableColumnWidthDraft" action={this.cancelTableColumnWidthDraft} />
+        <Action name="storeWidthGetters" action={this.storeWidthGetters} />
       </Plugin>
     );
   }

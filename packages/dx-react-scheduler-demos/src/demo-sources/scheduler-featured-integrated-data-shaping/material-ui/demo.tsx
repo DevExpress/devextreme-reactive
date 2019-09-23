e38b22d0a4074edb2ec794811a04067d2fdf1a67
AryamnovEugeniy <earyamnow@mail.ru>
2019-09-23 12:20:41 +0300

    Add types

--- packages/dx-react-scheduler-demos/src/demo-sources/scheduler-featured-integrated-data-shaping/material-ui/demo.tsx
+++ packages/dx-react-scheduler-demos/src/demo-sources/scheduler-featured-integrated-data-shaping/material-ui/demo.tsx
@@ -1,29 +1,57 @@
 import * as React from 'react';
 import Paper from '@material-ui/core/Paper';
-import orange from '@material-ui/core/colors/orange';
 import brown from '@material-ui/core/colors/brown';
 import { ViewState } from '@devexpress/dx-react-scheduler';
 import {
   Scheduler, DayView, Appointments, MonthView, Toolbar, DateNavigator, ViewSwitcher, TodayButton,
 } from '@devexpress/dx-react-scheduler-material-ui';
 import { withStyles } from '@material-ui/core';
+import classNames from 'classnames';
+import { fade } from '@material-ui/core/styles/colorManipulator';
 import { appointments } from '../../../demo-data/month-appointments-with-types';
+import { WithStyles } from '@material-ui/styles';
+import { amber } from '@material-ui/core/colors';
 
 const styles = ({ palette }) => ({
   toolbarRoot: {
-    backgroundColor: palette.primary[50],
+    backgroundColor: fade(palette.primary[400], 0.04),
   },
   personalAppointment: {
-    backgroundColor: orange[200],
+    backgroundColor: amber[200],
+    '&:hover': {
+      backgroundColor: amber[300],
+    },
   },
   nonPersonalAppoinment: {
     backgroundColor: brown[700],
+    '&:hover': {
+      backgroundColor: brown[800],
+    },
+  },
+  weekEndCell: {
+    backgroundColor: fade(palette.action.disabledBackground, 0.04),
+    '&:hover': {
+      backgroundColor: fade(palette.action.disabledBackground, 0.04),
+    },
+    '&:focus': {
+      backgroundColor: fade(palette.action.disabledBackground, 0.04),
+    },
+  },
+  weekEndDayScaleCell: {
+    backgroundColor: fade(palette.action.disabledBackground, 0.06),
   },
 });
 
-const AppointmentBase: React.ComponentType<Appointments.AppointmentProps> = ({
+type AppointmentProps = Appointments.AppointmentProps & WithStyles<typeof styles>;
+type ToolbarRootProps = Toolbar.RootProps & WithStyles<typeof styles>;
+type TimeTableCellProps = MonthView.TimeTableCellProps & WithStyles<typeof styles>;
+type DayScaleCellProps = MonthView.DayScaleCellProps & WithStyles<typeof styles>;
+
+const isWeekEnd = (date: Date): boolean => date.getDay() === 0 || date.getDay() === 6;
+
+const Appointment = withStyles(styles)(({
   classes, data, ...restProps
-}) => {
+}: AppointmentProps) => {
   if (data.type === 'personal') {
     return (
       <Appointments.Appointment
@@ -35,37 +63,47 @@ const AppointmentBase: React.ComponentType<Appointments.AppointmentProps> = ({
   }
   return (
     <Appointments.Appointment
-      {...restProps}
       className={classes.nonPersonalAppoinment}
       data={data}
+      {...restProps}
     />
   );
-};
+});
 
-const Appointment = withStyles(styles, { name: 'Appointment' })(AppointmentBase);
-// const TimeTableLayout: React.ComponentType<MonthView.TimeTableLayoutProps> = (props) => {
-//   const correctedCellsData = props.cellsData.map(dataItem =>  dataItem.slice(1, -1));
-//   return <MonthView.TimeTableLayout {...props} cellsData={correctedCellsData} />;
-// };
-// const DayScaleCell: React.ComponentType<MonthView.DayScaleCellProps> = (props) => {
-//   if (props.startDate!.getDay() === 0 || props.startDate!.getDay() === 6) return null;
-//   return <MonthView.DayScaleCell {...props} />;
-// };
-// const TimeTableCell: React.ComponentType<MonthView.TimeTableCellProps> = (props) => {
-//   if (props.startDate!.getDay() === 0 || props.startDate!.getDay() === 6) return null;
-//   return <MonthView.TimeTableCell {...props} />;
-// };
-const ToolbarRootBase: React.ComponentType<Toolbar.RootProps> = ({
-  classes, children, ...restProps
-}) => {
+const DayScaleCell = withStyles(styles)(({ startDate, classes, ...restProps }: DayScaleCellProps) => {
+  return (
+    <MonthView.DayScaleCell
+      className={classNames({
+        [classes.weekEndDayScaleCell]: isWeekEnd(startDate),
+      })}
+      startDate={startDate}
+      {...restProps}
+    />
+  );
+});
+
+const TimeTableCell = withStyles(styles)((
+  { startDate, classes, ...restProps }: TimeTableCellProps
+) => {
+  return (
+    <MonthView.TimeTableCell
+      className={classNames({
+        [classes.weekEndCell]: isWeekEnd(startDate!),
+      })}
+      startDate={startDate}
+      {...restProps}
+    />
+  );
+});
+
+const ToolbarRoot = withStyles(styles)((
+  { classes, children, ...restProps }: ToolbarRootProps) => {
   return (
     <Toolbar.Root {...restProps} className={classes.toolbarRoot}>
       {children}
     </Toolbar.Root>
   );
-};
-
-const ToolbarRoot = withStyles(styles, { name: 'ToolbarRoot' })(ToolbarRootBase);
+});
 
 const Demo: React.SFC = () => {
   return (
@@ -78,6 +116,8 @@ const Demo: React.SFC = () => {
         />
         <MonthView
           name="Work Weeks"
+          dayScaleCellComponent={DayScaleCell}
+          timeTableCellComponent={TimeTableCell}
         />
         <MonthView />
         <DayView

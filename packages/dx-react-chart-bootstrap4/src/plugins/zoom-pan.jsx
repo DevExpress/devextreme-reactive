7ee9e4b7df891195ba455f665d3a4f5f39198905
Nikita Ushkal <nikita.ushkal@devexpress.com>
2019-09-27 14:29:39 +0300

    fix merge

--- packages/dx-react-chart-bootstrap4/src/plugins/zoom-pan.jsx
+++
@@ -1,5 +0,0 @@
-import { ZoomAndPan as ZoomAndPanBase } from '@devexpress/dx-react-chart';
-import { withComponents } from '@devexpress/dx-react-core';
-import { DragBox } from '../templates/zoom-pan/drag-box';
-
-export const ZoomAndPan = withComponents({ DragBox })(ZoomAndPanBase);
--- packages/dx-react-chart-bootstrap4/src/templates/zoom-pan/drag-box.jsx
+++
@@ -1,6 +0,0 @@
-import { ZoomAndPan } from '@devexpress/dx-react-chart';
-import { withClassName } from '../utils';
-
-export const DragBox = withClassName(
-  'bg-secondary', 'dx-c-bs4-rect-opacity',
-)(ZoomAndPan.DragBox);

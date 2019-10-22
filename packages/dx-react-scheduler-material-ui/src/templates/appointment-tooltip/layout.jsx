41c81a86e2d69cd95cbfc22bfa89d684e3edbc7a
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2019-10-22 14:59:14 +0300

    Fix tests

--- packages/dx-react-scheduler-material-ui/src/templates/appointment-tooltip/layout.jsx
+++ packages/dx-react-scheduler-material-ui/src/templates/appointment-tooltip/layout.jsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
-import moment from 'moment';
 import Popover from '@material-ui/core/Popover';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -29,10 +28,6 @@ const LayoutBase = ({
   ...restProps
 }) => {
   const { target, data = {} } = appointmentMeta;
-
-  const timeText = moment(data.startDate).isSame(data.endDate)
-    ? formatDate(data.startDate, HOUR_MINUTE_OPTIONS)
-    : `${formatDate(data.startDate, HOUR_MINUTE_OPTIONS)} - ${formatDate(data.endDate, HOUR_MINUTE_OPTIONS)}`;
 
   return (
     <Popover

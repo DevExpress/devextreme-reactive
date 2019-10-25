026c05f702c19e5e96f163351f8bf652b74726dc
Nikita Ushkal <nikita.ushkal@devexpress.com>
2019-10-25 14:53:37 +0300

    redesign logo

--- packages/dx-site/src/components/logos/product.jsx
+++ packages/dx-site/src/components/logos/product.jsx
@@ -6,7 +6,10 @@ import styles from './product.module.scss';
 const Product = () => (
   <Link to="/" className={styles.product}>
     <span className={styles.title}>
-      DevExtreme Reactive
+      DevExtreme
+    </span>
+    <span className={styles.name}>
+      Reactive
     </span>
   </Link>
 );

04f3a6865e539c92212b53fcbad0205780e036bb
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2019-10-28 14:03:31 +0300

    Fix Open button

--- packages/dx-react-scheduler-material-ui/src/templates/date-navigator/open-button.jsx
+++ packages/dx-react-scheduler-material-ui/src/templates/date-navigator/open-button.jsx
@@ -1,25 +1,60 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import CalendarToday from '@material-ui/icons/CalendarToday';
+import IconButton from '@material-ui/core/IconButton';
+import { withStyles } from '@material-ui/core/styles';
+import classNames from 'clsx';
 
-export const OpenButton = React.memo(({
-  text, onVisibilityToggle, ...restProps
+const styles = ({ spacing }) => ({
+  textButton: {
+    '@media (max-width: 700px)': {
+      display: 'none',
+    },
+  },
+  iconButton: {
+    '@media (min-width: 700px)': {
+      display: 'none',
+    },
+    '@media (max-width: 500px)': {
+      width: spacing(4),
+      height: spacing(4),
+      padding: spacing(0),
+    },
+  },
+});
+
+const OpenButtonBase = React.memo(({
+  text, onVisibilityToggle, classes, className, ...restProps
 }) => (
-  <Button
-    onClick={onVisibilityToggle}
-    {...restProps}
-  >
-    {text}
-  </Button>
+  <>
+    <Button
+      onClick={onVisibilityToggle}
+      className={classNames(classes.textButton, className)}
+      {...restProps}
+    >
+      {text}
+    </Button>
+    <IconButton
+      onClick={onVisibilityToggle}
+      className={classNames(classes.iconButton, className)}
+      {...restProps}
+    >
+      <CalendarToday />
+    </IconButton>
+  </>
 ));
 
-OpenButton.propTypes = {
+OpenButtonBase.propTypes = {
   onVisibilityToggle: PropTypes.func.isRequired,
   text: PropTypes.string,
   className: PropTypes.string,
+  classes: PropTypes.object.isRequired,
 };
 
-OpenButton.defaultProps = {
+OpenButtonBase.defaultProps = {
   text: '',
   className: undefined,
 };
+
+export const OpenButton = withStyles(styles, { name: 'OpenButton' })(OpenButtonBase);

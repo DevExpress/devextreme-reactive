bca8f524b42c6e5ec5db60cc9c2df88dafc1c504
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2019-11-03 16:45:20 +0300

    Refactor types

--- packages/dx-scheduler-core/src/types/resources.types.ts
+++ packages/dx-scheduler-core/src/types/resources.types.ts
@@ -1,27 +1,16 @@
 import { PureComputed } from '@devexpress/dx-core';
 import { Appointment } from './../index';
 
-export type PlainResourceItem = {
-  /**
-   * Specifies a resource item's identifier.
-   * This id should be related with scheduler's data `fieldName` id.
-   */
-  id: number | string;
-  /**
-   * Specifies resource item's color.
-   * It is used to indicate appointments related to this resource.
-   */
-  color: string | Color;
-  /** Specifies the resource item's text that would be used in UI */
-  text: string;
+/** Specifies a resource that available in scheduler. */
+export type Resource = {
   /** The name of the appointment object field that specifies a resource of this kind. */
   fieldName: string;
   /** Specifies the resource title. */
-  title: string;
+  title?: string;
   /** Indicates whether or not several resources of this kind can be assigned to an appointment. */
-  allowMultiple: boolean;
-  /** Specifies whether resource is main */
-  isMain: boolean;
+  allowMultiple?: boolean;
+  /** Specifies an array of resource item objects. */
+  items: Array<ResourceItem>;
 };
 
 /** Specifies the resource item object. */
@@ -39,6 +28,26 @@ export type ResourceItem = {
   /** Specifies the resource item's text that would be used in UI */
   text?: string;
 };
+/** Specifies the resources palette. */
+export type Palette = Array<string | Color>;
+
+/** Specifies the color object. */
+export interface Color {
+  50: string;
+  100: string;
+  200: string;
+  300: string;
+  400: string;
+  500: string;
+  600: string;
+  700: string;
+  800: string;
+  900: string;
+  A100: string;
+  A200: string;
+  A400: string;
+  A700: string;
+}
 
 /** @internal */
 export type ValidResourceItem = {
@@ -64,18 +73,6 @@ export type ValidResourceItem = {
   isMain: boolean;
 };
 
-/** Specifies a resource that available in scheduler. */
-export type Resource = {
-  /** The name of the appointment object field that specifies a resource of this kind. */
-  fieldName: string;
-  /** Specifies the resource title. */
-  title?: string;
-  /** Indicates whether or not several resources of this kind can be assigned to an appointment. */
-  allowMultiple?: boolean;
-  /** Specifies an array of resource item objects. */
-  items: Array<ResourceItem>;
-};
-
 /** @internal */
 export type ValidResource = {
   /** The name of the appointment object field that specifies a resource of this kind. */
@@ -89,35 +86,6 @@ export type ValidResource = {
   /** Specifies the main resource kind */
   isMain: boolean;
 };
-
-export interface Color {
-  50: string;
-  100: string;
-  200: string;
-  300: string;
-  400: string;
-  500: string;
-  600: string;
-  700: string;
-  800: string;
-  900: string;
-  A100: string;
-  A200: string;
-  A400: string;
-  A700: string;
-}
-
-export type Palette = Array<string | Color>;
-
-export interface AppointmentWithResources extends Appointment {
-  resources: PlainResourceItem;
-}
-
-/** @internal */
-export type AttachResourcesComputed = PureComputed<
-  [Array<Appointment>, Array<Resource>, string | undefined, Palette],
-  Array<AppointmentWithResources>
->;
 
 /** @internal */
 export type GetAppointmentResources = PureComputed<

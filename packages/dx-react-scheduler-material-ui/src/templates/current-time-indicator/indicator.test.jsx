7dae8782bc88b03ee38abac33e3b5e986e36a8ac
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2019-11-13 17:06:48 +0300

    Test mui indicator

---
+++ packages/dx-react-scheduler-material-ui/src/templates/current-time-indicator/indicator.test.jsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { createShallow } from '@material-ui/core/test-utils';
+import { Indicator } from './indicator';
+
+jest.mock('@material-ui/core/styles', () => ({
+  ...require.requireActual('@material-ui/core/styles'),
+  makeStyles: jest.fn(() => () => ({
+    line: 'line',
+    circle: 'circle',
+    nowIndicator: 'nowIndicator',
+  })),
+}));
+
+describe('CurrentTimeIndicator', () => {
+  let shallow;
+  beforeAll(() => {
+    shallow = createShallow();
+  });
+
+  describe('Indicator', () => {
+    it('should pass rest props to the root element', () => {
+      const tree = shallow((
+        <Indicator data={{ testData: 'testData' }} />
+      ));
+
+      expect(tree.props().data)
+        .toMatchObject({ testData: 'testData' });
+    });
+
+    it('should render its elements', () => {
+      const tree = shallow((
+        <Indicator />
+      ));
+
+      expect(tree.find('.line'))
+        .toHaveLength(1);
+      expect(tree.find('.circle'))
+        .toHaveLength(1);
+      expect(tree.find('.nowIndicator'))
+        .toHaveLength(2);
+    });
+  });
+});

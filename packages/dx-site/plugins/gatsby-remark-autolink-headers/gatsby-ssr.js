fdf93fc7cc7dffc6bd177deb8741622ca596b91d
Nikita Ushkal <nikita.ushkal@devexpress.com>
2019-12-02 17:04:40 +0300

    tweak autolink-headers plugin

---
+++ packages/dx-site/plugins/gatsby-remark-autolink-headers/gatsby-ssr.js
@@ -0,0 +1,79 @@
+/* eslint-disable react/jsx-filename-extension */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const React = require('react');
+
+const pluginDefaults = {
+  className: 'anchor',
+  icon: true,
+  offsetY: 0,
+};
+
+exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+  const { className, icon, offsetY } = Object.assign(
+    pluginDefaults,
+    pluginOptions,
+  );
+
+  const styles = `
+    .${className} {
+      float: left;
+      padding-right: 4px;
+      margin-left: -20px;
+    }
+    h1 .${className} svg,
+    h2 .${className} svg,
+    h3 .${className} svg,
+    h4 .${className} svg,
+    h5 .${className} svg,
+    h6 .${className} svg {
+      visibility: hidden;
+    }
+    h1:hover .${className} svg,
+    h2:hover .${className} svg,
+    h3:hover .${className} svg,
+    h4:hover .${className} svg,
+    h5:hover .${className} svg,
+    h6:hover .${className} svg,
+    h1 .${className}:focus svg,
+    h2 .${className}:focus svg,
+    h3 .${className}:focus svg,
+    h4 .${className}:focus svg,
+    h5 .${className}:focus svg,
+    h6 .${className}:focus svg {
+      visibility: visible;
+    }
+  `;
+
+  const script = `
+    document.addEventListener("load", function(event) {
+      var hash = window.decodeURI(location.hash.replace('#', ''))
+      if (hash !== '') {
+        var element = document.getElementById(hash)
+        if (element) {
+          var offset = element.offsetTop
+          // Wait for the browser to finish rendering before scrolling.
+          setTimeout((function() {
+            window.scrollTo(0, offset + ${offsetY})
+          }), 0)
+        }
+      }
+    })
+  `;
+
+  const style = icon ? (
+    <style key="gatsby-remark-autolink-headers-style" type="text/css">
+      {styles}
+    </style>
+  ) : (
+    undefined
+  );
+
+  return setHeadComponents([
+    style,
+    <script
+      key="gatsby-remark-autolink-headers-script"
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: script }}
+    />,
+  ]);
+};

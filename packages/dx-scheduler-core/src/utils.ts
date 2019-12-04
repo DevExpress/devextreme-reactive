183d1d2e52051a05197f435bea6fc60d8dc23d8a
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2019-12-04 18:00:29 +0300

    Refactor

--- packages/dx-scheduler-core/src/utils.ts
+++ packages/dx-scheduler-core/src/utils.ts
@@ -80,26 +80,36 @@ export const viewPredicate: ViewPredicateFn = (
   return isAppointmentInBoundary && !isAppointmentInExcludedDays && considerAllDayAppointment;
 };
 
+const compareByDay: PureComputed<
+  [AppointmentMoment, AppointmentMoment], number
+> = (first, second) => {
+  if (first.start.isBefore(second.start, 'day')) return -1;
+  if (first.start.isAfter(second.start, 'day')) return 1;
+  return 0;
+};
+
+const compareByAllDay: PureComputed<
+  [AppointmentMoment, AppointmentMoment], number
+> = (first, second) => {
+  if (first.allDay && !second.allDay) return -1;
+  if (!first.allDay && second.allDay) return 1;
+  return 0;
+};
+
+const compareByTime: PureComputed<
+  [AppointmentMoment, AppointmentMoment], number
+> = (first, second) => {
+  if (first.start.isBefore(second.start)) return -1;
+  if (first.start.isAfter(second.start)) return 1;
+  if (first.end.isBefore(second.end)) return 1;
+  if (first.end.isAfter(second.end)) return -1;
+  return 0;
+};
+
 export const sortAppointments: PureComputed<
   [AppointmentMoment[]], AppointmentMoment[]
 > = appointments => appointments
-  .slice().sort((a, b) => {
-    if (a.start.isSame(b.start, 'day')) {
-      if (a.allDay && !b.allDay) return -1;
-      if (!a.allDay && b.allDay) return 1;
-      if (a.allDay && b.allDay) {
-        if (a.end.isBefore(b.end)) return 1;
-        if (a.end.isAfter(b.end)) return -1;
-      }
-    }
-    if (a.start.isBefore(b.start)) return -1;
-    if (a.start.isAfter(b.start)) return 1;
-    if (a.start.isSame(b.start)) {
-      if (a.end.isBefore(b.end)) return 1;
-      if (a.end.isAfter(b.end)) return -1;
-    }
-    return 0;
-  });
+  .slice().sort((a, b) => compareByDay(a, b) || compareByAllDay(a, b) || compareByTime(a, b));
 
 export const findOverlappedAppointments: CustomFunction<
   [AppointmentMoment[], boolean], any[]

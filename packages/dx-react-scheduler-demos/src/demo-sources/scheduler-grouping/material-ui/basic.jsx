2b452b8473703fa272e9b247d0910e4702070b38
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2019-12-12 13:04:59 +0300

    Refactor

--- packages/dx-react-scheduler-demos/src/demo-sources/scheduler-grouping/material-ui/basic.jsx
+++ packages/dx-react-scheduler-demos/src/demo-sources/scheduler-grouping/material-ui/basic.jsx
@@ -11,64 +11,61 @@ import {
   GroupingPanel,
   DayView,
 } from '@devexpress/dx-react-scheduler-material-ui';
-import { blue, teal, indigo, deepPurple, green, purple } from '@material-ui/core/colors';
+import {
+  blue, teal, indigo, deepPurple, green, purple,
+} from '@material-ui/core/colors';
 
-const appointments = [
-  {
-    id: 0,
-    title: 'Watercolor Landscape',
-    members: [1, 2, 3],
-    startDate: new Date(2017, 4, 28, 9, 30),
-    endDate: new Date(2017, 4, 28, 12, 30),
-  }, {
-    id: 1,
-    title: 'Oil Painting for Beginners',
-    members: [4, 5, 6],
-    startDate: new Date(2017, 4, 28, 9, 30),
-    endDate: new Date(2017, 4, 28, 12, 30),
-  },
-  {
-    id: 2,
-    title: 'Testing',
-    members: [1, 2, 3],
-    startDate: new Date(2017, 4, 29, 9, 30),
-    endDate: new Date(2017, 4, 29, 12, 30),
-  }, {
-    id: 3,
-    title: 'Final Exams',
-    members: [4, 5, 6],
-    startDate: new Date(2017, 4, 29, 9, 30),
-    endDate: new Date(2017, 4, 29, 12, 30),
-  },
-];
+const appointments = [{
+  id: 0,
+  title: 'Watercolor Landscape',
+  members: [1, 2, 3],
+  startDate: new Date(2017, 4, 28, 9, 30),
+  endDate: new Date(2017, 4, 28, 12, 30),
+}, {
+  id: 1,
+  title: 'Oil Painting for Beginners',
+  members: [4, 5, 6],
+  startDate: new Date(2017, 4, 28, 9, 30),
+  endDate: new Date(2017, 4, 28, 12, 30),
+}, {
+  id: 2,
+  title: 'Testing',
+  members: [1, 2, 3],
+  startDate: new Date(2017, 4, 29, 9, 30),
+  endDate: new Date(2017, 4, 29, 12, 30),
+}, {
+  id: 3,
+  title: 'Final Exams',
+  members: [4, 5, 6],
+  startDate: new Date(2017, 4, 29, 9, 30),
+  endDate: new Date(2017, 4, 29, 12, 30),
+}];
 
-export const owners = [
-  {
-    text: 'Andrew Glover',
-    id: 1,
-    color: blue,
-  }, {
-    text: 'Arnie Schwartz',
-    id: 2,
-    color: teal,
-  }, {
-    text: 'John Heart',
-    id: 3,
-    color: indigo,
-  }, {
-    text: 'Taylor Riley',
-    id: 4,
-    color: deepPurple,
-  }, {
-    text: 'Brad Farkus',
-    id: 5,
-    color: green,
-  }, {
-    text: 'Arthur Miller',
-    id: 6,
-    color: purple,
-  },
-];
+export const owners = [{
+  text: 'Andrew Glover',
+  id: 1,
+  color: blue,
+}, {
+  text: 'Arnie Schwartz',
+  id: 2,
+  color: teal,
+}, {
+  text: 'John Heart',
+  id: 3,
+  color: indigo,
+}, {
+  text: 'Taylor Riley',
+  id: 4,
+  color: deepPurple,
+}, {
+  text: 'Brad Farkus',
+  id: 5,
+  color: green,
+}, {
+  text: 'Arthur Miller',
+  id: 6,
+  color: purple,
+}];
 
 export default class Demo extends React.PureComponent {
   constructor(props) {

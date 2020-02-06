c2868f4afec4b49cacb35bd16abf5498e7978cfc
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2020-02-06 16:36:22 +0300

    Fix import

--- packages/dx-react-scheduler/src/types/views/base-view.types.ts
+++ packages/dx-react-scheduler/src/types/views/base-view.types.ts
@@ -1,7 +1,9 @@
-import { FormatterFn, CellElementsMeta, ScrollingStrategy, Group } from '../index';
+import {
+  FormatterFn, CellElementsMeta, ScrollingStrategy,
+  Group, GroupOrientation,
+} from '../index';
 import { CurrentTimeIndicator } from '../current-time-indicator';
 import { GroupingPanel } from '../grouping';
-import { GroupOrientation } from '@devexpress/dx-scheduler-core';
 
 // tslint:disable: no-namespace
 export interface CommonViewProps {

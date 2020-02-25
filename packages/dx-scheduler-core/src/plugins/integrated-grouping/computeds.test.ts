dd8d8059462eea4a6d6eed89dcccc5849adec649
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2020-02-25 16:31:16 +0300

    Test updateAllDayCellElementsMeta

--- packages/dx-scheduler-core/src/plugins/integrated-grouping/computeds.test.ts
+++ packages/dx-scheduler-core/src/plugins/integrated-grouping/computeds.test.ts
@@ -2,6 +2,7 @@ import {
   filterResourcesByGrouping, sortFilteredResources,
   getGroupsFromResources, expandViewCellsDataWithGroups,
   updateGroupingWithMainResource, expandGroups, updateTimeTableCellElementsMeta,
+  updateAllDayCellElementsMeta,
 } from './computeds';
 import { expandGroupedAppointment, groupAppointments } from './helpers';
 import { sliceAppointmentsByDays } from '../all-day-panel/helpers';
@@ -395,7 +396,7 @@ describe('IntegratedGrouping computeds', () => {
           'Twelfth cell',
         ],
       };
-      const groups = [[{}, {}]]
+      const groups = [[{}, {}]];
 
       expect(updateTimeTableCellElementsMeta(
         timeTableCellElementsMeta, () => VERTICAL_GROUP_ORIENTATION, groups, true,
@@ -414,6 +415,83 @@ describe('IntegratedGrouping computeds', () => {
             'Tenth cell',
             'Eleventh cell',
             'Twelfth cell',
+          ],
+        });
+    });
+  });
+
+  describe('#updateAllDayCellElementsMeta', () => {
+    it('should not update if timeTableCellElementsMeta\'s getCellRects is undefined', () => {
+      const timeTableCellElementsMeta = {};
+      const allDayElementsMeta = { test: 'test' };
+      expect(updateAllDayCellElementsMeta(
+        allDayElementsMeta, timeTableCellElementsMeta, () => VERTICAL_GROUP_ORIENTATION,
+        'groups', true, 'viewCellsData', {},
+      ))
+        .toEqual(allDayElementsMeta);
+    });
+    it('should leave cell elements as they are if groupOrientation is horizontal or all day panel does not exist', () => {
+      const timeTableCellElementsMeta = { getCellRects: 'test' };
+      const allDayElementsMeta = { test: 'test' };
+
+      let allDayPanelExists = false;
+      expect(updateAllDayCellElementsMeta(
+        allDayElementsMeta, timeTableCellElementsMeta, () => VERTICAL_GROUP_ORIENTATION,
+        'groups', allDayPanelExists, 'viewCellsData', {},
+      ))
+        .toEqual(allDayElementsMeta);
+
+      allDayPanelExists = true;
+      expect(updateAllDayCellElementsMeta(
+        allDayElementsMeta, timeTableCellElementsMeta, () => HORIZONTAL_GROUP_ORIENTATION,
+        'groups', allDayPanelExists, 'viewCellsData', {},
+      ))
+        .toEqual(allDayElementsMeta);
+    });
+    it('should delete elements belonging to timetable cells', () => {
+      const viewCellsData = [
+        [{ groupingInfo: 'First group' }, { groupingInfo: 'First group' }],
+        [{ groupingInfo: 'First group' }, { groupingInfo: 'First group' }],
+        [{ groupingInfo: 'Second group' }, { groupingInfo: 'Second group' }],
+        [{ groupingInfo: 'Second group' }, { groupingInfo: 'Second group' }],
+      ];
+      const timeTableCellElementsMeta = {
+        parentRect: 'Parent rect',
+        getCellRects: [
+          // All-day panel
+          'First cell',
+          'Second cell',
+          // TimeTable
+          'Third cell',
+          'Fourth cell',
+          'Fifth cell',
+          'Sixth cell',
+          // All-day panel
+          'Seventh cell',
+          'Eighth cell',
+          // TimeTable
+          'Ninth cell',
+          'Tenth cell',
+          'Eleventh cell',
+          'Twelfth cell',
+        ],
+      };
+      const allDayElementsMeta = {};
+      const groups = [[{}, {}]];
+
+      expect(updateAllDayCellElementsMeta(
+        allDayElementsMeta, timeTableCellElementsMeta, () => VERTICAL_GROUP_ORIENTATION,
+        groups, true, viewCellsData, {},
+      ))
+        .toEqual({
+          parentRect: 'Parent rect',
+          getCellRects: [
+            // All-day panel
+            'First cell',
+            'Second cell',
+            // All-day panel
+            'Seventh cell',
+            'Eighth cell',
           ],
         });
     });

453274d9a182267c89190bf7aca6bf4835aac5b8
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2020-02-28 17:45:21 +0300

    Rework to hooks

--- packages/dx-react-scheduler-demos/src/demo-sources/scheduler-editing/material-ui/editing-features.jsx
+++ packages/dx-react-scheduler-demos/src/demo-sources/scheduler-editing/material-ui/editing-features.jsx
@@ -16,142 +16,139 @@ import {
 
 import { appointments } from '../../../demo-data/appointments';
 
-export default class Demo extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: appointments,
-      currentDate: '2018-06-27',
-      allowAdding: true,
-      allowDeleting: true,
-      allowUpdating: true,
-      allowResizing: true,
-      allowDragging: true,
-    };
+const currentDate = '2018-06-27';
+const editingFeaturesList = [
+  { id: 'allowAdding', text: 'Allow Adding' },
+  { id: 'allowDeleting', text: 'Allow Deleting' },
+  { id: 'allowUpdating', text: 'Allow Updating' },
+  { id: 'allowResizing', text: 'Allow Resizing' },
+  { id: 'allowDragging', text: 'Allow Dragging' },
+];
 
-    this.commitChanges = this.commitChanges.bind(this);
-    this.handleEditingFeatureChange = this.handleEditingFeatureChange.bind(this);
-  }
+const Demo = () => {
+  const [data, setData] = React.useState(appointments);
+  const [editingFeatures, setEditingFeatures] = React.useState({
+    allowAdding: true,
+    allowDeleting: true,
+    allowUpdating: true,
+    allowResizing: true,
+    allowDragging: false,
+  });
+  const {
+    allowAdding, allowDeleting, allowUpdating, allowResizing, allowDragging,
+  } = editingFeatures;
 
-  commitChanges({ added, changed, deleted }) {
-    this.setState((state) => {
-      let { data } = state;
-      if (added) {
-        const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
-        data = [...data, { id: startingAddedId, ...added }];
-      }
-      if (changed) {
-        data = data.map(appointment => (
-          changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment));
-      }
-      if (deleted !== undefined) {
-        data = data.filter(appointment => appointment.id !== deleted);
-      }
-      return { data };
-    });
-  }
-
-  handleEditingFeatureChange({ target }) {
+  const onCommitChanges = React.useCallback(({ added, changed, deleted }) => {
+    if (added) {
+      const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
+      setData([...data, { id: startingAddedId, ...added }]);
+    }
+    if (changed) {
+      setData(data.map(appointment => (
+        changed[appointment.id] ? { ...appointment, ...changed[appointment.id] } : appointment)));
+    }
+    if (deleted !== undefined) {
+      setData(data.filter(appointment => appointment.id !== deleted));
+    }
+  });
+  const handleEditingFeaturesChange = React.useCallback(({ target }) => {
     const { value } = target;
-    const { [value]: checked } = this.state;
-    this.setState({
+    const { [value]: checked } = editingFeatures;
+    setEditingFeatures({
+      ...editingFeatures,
       [value]: !checked,
     });
-  }
+  });
+  // console.log(editingFeatures)
 
-  render() {
-    const {
-      currentDate, data, allowAdding, allowDeleting,
-      allowUpdating, allowDragging, allowResizing,
-    } = this.state;
+  const TimeTableCell = React.useCallback(React.memo(({ onDoubleClick, ...restProps }) => {
+    return <WeekView.TimeTableCell {...restProps} onDoubleClick={allowAdding ? onDoubleClick : undefined} />;
+  }), [allowAdding]);
 
-    const TimeTableCell = ({ onDoubleClick, ...restProps }) => {
-      return <WeekView.TimeTableCell {...restProps} onDoubleClick={allowAdding ? onDoubleClick : undefined} />;
-    };
+  const FormCommandLayout = React.useCallback(({ ...restProps }) => {
+    return <AppointmentForm.CommandLayout {...restProps} readOnly={false} />;
+  }, []);
 
-    const FormCommandLayout = ({ ...restProps }) => {
-      return <AppointmentForm.CommandLayout {...restProps} readOnly={false} />;
-    };
+  const CommandButton = React.useCallback(({ id, ...restProps }) => {
+    if (id === 'deleteButton') {
+      return <AppointmentForm.CommandButton id={id} {...restProps} disabled={!allowDeleting} />;
+    }
+    if (id === 'saveButton') {
+      return <AppointmentForm.CommandButton id={id} {...restProps} disabled={!allowUpdating} />;
+    }
+    return <AppointmentForm.CommandButton id={id} {...restProps} />;
+  }, [allowDeleting, allowUpdating]);
 
-    const CommandButton = ({ id, ...restProps }) => {
-      if (id === 'deleteButton') {
-        return <AppointmentForm.CommandButton id={id} {...restProps} disabled={!allowDeleting} />;
-      }
-      if (id === 'saveButton') {
-        return <AppointmentForm.CommandButton id={id} {...restProps} disabled={!allowUpdating} />;
-      }
-      return <AppointmentForm.CommandButton id={id} {...restProps} />;
-    };
+  return (
+    <>
+      <FormGroup row>
+        <FormControlLabel
+          control={
+            <Checkbox checked={allowAdding} onChange={handleEditingFeaturesChange} value="allowAdding" />
+          }
+          label="Allow Adding"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={allowDeleting} onChange={handleEditingFeaturesChange} value="allowDeleting" />
+          }
+          label="Allow Deleting"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={allowUpdating} onChange={handleEditingFeaturesChange} value="allowUpdating" />
+          }
+          label="Allow Updating"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={allowDragging} onChange={handleEditingFeaturesChange} value="allowDragging" />
+          }
+          label="Allow Dragging"
+        />
+        <FormControlLabel
+          control={
+            <Checkbox checked={allowResizing} onChange={handleEditingFeaturesChange} value="allowResizing" />
+          }
+          label="Allow Resizing"
+        />
+      </FormGroup>
+      <Paper>
+        <Scheduler
+          data={data}
+          height={660}
+        >
+          <ViewState
+            currentDate={currentDate}
+          />
+          <EditingState
+            onCommitChanges={onCommitChanges}
+          />
+          <IntegratedEditing />
+          <WeekView
+            startDayHour={9}
+            endDayHour={19}
+            timeTableCellComponent={TimeTableCell}
+          />
+          <ConfirmationDialog />
+          <Appointments />
+          <AppointmentTooltip
+            showOpenButton
+            showDeleteButton={allowDeleting}
+          />
+          <AppointmentForm
+            commandLayoutComponent={FormCommandLayout}
+            commandButtonComponent={CommandButton}
+            readOnly={!allowUpdating}
+          />
+          <DragDropProvider
+            allowDrag={() => allowDragging && allowUpdating}
+            allowResize={() => allowResizing && allowUpdating}
+          />
+        </Scheduler>
+      </Paper>
+    </>
+  );
+};
 
-    return (
-      <>
-        <FormGroup row>
-          <FormControlLabel
-            control={
-              <Checkbox checked={allowAdding} onChange={this.handleEditingFeatureChange} value="allowAdding" />
-            }
-            label="Allow Adding"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={allowDeleting} onChange={this.handleEditingFeatureChange} value="allowDeleting" />
-            }
-            label="Allow Deleting"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={allowUpdating} onChange={this.handleEditingFeatureChange} value="allowUpdating" />
-            }
-            label="Allow Updating"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={allowDragging} onChange={this.handleEditingFeatureChange} value="allowDragging" />
-            }
-            label="Allow Dragging"
-          />
-          <FormControlLabel
-            control={
-              <Checkbox checked={allowResizing} onChange={this.handleEditingFeatureChange} value="allowResizing" />
-            }
-            label="Allow Resizing"
-          />
-        </FormGroup>
-        <Paper>
-          <Scheduler
-            data={data}
-            height={660}
-          >
-            <ViewState
-              currentDate={currentDate}
-            />
-            <EditingState
-              onCommitChanges={this.commitChanges}
-            />
-            <IntegratedEditing />
-            <WeekView
-              startDayHour={9}
-              endDayHour={19}
-              timeTableCellComponent={TimeTableCell}
-            />
-            <ConfirmationDialog />
-            <Appointments />
-            <AppointmentTooltip
-              showOpenButton
-              showDeleteButton={allowDeleting}
-            />
-            <AppointmentForm
-              commandLayoutComponent={FormCommandLayout}
-              commandButtonComponent={CommandButton}
-              readOnly={!allowUpdating}
-            />
-            <DragDropProvider
-              allowDrag={() => allowDragging && allowUpdating}
-              allowResize={() => allowResizing && allowUpdating}
-            />
-          </Scheduler>
-        </Paper>
-      </>
-    );
-  }
-}
+export default Demo;

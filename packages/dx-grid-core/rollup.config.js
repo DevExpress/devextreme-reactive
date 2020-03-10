96ff2134a014ac7fccd1aac17eaee87f6f8224e5
Maxim Kudryavtsev <maxim_71rus_wow@mail.ru>
2020-03-10 20:15:52 +0300

    chore: move scheduler grouping landing section to `done` (#2732)
    - update Babel
    - update yarn
    - remove deprecated rollup plugins and use new ones
    - move scheduler grouping landing section to `done`

--- packages/dx-grid-core/rollup.config.js
+++ packages/dx-grid-core/rollup.config.js
@@ -1,6 +1,6 @@
 import license from 'rollup-plugin-license';
 import { default as typescriptRollup } from 'rollup-plugin-typescript2';
-import replace from 'rollup-plugin-replace';
+import replace from '@rollup/plugin-replace';
 import typescript from 'typescript';
 import {
   banner, external, globals, stubProcess,

17951a76901b23eeb8354fbbf7a85e6dac4cf316
Nikita Ushkal <nikita.ushkal@devexpress.com>
2020-03-10 15:35:07 +0300

    fix(react-grid): remove exceljs from ts declaration

--- packages/dx-react-grid-export/src/types/index.ts
+++ packages/dx-react-grid-export/src/types/index.ts
@@ -1,1 +1,7 @@
+export { 
+  CustomizeSummaryCellFn, ExportSummary, ExportRanges,
+} from '../../../dx-grid-core/src/types/export.types';
+export {
+  Column, SummaryType, GetCellValueFn,
+} from '../../../dx-grid-core/src/types';
 export * from './export';

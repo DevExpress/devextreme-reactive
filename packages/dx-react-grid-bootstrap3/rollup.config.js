6ec8ce815df61144a7c355cee4f99159310c4f6a
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2020-03-10 15:02:28 +0300

    Update rollup plugins

--- packages/dx-react-grid-bootstrap3/rollup.config.js
+++ packages/dx-react-grid-bootstrap3/rollup.config.js
@@ -1,19 +1,22 @@
 import babel from 'rollup-plugin-babel';
 import license from 'rollup-plugin-license';
-import resolve from 'rollup-plugin-node-resolve';
-import { banner, external, globals, stubProcess } from '../../tools/rollup-utils';
+import resolve from '@rollup/plugin-node-resolve';
+import {
+  banner, external, globals, stubProcess,
+} from '../../tools/rollup-utils';
 import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
   output: [
-    { file: pkg.main, format: 'umd', name: pkg.globalName, sourcemap: true, globals: globals(), intro: stubProcess },
+    {
+      file: pkg.main, format: 'umd', name: pkg.globalName, sourcemap: true, globals: globals(), intro: stubProcess,
+    },
     { file: pkg.module, format: 'es', sourcemap: true },
   ],
   external: external(__dirname),
   plugins: [
     resolve({
-      main: false,
       extensions: ['.js', '.jsx'],
     }),
     babel({

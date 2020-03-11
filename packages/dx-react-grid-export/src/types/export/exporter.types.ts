c40abf3f15c6160bbc2b9bdf713cb69630751b8f
ushkal <44223690+ushkal@users.noreply.github.com>
2020-03-11 13:25:18 +0300

    fix(react-grid): remove exceljs from ts declaration (#2734)

--- packages/dx-react-grid-export/src/types/export/exporter.types.ts
+++ packages/dx-react-grid-export/src/types/export/exporter.types.ts
@@ -3,8 +3,11 @@ import { Workbook, Cell as ExcelCell, Worksheet, Row as ExcelRow } from 'exceljs
 import {
   SortingStateProps, GroupingStateProps, TableGroupRowProps, SelectionStateProps,
   TableProps, TableColumnVisibilityProps, TableGroupRow, FilteringStateProps,
-  GridProps, SummaryItem, GroupSummaryItem, Column, CustomizeSummaryCellFn,
+  GridProps, GroupSummaryItem, SummaryItem, Column,
 } from '@devexpress/dx-react-grid/dist/dx-react-grid';
+import {
+  CustomizeSummaryCellFn,
+} from '../index';
 /* tslint:enable no-submodule-imports */
 
 export type ExporterProps =
@@ -17,14 +20,23 @@ export type ExporterProps =
   Pick<TableProps, 'columnExtensions'> &
   Pick<TableColumnVisibilityProps, 'hiddenColumnNames'> &
 {
+  /* The column order. */
   columnOrder?: string[],
+  /* Specifies additional properties for the columns used in grouping. */
   groupColumnExtensions?: TableGroupRow.ColumnExtension[],
+  /* Total summary items. */
   totalSummaryItems?: SummaryItem[],
+  /* Group summary items. */
   groupSummaryItems?: GroupSummaryItem[],
+  /* A function that should save the Excel document. */
   onSave: (workbook: Workbook) => void;
+  /* Customizes Excel cells. */
   customizeCell?: (cell: ExcelCell, row: ExcelRow, column: Column) => void;
+  /* Customizes Excel cells that display summaries. */
   customizeSummaryCell?: CustomizeSummaryCellFn;
+  /* Customizes the document's header. */
   customizeHeader?: (worksheet: Worksheet) => void;
+  /* Customizes the document's footer. */
   customizeFooter?: (worksheet: Worksheet) => void;
   /** @internal */
   exportSelected: boolean;

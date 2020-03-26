ad19b4b01e5e1b8cee677e25546f9a3b4b5b5c29
AryamnovEugeniy <earyamnow@mail.ru>
2020-03-26 16:18:03 +0300

    Fix test

--- packages/dx-react-grid-demos/src/demo-sources/grid-editing/edit-row-controlled.testcafe.js
+++ packages/dx-react-grid-demos/src/demo-sources/grid-editing/edit-row-controlled.testcafe.js
@@ -4,10 +4,19 @@ fixture `React Grid Demos - Edit Row Ccontrolled`
   .page`http://localhost:3002/#/demo/grid-editing/edit-row-controlled/bootstrap4/default`;
 
 test('Should correctly process editing in Controlled Mode', async (t) => {
-  await t
-    .switchToIframe('iframe')
-    .click('.dx-g-bs4-table-edit-command-cell#edit')
-    .click('.dx-g-bs4-table-edit-command-cell#cancel')
-    .expect(Selector('input').count)
-    .eql(0);
+  const iframe = Selector('iframe');
+  if (await iframe.exists) {
+    await t
+      .switchToIframe('iframe')
+      .click('.dx-g-bs4-table-edit-command-cell#edit')
+      .click('.dx-g-bs4-table-edit-command-cell#cancel')
+      .expect(Selector('input').count)
+      .eql(0);
+  } else {
+    await t
+      .click('.dx-g-bs4-table-edit-command-cell#edit')
+      .click('.dx-g-bs4-table-edit-command-cell#cancel')
+      .expect(Selector('input').count)
+      .eql(0);
+  }
 });

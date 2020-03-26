ede245837586462f0c461f8ce8913da1efd06b53
AryamnovEugeniy <earyamnow@mail.ru>
2020-03-26 17:48:06 +0300

    Use id

--- packages/dx-react-grid-demos/src/demo-sources/grid-editing/edit-row-controlled.testcafe.js
+++ packages/dx-react-grid-demos/src/demo-sources/grid-editing/edit-row-controlled.testcafe.js
@@ -1,13 +1,12 @@
 import { Selector } from 'testcafe';
 
-fixture `React Grid Demos - Edit Row Ccontrolled`
+fixture `React Grid Demos - Edit Row Controlled`
   .page`http://localhost:3002/#/demo/grid-editing/edit-row-controlled/bootstrap4/default/without-frame`;
 
 test('Should correctly process editing in Controlled Mode', async (t) => {
   await t
-    .wait(5000)
-    .click('.dx-g-bs4-table-edit-command-cell#edit')
-    .click('.dx-g-bs4-table-edit-command-cell#cancel')
+    .click('#edit')
+    .click('#cancel')
     .expect(Selector('input').count)
     .eql(0);
 });

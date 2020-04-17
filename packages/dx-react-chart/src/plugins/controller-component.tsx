7617c7ac75fef927d795df233022625bcf00603c
Eugeniy Aryamnov (DevExpress) <aryamnov.eugeniy@corp.devexpress.com>
2020-04-17 12:11:47 +0300

    Animate series after axes finished rerendering

--- packages/dx-react-chart/src/plugins/controller-component.tsx
+++ packages/dx-react-chart/src/plugins/controller-component.tsx
@@ -2,13 +2,21 @@ import * as React from 'react';
 import { Plugin, Getter, Getters } from '@devexpress/dx-react-core';
 import { getReadiness } from '@devexpress/dx-chart-core';
 
-const doGetReadiness = ({ layouts, centerDivRef }: Getters) => getReadiness(layouts, centerDivRef);
-
 export class ControllerComponent extends React.PureComponent {
+  isPreviousDataEmpty: boolean = true;
+
+  readyToRenderSeriesComputed = ({
+    layouts, centerDivRef, data, axesExist,
+  }: Getters) => {
+    const isPreviousDataEmpty = this.isPreviousDataEmpty;
+    this.isPreviousDataEmpty = !data.length;
+    return getReadiness(layouts, centerDivRef)
+      && ((isPreviousDataEmpty && axesExist) || !axesExist);
+  }
   render () {
     return (
     <Plugin name="ControllerComponent">
-      <Getter name="readyToRenderSeries" computed={doGetReadiness} />
+      <Getter name="readyToRenderSeries" computed={this.readyToRenderSeriesComputed} />
     </Plugin>
     );
   }

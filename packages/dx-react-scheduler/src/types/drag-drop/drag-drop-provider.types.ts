d0b60f359abde969f28eaa65bb900eccbee1c16f
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-06-18 16:23:44 +0300

    Update TS

--- packages/dx-react-scheduler/src/types/drag-drop/drag-drop-provider.types.ts
+++ packages/dx-react-scheduler/src/types/drag-drop/drag-drop-provider.types.ts
@@ -49,7 +49,7 @@ export interface DragDropProviderProps {
   allowDrag?: (appointmentData: AppointmentModel) => boolean;
   /** A function that specifies resizable appointments. */
   allowResize?: (appointmentData: AppointmentModel) => boolean;
-  /** Specifies the scroll speed when resizing or dragging an appointment. */
+  /** Specifies the scroll speed when an appointment is resized or dragged and dropped. */
   scrollSpeed?: number;
   /** A component that renders the appointment being dragged. */
   draftAppointmentComponent: React.ComponentType<DragDropProvider.DraftAppointmentProps>;

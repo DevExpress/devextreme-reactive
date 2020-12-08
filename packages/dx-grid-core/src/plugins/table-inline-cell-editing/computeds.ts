4415f4ec98365efc9b485bd053b1201f30dc147b
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2020-12-08 13:46:49 +0300

    Ignore no data rows

--- packages/dx-grid-core/src/plugins/table-inline-cell-editing/computeds.ts
+++ packages/dx-grid-core/src/plugins/table-inline-cell-editing/computeds.ts
@@ -1,10 +1,15 @@
 import { TableRowsWithEditingCellsFn, TableColumnsWithEditingCellsFn } from '../../types';
+import { TABLE_DATA_TYPE } from '../table/constants';
 
 export const rowsWithEditingCells: TableRowsWithEditingCellsFn = (
   tableBodyRows, editingCells,
 ) => tableBodyRows.map((row) => {
-  const { rowId } = row;
-  if (rowId !== undefined && editingCells.some(elem => elem.rowId === rowId)) {
+  const { rowId, type } = row;
+  if (
+    rowId !== undefined &&
+    type === TABLE_DATA_TYPE &&
+    editingCells.some(elem => elem.rowId === rowId)
+  ) {
     return {
       ...row,
       hasEditCell: true,

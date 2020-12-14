cec4a32131b7e318103097d23e9e9af30e51853d
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-12-14 18:16:38 +0300

    Refactor type

--- packages/dx-react-scheduler/src/types/views/common-vertical-view.types.ts
+++ packages/dx-react-scheduler/src/types/views/common-vertical-view.types.ts
@@ -1,12 +1,5 @@
-import { ViewCell } from '../index';
-import { WeekViewProps } from './index';
+import { WeekViewProps, BasicViewProps } from './index';
 
 /** @internal */
-export interface CommonVerticalViewProps extends WeekViewProps {
-  /** A function that calculates cells data. */
-  viewCellsDataComputed: (
-    cellDuration: number, startDayHour: number, endDayHour: number,
-  ) => (payload: any) => readonly ViewCell[][];
-  /** The view's type */
-  type: string;
-}
+export type CommonVerticalViewProps = WeekViewProps
+  & Pick<BasicViewProps, 'viewCellsDataComputed' | 'type'>

9a3e75dd9c7c46674ea5191d5b0b9f506246cdfc
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2021-02-06 09:26:21 +0300

    chore: update site

---
+++ @next/component---src-pages-404-jsx-68c4f8ee14bebc334a69.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{pssB:function(e,t,n){"use strict";n.r(t);var s=n("ERkP");t.default=function(){return s.createElement("div",null,s.createElement("h1",null,"NOT FOUND"),s.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-68c4f8ee14bebc334a69.js.map

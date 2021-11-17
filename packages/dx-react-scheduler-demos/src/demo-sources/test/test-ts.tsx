e6e9e024a77e9342026a231167a77977c70d900f
Aryamnov Eugeniy <51905811+AryamnovEugeniy@users.noreply.github.com>
2021-11-17 12:28:13 +0300

    fix(react-scheduler): use correct type for ConfirmationDialog's messages (#3433)

---
+++ packages/dx-react-scheduler-demos/src/demo-sources/test/test-ts.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-filename-extension */
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import {
+  Scheduler, ConfirmationDialog,
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+const Demo: React.SFC = () => (
+  <Paper>
+    <Scheduler
+      data={[]}
+    >
+      <ConfirmationDialog
+        ignoreCancel
+        messages={{
+          discardButton: 'ddd',
+        }}
+      />
+    </Scheduler>
+  </Paper>
+);
+
+export default Demo;

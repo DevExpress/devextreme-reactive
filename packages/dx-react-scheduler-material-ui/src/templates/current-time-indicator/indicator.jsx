0d810cae7f5f53927d359f654433233380934236
Smirnova Yuliya <yuliya.smirnova@devexpress.com>
2021-11-19 19:52:19 +0300

    fix indicator

--- packages/dx-react-scheduler-material-ui/src/templates/current-time-indicator/indicator.jsx
+++ packages/dx-react-scheduler-material-ui/src/templates/current-time-indicator/indicator.jsx
@@ -11,48 +11,36 @@ export const classes = {
   nowIndicator: `${PREFIX}-nowIndicator`,
 };
 
-const nowIndicatorStyles = (theme, topValue) => ({
-  position: 'absolute',
-  left: 0,
-  top: topValue,
-  background: theme.palette.secondary.main,
-  zIndex: 1,
-});
+const StyledDiv = styled('div', {
+  shouldForwardProp: prop => prop !== 'topValue',
+})(({ theme, topValue }) => ({
+  [`& .${classes.circle}`]: {
+    width: theme.spacing(1.5),
+    height: theme.spacing(1.5),
+    borderRadius: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  [`& .${classes.line}`]: {
+    height: '2px',
+    width: '100%',
+    transform: 'translate(0, -1px)',
+  },
+  [`& .${classes.nowIndicator}`]: {
+    position: 'absolute',
+    left: 0,
+    top: topValue,
+    background: theme.palette.secondary.main,
+    zIndex: 1,
+  },
+}));
 
-const StyledDivCircle = styled('div')(({ theme, topValue }) => {
-  const nowIndicator = nowIndicatorStyles(theme, topValue);
-  return {
-    [`&.${classes.circle}`]: {
-      width: theme.spacing(1.5),
-      height: theme.spacing(1.5),
-      borderRadius: '50%',
-      transform: 'translate(-50%, -50%)',
-    },
-    [`&.${classes.nowIndicator}`]: {
-      ...nowIndicator,
-    },
-  };
-});
-
-const StyledDivLine = styled('div')(({ theme, topValue }) => {
-  const nowIndicator = nowIndicatorStyles(theme, topValue);
-  return {
-    [`&.${classes.line}`]: {
-      height: '2px',
-      width: '100%',
-      transform: 'translate(0, -1px)',
-    },
-    [`&.${classes.nowIndicator}`]: {
-      ...nowIndicator,
-    },
-  };
-});
-
-export const Indicator = props => (
-  <div {...props}>
-    <StyledDivCircle className={classNames(classes.nowIndicator, classes.circle)} />
-    <StyledDivLine className={classNames(classes.nowIndicator, classes.line)} />
-  </div>
+export const Indicator = ({
+  top, ...restProps
+}) => (
+  <StyledDiv topValue={top} {...restProps}>
+    <div className={classNames(classes.nowIndicator, classes.circle)} />
+    <div className={classNames(classes.nowIndicator, classes.line)} />
+  </StyledDiv>
 );
 
 Indicator.propTypes = {

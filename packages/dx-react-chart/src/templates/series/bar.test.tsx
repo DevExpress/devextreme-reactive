820f4cb66243534a76466ebb9d39a42640f72d99
ivanblinov2k17 <ivanblinov2015@mail.ru>
2022-04-05 15:37:17 +0300

    fix test

--- packages/dx-react-chart/src/templates/series/bar.test.tsx
+++ packages/dx-react-chart/src/templates/series/bar.test.tsx
@@ -40,7 +40,7 @@ describe('Bar', () => {
     maxBarWidth: 20,
     val: 2,
     startVal: 18,
-    color: 'blue',
+    color: 'color',
     rotated: true,
     style: { tag: 'test-style' },
     scales: { tag: 'test-scales' } as any,
@@ -91,7 +91,7 @@ describe('Bar', () => {
     expect((withPattern as jest.Mock).mock.calls[0][0]({ seriesIndex: 1, index: 2 }))
       .toEqual('series-1-point-2-hover');
     expect((withPattern as jest.Mock).mock.calls[1][0]({ seriesIndex: 2, index: 3 }))
-      .toEqual('series-2-point-3-color-blue-selection');
+      .toEqual('series-2-point-3-color-undefined-selection');
   });
 
   it('should animate', () => {

e9aa4f4c47a996126a2a775266b180baaa6abef0
timbset <timbset@gmail.com>
2022-10-10 19:45:38 +0400

    Fix tests in dx-chart-core

--- packages/dx-chart-core/jest.config.js
+++ packages/dx-chart-core/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/js-with-babel',
   globals: {
     'ts-jest': {
       tsConfig: './tsconfig.json',
@@ -7,6 +7,9 @@ module.exports = {
     },
   },
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  transformIgnorePatterns: [
+    '/node_modules/(?!(d3|d3-scale|d3-array|d3-interpolate|d3-color|d3-format|d3-time|d3-time-format|internmap)/)',
+  ],
   testMatch: [
     '**/*.test.(ts|tsx)',
   ],

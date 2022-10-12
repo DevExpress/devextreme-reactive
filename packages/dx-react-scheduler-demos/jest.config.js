1b37249fb606763daf32b2d6f9e7776b7ae482ae
timbset <timbset@gmail.com>
2022-10-12 18:23:33 +0400

    Rework demos jest with babel configs

--- packages/dx-react-scheduler-demos/jest.config.js
+++ packages/dx-react-scheduler-demos/jest.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.jsx?$': '../../tools/setup-babel-jest.js',
     '^.+\\.tsx?$': 'ts-jest',
   },
   transformIgnorePatterns: [
-    '/node_modules/(?!(d3|d3-scale|d3-array|d3-interpolate|d3-color|d3-format|d3-time|d3-time-format|d3-scale-chromatic|internmap)/)',
+    '/node_modules/',
     '/dist/',
   ],
   moduleNameMapper: {

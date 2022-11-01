c5b91901ad51550199d49df0a151eace12cbfacf
Shpileva Yuliya <krijovnick@gmail.com>
2022-11-01 16:21:42 +0100

    fix test

--- packages/dx-chart-core/src/types/plugins.tooltip.types.ts
+++ packages/dx-chart-core/src/types/plugins.tooltip.types.ts
@@ -3,7 +3,7 @@ import { Rect } from './chart-core.types';
 export interface TooltipReference {
   readonly clientWidth: number;
   readonly clientHeight: number;
-  getBoundingClientRect(): Partial<DOMRect>
+  getBoundingClientRect(): Partial<DOMRect>;
 }
 /** @internal */
 export type TooltipParameters = {

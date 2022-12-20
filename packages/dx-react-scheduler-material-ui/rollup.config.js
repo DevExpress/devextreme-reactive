9854c6ae560554bee907f48316e79c2dc47db767
Lykoi18 <aiden.ghost666@gmail.com>
2022-12-20 20:50:44 +0400

    Refactor rollup config

--- packages/dx-react-scheduler-material-ui/rollup.config.js
+++ packages/dx-react-scheduler-material-ui/rollup.config.js
@@ -5,29 +5,43 @@ import modify from 'rollup-plugin-modify';
 import { banner, external, stubProcess } from '../../tools/rollup-utils';
 import pkg from './package.json';
 
-export default {
+const commonPlugins = [
+  resolve({
+    extensions: ['.js', '.jsx'],
+  }),
+  babel({
+    runtimeHelpers: true,
+    exclude: 'node_modules/**',
+  }),
+  license({
+    banner,
+  }),
+];
+
+const commonConfig = {
   input: 'src/index.js',
-  output: [
-    {
+  external: external(__dirname),
+  plugins: commonPlugins,
+};
+
+export default [
+  {
+    ...commonConfig,
+    output: {
       file: pkg.main, format: 'cjs', sourcemap: true, intro: stubProcess,
     },
-    { file: pkg.module, format: 'es', sourcemap: true },
-  ],
-  external: external(__dirname),
-  plugins: [
-    resolve({
-      extensions: ['.js', '.jsx'],
-    }),
-    babel({
-      runtimeHelpers: true,
-      exclude: 'node_modules/**',
-    }),
-    modify({
-      find: /import (\w+) from '@mui\/icons-material\/(.+)';/,
-      replace: (match, name, path) => `import ${name}Pkg from '@mui/icons-material/${path}.js'; const ${name} = ${name}Pkg.default;`,
-    }),
-    license({
-      banner,
-    }),
-  ],
-};
+  },
+  {
+    ...commonConfig,
+    output: {
+      file: pkg.module, format: 'es', sourcemap: true,
+    },
+    plugins: [
+      ...commonPlugins,
+      modify({
+        find: /import (\w+) from '@mui\/icons-material\/(.+)';/,
+        replace: (match, name, path) => `import ${name}Pkg from '@mui/icons-material/${path}.js'; const ${name} = ${name}Pkg.default;`,
+      }),
+    ],
+  },
+];

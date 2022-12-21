8ec2bd69fc0324b477c5828143a02404c2d0d551
Timofey Traynenkov <timbset@gmail.com>
2022-12-21 16:46:22 +0400

    fix(react-core): fix importing of mui packages that do not support ESM yet (#3608)
    * Use material root import instead of specific components to support ESM
    
    * Use material root import instead of specific components to support ESM in scheduler material ui package
    
    * Transform icons-material to add extension to a file
    
    * Add bootstrap4 styles to exports
    
    * Modify x-date-pickers import path
    
    * Fix rrule import error
    
    * Fix bundled icons-material imports
    
    * Update yarn.lock
    
    * Rollback dependencies
    
    * Refactor rollup config
    
    Co-authored-by: Lykoi18 <aiden.ghost666@gmail.com>

--- packages/dx-react-grid-material-ui/rollup.config.js
+++ packages/dx-react-grid-material-ui/rollup.config.js
@@ -1,28 +1,47 @@
 import babel from 'rollup-plugin-babel';
 import license from 'rollup-plugin-license';
 import resolve from '@rollup/plugin-node-resolve';
+import modify from 'rollup-plugin-modify';
 import { banner, external, stubProcess } from '../../tools/rollup-utils';
 import pkg from './package.json';
 
-export default {
+const commonPlugins = [
+  resolve({
+    extensions: ['.js', '.jsx'],
+  }),
+  babel({
+    runtimeHelpers: true,
+    exclude: 'node_modules/**',
+  }),
+  license({
+    banner,
+  }),
+];
+
+const commonConfig = {
   input: 'src/index.js',
-  output: [
-    {
+  external: external(__dirname),
+  plugins: commonPlugins,
+};
+
+export default [
+  {
+    ...commonConfig,
+    output: {
       file: pkg.main, format: 'cjs', sourcemap: true, intro: stubProcess,
     },
-    { file: pkg.module, format: 'es', sourcemap: true },
-  ],
-  external: external(__dirname),
-  plugins: [
-    resolve({
-      extensions: ['.js', '.jsx'],
-    }),
-    babel({
-      runtimeHelpers: true,
-      exclude: 'node_modules/**',
-    }),
-    license({
-      banner,
-    }),
-  ],
-};
+  },
+  {
+    ...commonConfig,
+    output: {
+      file: pkg.module, format: 'es', sourcemap: true,
+    },
+    plugins: [
+      ...commonPlugins,
+      modify({
+        find: /import (\w+) from '@mui\/icons-material\/(.+)';/,
+        replace: (match, name, path) => `import ${name}Pkg from '@mui/icons-material/${path}.js'; const ${name} = ${name}Pkg.default;`,
+      }),
+    ],
+  },
+];

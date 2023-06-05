caf26debca778f4a857a8274eb14d58176b748f1
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2023-06-05 19:08:40 +0400

    silence the linter

--- packages/dx-grid-core/src/plugins/sorting-state/helpers.ts
+++ packages/dx-grid-core/src/plugins/sorting-state/helpers.ts
@@ -15,6 +15,7 @@ export const getColumnSortingDirection: GetColumnSortingDirectionFn = (
 export const getPersistentSortedColumns: GetPersistentSortedColumnsFn = (
   sorting, columnExtensions = [],
 ) => columnExtensions.reduce((acc, { columnName, sortingEnabled }) => {
+  // tslint:disable-next-line:no-boolean-literal-compare
   if (sortingEnabled === false) {
     if (sorting.findIndex(sortItem => sortItem.columnName === columnName) > -1) {
       acc.push(columnName);

fd012c0310e07631f992a4c655f414cbb863f579
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2023-10-18 15:30:18 +0400

    fix matomo setup

--- packages/dx-site/src/html.jsx
+++ packages/dx-site/src/html.jsx
@@ -35,17 +35,6 @@ export default class extends React.Component {
           <link rel="preconnect" href="https://matomo.devexpress.com" />
           <link rel="dns-prefetch" href="https://matomo.devexpress.com" />
           <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.js" />
-
-          { /* Matomo Tag Manager */ }
-          <script>
-            {`
-              var _mtm = window._mtm = window._mtm || [];
-              _mtm.push({ 'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start' });
-              var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-              g.async = true; g.src = 'https://matomo.devexpress.com/js/container_foTT0fJ8.js'; g.setAttributeNode(d.createAttribute('data-ot-ignore')); s.parentNode.insertBefore(g, s);
-            `}
-          </script>
-          { /* End Matomo Tag Manager */ }
         </head>
         <body>
           <div

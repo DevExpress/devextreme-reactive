d95192a1efc8b00c358620cea6972213a671485a
Mikhail Preyskurantov <mikhail.preyskurantov@devexpress.com>
2023-10-23 17:44:19 +0300

    chore: update site

---
+++ component---src-pages-404-jsx-2e9a5cc29ac093b72996.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{pssB:function(e,t,n){"use strict";n.r(t);var s=n("ERkP");t.default=function(){return s.createElement("div",null,s.createElement("h1",null,"NOT FOUND"),s.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-2e9a5cc29ac093b72996.js.map

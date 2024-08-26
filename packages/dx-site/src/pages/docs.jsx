975e0f34ee42568961261be03eff28aad1791de3
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-08-26 19:19:11 +0400

    formatting

--- packages/dx-site/src/pages/docs.jsx
+++ packages/dx-site/src/pages/docs.jsx
@@ -57,13 +57,12 @@ export default () => (
               <div className="note-start">NOTE</div>
               <p>
                 <div className="part-title">DevExtreme Reactive Components - Maintenance Support Mode</div>
-                DevExtreme Reactive component libraries are in
-                <a
+                DevExtreme Reactive component libraries are in <a
                 href="https://github.com/DevExpress/devextreme-reactive/blob/master/README.md"
                 target="_blank"
                 rel="noopener noreferrer"
                 >
-                 maintenance support mode
+                  &nbsp;maintenance support mode
                 </a>
                  .No new features/capabilities will be added to DevExtreme Reactive component
                 libraries in the future (end-of-life: December 2025).

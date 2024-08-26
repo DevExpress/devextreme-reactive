b410b61aad31f8cbd2f3f377669b36ee3ce6b2dd
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-08-26 18:49:39 +0400

    add link

--- packages/dx-site/src/components/docs/notification-box.jsx
+++ packages/dx-site/src/components/docs/notification-box.jsx
@@ -11,7 +11,13 @@ const NotificationBox = ({ style }) => (
             DevExtreme Reactive Components - Maintenance Support Mode
           </p>
           <p className={styles.notificationBoxText}>
-            DevExtreme Reactive component libraries are in maintenance support mode.
+            DevExtreme Reactive component libraries are in <a
+            href="https://github.com/DevExpress/devextreme-reactive/blob/master/README.md"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+            maintenance support mode
+            </a>.
             No new features/capabilities will be added to DevExtreme Reactive component
             libraries in the future (end-of-life: December 2025).
           </p>
